85cc1c87ae6a10081c000b2462e8c13b2c7b7ff9
riordhn <ramadhanar.rio@gmail.com>
2019-11-25 09:15:14 +0700

    test CI/CD

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,6 @@ Route::post('upload', function(Request $request) {
 
 Route::get('/', 'SignInController@indexSignIn');
 Route::post('signin', 'SignInController@actionSignIn');
-
 Route::group(array('middleware'=> ['token_staff']), function () {
     //
     Route::group(array('prefix' => '{global}'), function () {
