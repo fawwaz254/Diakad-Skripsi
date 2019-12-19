38d0874477f00134ad62c64f98c15cf4b68ad43b
riordhn <ramadhanar.rio@gmail.com>
2019-12-19 16:04:50 +0700

    adding backspace in rekap absen

--- resources/views/guru/presensi/rekap-absen/view-kbm-rekap-absen.blade.php
+++ resources/views/guru/presensi/rekap-absen/view-kbm-rekap-absen.blade.php
@@ -5,6 +5,9 @@
     }
 </style>
 <div class="container-fluid">
+    <div class="block-header">
+        <h2><a class="btn bg-blue waves-effect target-link " href="{{url(Request::segment(1).'#presensi/rekap-absen')}}"><i class="material-icons">backspace</i><span>Kembali</span></a></h2>
+    </div>
     <div class="row clearfix">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="card">
