3a493ee50f5a3c57a112e75b8515211edeab0991
riordhn <ramadhanar.rio@gmail.com>
2020-01-09 10:52:47 +0700

    perawatan rutin ruangan refixing

--- app/Http/Controllers/SaranaPrasarana/PerawatanSarpras/InputPerawatanRutinController.php
+++ app/Http/Controllers/SaranaPrasarana/PerawatanSarpras/InputPerawatanRutinController.php
@@ -16,18 +16,19 @@ use DB;
 use Session;
 use Validator;
 
-class InputPerawatanRutinController extends BaseController{
-
-    public function viewInputPerawatanRutin(Request $request){
+class InputPerawatanRutinController extends BaseController
+{
+    public function viewInputPerawatanRutin(Request $request)
+    {
         # code...
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
 
-    	return view('sarana-prasarana/perawatan-sarpras/perawatan-rutin/view-perawatan-rutin',compact('auth_data'));
-
+        return view('sarana-prasarana/perawatan-sarpras/perawatan-rutin/view-perawatan-rutin', compact('auth_data'));
     }
 
-    public function ajaxGetInventarisByRuangan(Request $request) {
+    public function ajaxGetInventarisByRuangan(Request $request)
+    {
         # code...
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
@@ -38,7 +39,8 @@ class InputPerawatanRutinController extends BaseController{
         return $data_inventaris;
     }
 
-    public function addInputPerawatanRutin(Request $request){
+    public function addInputPerawatanRutin(Request $request)
+    {
         # code...
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
@@ -46,22 +48,22 @@ class InputPerawatanRutinController extends BaseController{
         // mengambil waktu sekarang
         $now = Carbon::now(env('APP_TIMEZONE', ''));
 
-        $data_ruangan = LibDataSarpras::fetchDataRuangan($auth_data, 1);
+        $data_ruangan = LibDataSarpras::fetchDataRuangan($auth_data);
 
         $data_buku_alat = LibDataSarpras::fetchDataBukuAlat($auth_data);
 
         $id_perawatan_sarpras = $auth_data->sekolah_data->prefix.strtotime($now).uniqid();
 
-        return view('sarana-prasarana/perawatan-sarpras/perawatan-rutin/add-perawatan-rutin',compact('auth_data','data_ruangan','data_buku_alat','id_perawatan_sarpras'));
-
+        return view('sarana-prasarana/perawatan-sarpras/perawatan-rutin/add-perawatan-rutin', compact('auth_data', 'data_ruangan', 'data_buku_alat', 'id_perawatan_sarpras'));
     }
 
-    public function editInputPerawatanRutin($id, Request $request){
+    public function editInputPerawatanRutin($id, Request $request)
+    {
         # code...
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
 
-        $data_ruangan = LibDataSarpras::fetchDataRuangan($auth_data, 1);
+        $data_ruangan = LibDataSarpras::fetchDataRuangan($auth_data);
 
         $data_inventaris_ruangan = LibDataSarpras::fetchDataInventarisRuangan($auth_data);
 
@@ -70,36 +72,35 @@ class InputPerawatanRutinController extends BaseController{
         $data_perawatan_sarpras = LibDataSarpras::fetchDataPerawatanSarpras($auth_data, 0, $id);
 
         // convert format date
-        $tgl_perawatan = strftime( "%A, %d %B %Y", strtotime($data_perawatan_sarpras->tgl_perawatan));
+        $tgl_perawatan = strftime("%A, %d %B %Y", strtotime($data_perawatan_sarpras->tgl_perawatan));
 
-        return view('sarana-prasarana/perawatan-sarpras/perawatan-rutin/edit-perawatan-rutin',compact('auth_data','data_ruangan','data_inventaris_ruangan','data_buku_alat','data_perawatan_sarpras','tgl_perawatan'));
-
+        return view('sarana-prasarana/perawatan-sarpras/perawatan-rutin/edit-perawatan-rutin', compact('auth_data', 'data_ruangan', 'data_inventaris_ruangan', 'data_buku_alat', 'data_perawatan_sarpras', 'tgl_perawatan'));
     }
 
-    public function datatablesInputPerawatanRutinBelum(Request $request){
+    public function datatablesInputPerawatanRutinBelum(Request $request)
+    {
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
         $list_data = LibDataSarpras::fetchDataPerawatanSarpras($auth_data, 0, null, 1);
 
         return Datatables::of($list_data)
-                ->addColumn('nm_ruangan', function($item){
+                ->addColumn('nm_ruangan', function ($item) {
                     return $item->nm_ruangan." - ".$item->nm_jenis_ruangan." (".$item->nm_gedung.")";
                 })
-                ->addColumn('nm_inventaris_ruangan', function($item){
-                    if( ! empty($item->nm_inventaris_ruangan)) {
+                ->addColumn('nm_inventaris_ruangan', function ($item) {
+                    if (! empty($item->nm_inventaris_ruangan)) {
                         return $item->nm_inventaris_ruangan;
-                    }
-                    else {
+                    } else {
                         return "-";
                     }
                 })
-                ->addColumn('nm_buku_alat', function($item){
+                ->addColumn('nm_buku_alat', function ($item) {
                     return $item->nm_buku_alat." - ".$item->nm_jenis_buku_alat;
                 })
-                ->addColumn('tgl_perawatan', function($item){
-                    return strftime( "%d %B %Y", strtotime($item->tgl_perawatan));
+                ->addColumn('tgl_perawatan', function ($item) {
+                    return strftime("%d %B %Y", strtotime($item->tgl_perawatan));
                 })
-                ->addColumn('action', function($item){
+                ->addColumn('action', function ($item) {
                     $data = array(
                         'id' => $item->id_perawatan_sarpras
                     );
@@ -108,30 +109,30 @@ class InputPerawatanRutinController extends BaseController{
                 ->make(true);
     }
 
-    public function datatablesInputPerawatanRutinSudah(Request $request){
+    public function datatablesInputPerawatanRutinSudah(Request $request)
+    {
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
         $list_data = LibDataSarpras::fetchDataPerawatanSarpras($auth_data, 1, null, 1);
 
         return Datatables::of($list_data)
-                ->addColumn('nm_ruangan', function($item){
+                ->addColumn('nm_ruangan', function ($item) {
                     return $item->nm_ruangan." - ".$item->nm_jenis_ruangan." (".$item->nm_gedung.")";
                 })
-                ->addColumn('nm_inventaris_ruangan', function($item){
-                    if( ! empty($item->nm_inventaris_ruangan)) {
+                ->addColumn('nm_inventaris_ruangan', function ($item) {
+                    if (! empty($item->nm_inventaris_ruangan)) {
                         return $item->nm_inventaris_ruangan;
-                    }
-                    else {
+                    } else {
                         return "-";
                     }
                 })
-                ->addColumn('nm_buku_alat', function($item){
+                ->addColumn('nm_buku_alat', function ($item) {
                     return $item->nm_buku_alat." - ".$item->nm_jenis_buku_alat;
                 })
-                ->addColumn('tgl_perawatan', function($item){
-                    return strftime( "%d %B %Y", strtotime($item->tgl_perawatan));
+                ->addColumn('tgl_perawatan', function ($item) {
+                    return strftime("%d %B %Y", strtotime($item->tgl_perawatan));
                 })
-                ->addColumn('action', function($item){
+                ->addColumn('action', function ($item) {
                     $data = array(
                         'id' => $item->id_perawatan_sarpras
                     );
@@ -141,8 +142,8 @@ class InputPerawatanRutinController extends BaseController{
     }
 
     // Action POST
-    public function actionInputPerawatanRutin(Request $request, $mode, $id = null){
-
+    public function actionInputPerawatanRutin(Request $request, $mode, $id = null)
+    {
         $input = (object) $request->input();
 
         $validator = Validator::make($request->all(), [
@@ -151,18 +152,17 @@ class InputPerawatanRutinController extends BaseController{
             'is_sudah_perawatan' => 'required'
         ]);
         
-        if($validator->fails() && $mode != 'delete') {
+        if ($validator->fails() && $mode != 'delete') {
             return [
                 'status' => 300, // FAILED
                 'message' => $validator->errors()->first()
             ];
-        }
-        else{
+        } else {
             // mengambil waktu sekarang
             $now = Carbon::now(env('APP_TIMEZONE', ''));
 
-            if($mode == 'add'){
-                if ( ! empty($input->id_ruangan) or ! empty($input->id_inventaris_ruangan) or ! empty($input->id_buku_alat)) {
+            if ($mode == 'add') {
+                if (! empty($input->id_ruangan) or ! empty($input->id_inventaris_ruangan) or ! empty($input->id_buku_alat)) {
                     // make object to find id
                     $perawatanSarpras                           = new PerawatanSarpras;
                     $perawatanSarpras->id_perawatan_sarpras     = $input->auth_data->sekolah_data->prefix.strtotime($now).uniqid();
@@ -170,7 +170,7 @@ class InputPerawatanRutinController extends BaseController{
                     $perawatanSarpras->id_inventaris_ruangan    = $input->id_inventaris_ruangan;
                     $perawatanSarpras->id_buku_alat             = $input->id_buku_alat;
                     // convert format date
-                    $perawatanSarpras->tgl_perawatan            = date_format(date_create($input->tgl_perawatan),"Y-m-d H:i:s");
+                    $perawatanSarpras->tgl_perawatan            = date_format(date_create($input->tgl_perawatan), "Y-m-d H:i:s");
                     $perawatanSarpras->keterangan_perawatan     = $input->keterangan_perawatan;
                     $perawatanSarpras->is_sudah_perawatan       = $input->is_sudah_perawatan;
                     $perawatanSarpras->created_by               = $input->auth_data->pengguna->id_pengguna;
@@ -181,19 +181,17 @@ class InputPerawatanRutinController extends BaseController{
                         'path' => 'perawatan-sarpras/input-perawatan-rutin',
                         'message' => 'Input Perawatan Rutin successfully'
                     ];
-                }
-                else {
+                } else {
                     return [
                         'status' => 300, // SUCCESS AND LOAD TABLE
                         'message' => 'Isi Salah Satu (Ruangan, Inventaris atau Buku/Alat)'
-                    ]; 
+                    ];
                 }
-            }
-            elseif($mode == 'edit'){
+            } elseif ($mode == 'edit') {
                 // make object to find id
                 $perawatanSarpras                           = PerawatanSarpras::find($id);
                 // convert format date
-                $perawatanSarpras->tgl_perawatan            = date_format(date_create($input->tgl_perawatan),"Y-m-d H:i:s");
+                $perawatanSarpras->tgl_perawatan            = date_format(date_create($input->tgl_perawatan), "Y-m-d H:i:s");
                 $perawatanSarpras->keterangan_perawatan     = $input->keterangan_perawatan;
                 $perawatanSarpras->is_sudah_perawatan       = $input->is_sudah_perawatan;
                 $perawatanSarpras->updated_by               = $input->auth_data->pengguna->id_pengguna;
@@ -205,8 +203,7 @@ class InputPerawatanRutinController extends BaseController{
                     'path' => 'perawatan-sarpras/input-perawatan-rutin',
                     'message' => 'Update Perawatan Rutin successfully'
                 ];
-            }
-            elseif($mode == 'delete'){
+            } elseif ($mode == 'delete') {
                 // make object to find id
                 $perawatanSarpras               = PerawatanSarpras::find($id);
                 $perawatanSarpras->deleted_by   = $input->auth_data->pengguna->id_pengguna;
@@ -221,6 +218,4 @@ class InputPerawatanRutinController extends BaseController{
             }
         }
     }
-
-
 }
