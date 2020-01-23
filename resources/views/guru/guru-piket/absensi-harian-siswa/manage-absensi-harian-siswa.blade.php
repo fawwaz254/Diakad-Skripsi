32809e2f34e702ed790c86dae70c9db07af9e24f
riordhn <ramadhanar.rio@gmail.com>
2020-01-23 14:25:37 +0700

    adding absensi harian with mutasi

--- resources/views/guru/guru-piket/absensi-harian-siswa/manage-absensi-harian-siswa.blade.php
+++ resources/views/guru/guru-piket/absensi-harian-siswa/manage-absensi-harian-siswa.blade.php
@@ -89,18 +89,21 @@
             { data: 'nm_pengguna', name: 'pengguna.nm_pengguna' },
             { data: 'alasan', name: 'alasan', searchable: false, orderable: false,
                 render: function(data){
-                    let html = '';
-                    console.log(data.kehadiran);
-                    $.each(data.options, function(index, item){
-                        if(data.kehadiran == item.id){
-                            html += '<option value="'+item.id+'" selected>'+ item.text + '</option>';
-                        }else{
-                            html += '<option value="'+item.id+'">'+ item.text + '</option>';
-                        }
-                    })
-                    return '<select class="form-control show-tick" style="width:85px;" name="alasan[]">'+
-                    html +
-                    '</select>';
+                    if(data.status_pengguna.status == 1){
+                        let html = '';
+                        $.each(data.options, function(index, item){
+                            if(data.kehadiran == item.id){
+                                html += '<option value="'+item.id+'" selected>'+ item.text + '</option>';
+                            }else{
+                                html += '<option value="'+item.id+'">'+ item.text + '</option>';
+                            }
+                        })
+                        return '<select class="form-control show-tick" style="width:85px;" name="alasan[]">'+
+                        html +
+                        '</select>';
+                    }else{
+                        return '<p class="font-underline col-orange font-24">' + data.status_pengguna.nm_status + '</p>';
+                    }
                 }
             }
         ]
