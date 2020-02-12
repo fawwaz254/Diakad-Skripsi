e40459cdccd7d338d4d5d53bc3f98ba2ce2ba335
riordhn <ramadhanar.rio@gmail.com>
2020-02-12 12:22:59 +0700

    action delete on tendik absensi siswa

--- routes/tendik/web.php
+++ routes/tendik/web.php
@@ -24,6 +24,7 @@ Route::group(array('middleware'=> ['token_staff']), function () {
             Route::post('absensi-harian-siswa/datatables/{id_semester}/{id_kelas}', 'Guru\GuruPiket\AbsensiHarianSiswaController@datatablesAbsensiHarianSiswa');
             Route::post('absensi-harian-siswa/datatables-detail/{id_semester}/{id_kelas}/{id_presensi_harian}', 'Guru\GuruPiket\AbsensiHarianSiswaController@datatablesKelasAbsensiHariSiswa');
             Route::post('absensi-harian-siswa/action/{mode}', 'Guru\GuruPiket\AbsensiHarianSiswaController@actionAbsensiHarianSiswa');
+            Route::post('absensi-harian-siswa/action/{mode}/{id}', 'Guru\GuruPiket\AbsensiHarianSiswaController@actionAbsensiHarianSiswa');
 
             // MENU Input Pelanggaran Siswa Non-KBM
             Route::get('input-pelanggaran', 'Guru\GuruPiket\InputPelanggaranController@viewInputPelanggaran');
