ed288be0cf027e72507ddc0568f89a0f90c98e38
riordhn <ramadhanar.rio@gmail.com>
2020-05-11 11:34:33 +0700

    adding migration to server

---
+++ database/migrations/2020_05_11_112638_adding_is_report_tabel_realisasi.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddingIsReportTabelRealisasi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('realisasi', function (Blueprint $table) {
+            $table->tinyInteger('is_report')->default('1')->after('is_maba')->comment('1 = Masuk ke dalam report; 2 = Tidak masuk dalam report;')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
