61ea85c021101894d40a3ba10e75c3cbeb7da0a0
Rio Ramadhan <ramadhanar.rio@gmail.com>
2020-07-01 12:59:45 +0700

    remove button on semester

--- resources/views/pendidikan/data-akademik/nama-semester/view-nama-semester.blade.php
+++ resources/views/pendidikan/data-akademik/nama-semester/view-nama-semester.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
-    <div class="block-header">
+    <!-- <div class="block-header">
         <h2><a class="btn bg-blue waves-effect target-link" href="{{url(Request::segment(1).'#data-akademik/nama-semester/add')}}"><i class="material-icons">note_add</i><span>Tambah Nama Semester</span></a></h2>
-    </div>
+    </div> -->
     <div class="row clearfix">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class="card">
@@ -20,7 +20,7 @@
                                         <th>Tahun Akademik</th>
                                         <th>Kode Semeester</th>
                                         <th>Status Aktif</th>
-                                        <th>Action</th>
+                                        <!-- <th>Action</th> -->
                                     </tr>
                                 </thead>
                             </table>
@@ -41,7 +41,7 @@
     var primary_table = $('#primary_table').DataTable({
         processing: true,
         serverSide: true,
-responsive: true,
+        responsive: true,
         ajax: {
             url: datatable_url,
             type: 'GET'
@@ -53,16 +53,16 @@ responsive: true,
             { data: 'thn_akademik_semester', name: 'thn_akademik_semester' },
             { data: 'kode_semester', name: 'kode_semester' },
             { data: 'status_aktif', name: 'status_aktif'},
-            { data: 'action', name: 'action', searchable: false, orderable: false,
-                render: function(data){
-                    return '<a class="target-link btn btn-info btn-circle waves-effect waves-circle waves-float" href="'+ edit_url + '/' + data.id +'">'+
-                    '    <i class="material-icons">edit</i>'+
-                    '</a> '+
-                    '<button class="btn btn-danger btn-circle waves-effect waves-circle waves-float" onclick="deleteAction(\''+ delete_url +'\', this)" data-id="'+  data.id +'">'+
-                    '    <i class="material-icons">delete_forever</i>'+
-                    '</button>';
-                }
-            }
+            // { data: 'action', name: 'action', searchable: false, orderable: false,
+            //     render: function(data){
+            //         return '<a class="target-link btn btn-info btn-circle waves-effect waves-circle waves-float" href="'+ edit_url + '/' + data.id +'">'+
+            //         '    <i class="material-icons">edit</i>'+
+            //         '</a> '+
+            //         '<button class="btn btn-danger btn-circle waves-effect waves-circle waves-float" onclick="deleteAction(\''+ delete_url +'\', this)" data-id="'+  data.id +'">'+
+            //         '    <i class="material-icons">delete_forever</i>'+
+            //         '</button>';
+            //     }
+            // }
         ]
     });
 
