3ca9b6af50d4be45adafe56b23be96d799355f12
Rio Ramadhan <ramadhanar.rio@gmail.com>
2020-07-09 14:43:42 +0700

    migrate api_token longtext

--- app/Http/Controllers/Apiv1Controller.php
+++ app/Http/Controllers/Apiv1Controller.php
@@ -1290,7 +1290,7 @@ class Apiv1Controller extends BaseController
                 return response()->json([
                     'status_code' 	=> 300,
                     'status_text' 	=> 'Failed',
-                    'message' => 'Terdapat error'
+                    'message' => 'Terdapat error '.$e->getMessage()
                 ]);
             }
         }
