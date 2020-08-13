23312cdc152d9883a8d79ab57bd8179a2ea3d017
Rio Ramadhan <ramadhanar.rio@gmail.com>
2020-08-13 13:44:51 +0700

    rename migration file create kelas mp grup

--- database/migrations/2020_07_27_122924_create_kelas_mp_grup_table_new.php
+++ database/migrations/2020_07_27_122924_create_kelas_mp_grup_table_new.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateKelasMpGrupTable extends Migration
+class CreateKelasMpGrupTableNew extends Migration
 {
     /**
      * Run the migrations.
