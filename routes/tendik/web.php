e17027dd26562f1ada05bf61e6ff1d69103cfb24
Rio Ramadhan <ramadhanar.rio@gmail.com>
2020-09-01 12:51:49 +0700

    refixing tendik download rekap kesehatan

--- routes/tendik/web.php
+++ routes/tendik/web.php
@@ -41,6 +41,7 @@ Route::group(array('middleware'=> ['token_staff']), function () {
             Route::get('rekap-kesehatan', 'Guru\GuruPiket\RekapKesehatanController@viewRekapKesehatan');
             Route::get('rekap-kesehatan/{id}', 'Guru\GuruPiket\RekapKesehatanController@viewDetailRekapKesehatan');
             Route::get('rekap-kesehatan/{id}/{bulan}', 'Guru\GuruPiket\RekapKesehatanController@viewDetailRekapKesehatan');
+            Route::get('rekap-kesehatan/{id}/{bulan}/download', 'Guru\GuruPiket\RekapKesehatanController@downloadDetailRekapKesehatan');
             
             Route::get('rekap-kesehatan/user/{id}/{date}', 'Guru\WaliKelas\RekapKesehatanController@viewRekapKesehatanSiswa');
             Route::get('rekap-kesehatan/detail/form/{id}', 'Tendik\KegiatanHarian\FormKesehatanController@viewDetailFormKesehatan');
