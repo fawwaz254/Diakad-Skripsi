92d00ca80448b8cc5794bb6f2aa967ce6c392a20
Rio Ramadhan <ramadhanar.rio@gmail.com>
2020-09-03 11:40:49 +0700

    refixing id log kelas siswa in upload data siswa

--- app/Http/Controllers/Pendidikan/Siswa/UploadDataSiswaController.php
+++ app/Http/Controllers/Pendidikan/Siswa/UploadDataSiswaController.php
@@ -149,6 +149,7 @@ class UploadDataSiswaController extends BaseController
 	                		$id_jalur_siswa 	= $input->auth_data->sekolah_data->prefix.strtotime($now).uniqid();
 	                		$id_log_kelas_siswa = $input->auth_data->sekolah_data->prefix.strtotime($now).uniqid();
 	                		$arr[]= array(
+	                				'id_log_kelas_siswa'=> $id_log_kelas_siswa,
 	                				'id_siswa' 			=> $id_siswa,
 	                				'id_pengguna' 		=> $id_pengguna,
 	                				'id_c_siswa' 		=> $id_c_siswa,
