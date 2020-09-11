c01afa5f1e59b5032e4c1c00553e8913130ccbd9
Rio Ramadhan <ramadhanar.rio@gmail.com>
2020-09-11 15:44:54 +0700

    adding menu rekap absen tanpa jadwal

---
+++ database/migrations/2020_09_11_154105_adding_menu_rekap_absensi_tanpa_jadwal.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use App\Models\Modul;
+use App\Models\Menu;
+
+class AddingMenuRekapAbsensiTanpaJadwal extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $menu               = new Menu;
+        $menu->id_modul     = 10;
+        $menu->nm_menu      = "Rekap Absen Tanpa Jadwal";
+        $menu->page         = "rekap-absen-tanpa-jadwal";
+        $menu->urutan       = 4;
+        $menu->akses        = 1;
+        $menu->save();
+        
+        $menu = Menu::where('id_modul', 10)->where('nm_menu', 'Rekap Absen')->first();
+        $menu->urutan       = 2;
+        $menu->save();
+        
+        $menu = Menu::where('id_modul', 10)->where('nm_menu', 'Absensi Tanpa Jadwal')->first();
+        $menu->urutan       = 3;
+        $menu->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
