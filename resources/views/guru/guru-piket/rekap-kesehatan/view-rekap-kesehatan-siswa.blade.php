b1903f563bbe783506ee3bf9d660999da48102f5
Rio Ramadhan <ramadhanar.rio@gmail.com>
2020-09-16 10:00:25 +0700

    push fixedcolumn

--- resources/views/guru/guru-piket/rekap-kesehatan/view-rekap-kesehatan-siswa.blade.php
+++ resources/views/guru/guru-piket/rekap-kesehatan/view-rekap-kesehatan-siswa.blade.php
@@ -47,89 +47,91 @@
                     </h2>
                 </div>
                 <div class="body">
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-striped table-hover dataTable display responsive nowrap" style="overflow-x:auto;" id="primary_table">
-                            <thead>
-                                <tr>
-                                    <th rowspan="2">No. </th>
-                                    <th rowspan="2">NIS</th>
-                                    <th rowspan="2">NISN</th>
-                                    <th rowspan="2">Nama</th>
-                                    <th colspan="{{$dates->count()}}">Tanggal</th>
-                                </tr>
-                                <tr>
-                                    @foreach($dates as $date)
-                                    <th>
-                                        {{substr($date->format('l'), 0, 3)}}
-                                        <br>
-                                        {{$date->format('d')}}
-                                    </th>
+                    <table class="table table-bordered table-striped table-hover dataTable" id="primary_table">
+                        <thead>
+                            <tr>
+                                <th rowspan="2">No. </th>
+                                <th rowspan="2">NIS</th>
+                                <th rowspan="2">Nama</th>
+                                <th colspan="{{$dates->count()}}">Tanggal</th>
+                            </tr>
+                            <tr>
+                                @foreach($dates as $date)
+                                <th>
+                                    {{substr($date->format('l'), 0, 3)}}
+                                    <br>
+                                    {{$date->format('d')}}
+                                </th>
 
-                                    @php
-                                        $total_pengisi[$date->format('d')] = 0;
-                                        $total_normal[$date->format('d')] = 0;
-                                        $total_warning[$date->format('d')] = 0;
-                                    @endphp
-                                    @endforeach
-                                </tr>
-                            </thead>
-                            <tbody>
                                 @php
-                                    $no = 1;
+                                    $total_pengisi[$date->format('d')] = 0;
+                                    $total_normal[$date->format('d')] = 0;
+                                    $total_warning[$date->format('d')] = 0;
                                 @endphp
-                                @foreach($data_siswa as $siswa)
-                                <tr>
-                                    <td>{{$no++}}</td>
-                                    <td>{{$siswa->nis_siswa}}</td>
-                                    <td>{{$siswa->nisn_siswa}}</td>
-                                    <td>{{$siswa->nm_pengguna}}</td>
-                                    @foreach($dates as $date)
-                                        @php
-                                            $all_pengisian = $data_pengisian->where('tgl_pengisian', $date->format('Y-m-d'))->where('id_pengguna_pengisi', $siswa->id_pengguna)->sortByDesc('status_pengisian')->all();
-                                        @endphp
-                                        @if(count($all_pengisian) > 0)
-                                        <td style="background: #{{collect($all_pengisian)->first()->warna_keadaan}}; text-align:center; vertical-align:middle !important;">
-                                            <b><a class="target-link" href="{{url(Request::segment(1).'#'.Request::segment(2).'/'.Request::segment(3).'/user/'.$siswa->id_pengguna.'/'.$date->format('Y-m-d'))}}">{{count($all_pengisian)}}x</a></b>
-                                        </td>
-                                            @php
-                                                $total_pengisi[$date->format('d')]++;
-                                            @endphp
-                                            @if(collect($all_pengisian)->first()->status_pengisian == 1)
-                                                @php
-                                                    $total_normal[$date->format('d')]++;
-                                                @endphp
-                                            @elseif(collect($all_pengisian)->first()->status_pengisian == 2)
-                                                @php
-                                                    $total_warning[$date->format('d')]++;
-                                                @endphp
-                                            @endif
-                                        @else
-                                        <td></td>
-                                        @endif
-                                    @endforeach
-                                </tr>
                                 @endforeach
-                                <tr>
-                                    <td colspan=4>Total Normal</td>
-                                    @foreach($dates as $date)
-                                    <td>{{$total_normal[$date->format('d')]}}</td>
-                                    @endforeach
-                                </tr>
-                                <tr>
-                                    <td colspan=4>Total Warning</td>
-                                    @foreach($dates as $date)
-                                    <td>{{$total_warning[$date->format('d')]}}</td>
-                                    @endforeach
-                                </tr>
-                                <tr>
-                                    <td colspan=4>Total pengisi</td>
-                                    @foreach($dates as $date)
-                                    <td>{{$total_pengisi[$date->format('d')]}}</td>
-                                    @endforeach
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php
+                                $no = 1;
+                            @endphp
+                            @foreach($data_siswa as $siswa)
+                            <tr>
+                                <td>{{$no++}}</td>
+                                <td>{{$siswa->nis_siswa}}</td>
+                                <td>{{$siswa->nm_pengguna}}</td>
+                                @foreach($dates as $date)
+                                    @php
+                                        $all_pengisian = $data_pengisian->where('tgl_pengisian', $date->format('Y-m-d'))->where('id_pengguna_pengisi', $siswa->id_pengguna)->sortByDesc('status_pengisian')->all();
+                                    @endphp
+                                    @if(count($all_pengisian) > 0)
+                                    <td style="background: #{{collect($all_pengisian)->first()->warna_keadaan}}; text-align:center; vertical-align:middle !important;">
+                                        <b><a class="target-link" href="{{url(Request::segment(1).'#'.Request::segment(2).'/'.Request::segment(3).'/user/'.$siswa->id_pengguna.'/'.$date->format('Y-m-d'))}}">{{count($all_pengisian)}}x</a></b>
+                                    </td>
+                                        @php
+                                            $total_pengisi[$date->format('d')]++;
+                                        @endphp
+                                        @if(collect($all_pengisian)->first()->status_pengisian == 1)
+                                            @php
+                                                $total_normal[$date->format('d')]++;
+                                            @endphp
+                                        @elseif(collect($all_pengisian)->first()->status_pengisian == 2)
+                                            @php
+                                                $total_warning[$date->format('d')]++;
+                                            @endphp
+                                        @endif
+                                    @else
+                                    <td></td>
+                                    @endif
+                                @endforeach
+                            </tr>
+                            @endforeach
+                            <tr>
+                                <td>Total Normal</td>
+                                <td></td>
+                                <td></td>
+                                @foreach($dates as $date)
+                                <td>{{$total_normal[$date->format('d')]}}</td>
+                                @endforeach
+                            </tr>
+                            <tr>
+                                <td>Total Warning</td>
+                                <td></td>
+                                <td></td>
+                                @foreach($dates as $date)
+                                <td>{{$total_warning[$date->format('d')]}}</td>
+                                @endforeach
+                            </tr>
+                            <tr>
+                                <td>Total pengisi</td>
+                                <td></td>
+                                <td></td>
+                                @foreach($dates as $date)
+                                <td>{{$total_pengisi[$date->format('d')]}}</td>
+                                @endforeach
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -140,4 +142,14 @@
 function filterAction(){
     loadURI('{{Request::segment(2)}}/{{Request::segment(3)}}/{{$data_kelas->id_kelas}}/' + $('select[name=id_bulan]').val());
 }
+
+var primary_table = $('#primary_table').DataTable({
+    ordering: false,
+    scrollX: true,
+    fixedColumns:   {
+        leftColumns: 3
+    },
+    scrollCollapse: true,
+    paging: false
+});
 </script>
