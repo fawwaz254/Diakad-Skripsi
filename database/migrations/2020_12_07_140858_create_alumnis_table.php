747bfdca7dd90575c1c464b1d8d69d180d776368
Achmad Ali B <achmadali.baidlowi@gmail.com>
2020-12-08 10:45:21 +0700

    add new column

--- database/migrations/2020_12_07_140858_create_alumnis_table.php
+++ database/migrations/2020_12_07_140858_create_alumnis_table.php
@@ -17,6 +17,8 @@ class CreateAlumnisTable extends Migration
             $table->engine = 'InnoDB';
             $table->string('id_alumni', 40)->primary();
             $table->string('id_siswa', 40)->comment('FK: siswa.id_siswa');
+            $table->string('email');
+            $table->string('nomor_telepon');
             $table->integer('tahun_lulus');
             $table->string('status', 10);
             $table->string('created_by', 40)->nullable();
