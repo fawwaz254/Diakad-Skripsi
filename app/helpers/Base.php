db1fa87ce1a9b675aef9591fdb8c9a289441603f
Achmad Ali B <achmadali.baidlowi@gmail.com>
2020-12-17 12:53:45 +0700

    create base helper file

---
+++ app/helpers/Base.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Session;
+
+/**
+ * create auth_data helper if doesnt exists
+ * @return auth_data
+ */
+if (!function_exists('auth_data')) {
+    function auth_data()
+    {
+      return Session::get('auth_data');
+    }
+}
+
+/**
+ * create genereate_id if doesnt exists
+ * @return String
+ */
+if (!function_exists('generate_id')) {
+  function generate_id()
+  {
+    $now  = Carbon::now(env('APP_TIMEZONE', '')); 
+    $id   = auth_data()->sekolah_data->prefix.strtotime($now).uniqid();
+    
+    return $id;
+  }
+}
+
+/**
+ * create get_moduls if doesnt exists
+ * @return array
+ */
+if (!function_exists('get_moduls')) {
+  function get_moduls()
+  {    
+    return auth_data()->moduls;
+  }
+}
