a52a611d22a8b113960fd8a4541132a091ede623
Rio Ramadhan <ramadhanar.rio@gmail.com>
2020-12-17 09:58:15 +0700

    feature-fix: move magang siswa from role pendidikan to role humas

--- routes/humas/web.php
+++ routes/humas/web.php
@@ -55,6 +55,69 @@ Route::group(array('middleware'=> ['token_staff']), function() {
 			Route::post('rekap-kesehatan/datatables', 'Tendik\KegiatanHarian\FormKesehatanController@showDatatablesFormKesehatan');
 		});
 
+		/** === MODUL MAGANG SISWA === **/
+		Route::group(array('prefix' => 'magang-siswa'), function() {
+			// MENU Nama Magang
+			Route::get('nama-magang', 'Humas\MagangSiswa\MagangSiswaController@viewMagangSiswa');
+			Route::get('nama-magang/datatables', 'Humas\MagangSiswa\MagangSiswaController@datatablesMagangSiswa');
+			Route::get('nama-magang/add', 'Humas\MagangSiswa\MagangSiswaController@addMagangSiswa');
+			Route::get('nama-magang/edit/{id}', 'Humas\MagangSiswa\MagangSiswaController@editMagangSiswa');
+
+			Route::post('action-nama-magang/{mode}/{id}', 'Humas\MagangSiswa\MagangSiswaController@actionMagang');
+
+			//MENU Periode Magang
+			Route::get('periode-magang', 'Humas\MagangSiswa\PeriodeMagangController@viewPeriodeMagang');
+			Route::get('periode-magang/datatables', 'Humas\MagangSiswa\PeriodeMagangController@datatablesPeriodeMagang');
+			Route::get('periode-magang/add', 'Humas\MagangSiswa\PeriodeMagangController@addPeriodeMagang');
+			Route::get('periode-magang/edit/{id}', 'Humas\MagangSiswa\PeriodeMagangController@editPeriodeMagang');
+
+			Route::post('action-periode-magang/{mode}/{id}', 'Humas\MagangSiswa\PeriodeMagangController@actionPeriodeMagang');
+
+			//MENU Rekanan Magang
+			Route::get('rekanan-magang', 'Humas\MagangSiswa\RekananMagangController@viewRekananMagang');
+			Route::get('rekanan-magang/datatables', 'Humas\MagangSiswa\RekananMagangController@datatablesRekananMagang');
+			Route::get('rekanan-magang/add', 'Humas\MagangSiswa\RekananMagangController@addRekananMagang');
+			Route::get('rekanan-magang/edit/{id}', 'Humas\MagangSiswa\RekananMagangController@editRekananMagang');
+
+			Route::post('action-rekanan-magang/{mode}/{id}', 'Humas\MagangSiswa\RekananMagangController@actionRekananMagang');
+
+			//MENU Pengajuan Siswa Magang
+			Route::get('pengajuan-siswa-magang', 'Humas\MagangSiswa\PengajuanSiswaMagangController@viewPengajuanSiswaMagang');
+			Route::post('post-view-pengajuan-magang', 'Humas\MagangSiswa\PengajuanSiswaMagangController@actionViewDetailPengajuanMagang');
+			Route::get('pengajuan-siswa-magang/view-detail/{id_periode_magang}/{id_rekanan_magang}/{nis_nama_siswa}', 'Humas\MagangSiswa\PengajuanSiswaMagangController@viewDetailPengajuanMagang');
+			Route::get('pengajuan-siswa-magang/datatables/{id_periode_magang}/{id_rekanan_magang}/{nis_nama_siswa}', 'Humas\MagangSiswa\PengajuanSiswaMagangController@datatablesPengajuanMagang');
+			Route::get('pengajuan-siswa-magang/cancel/{id}', 'Humas\MagangSiswa\PengajuanSiswaMagangController@cancelPengajuanMagang');
+
+			Route::post('action-pengajuan-siswa-magang/{mode}/{id}/{id_siswa}/{id_periode_magang}/{id_rekanan_magang}', 'Humas\MagangSiswa\PengajuanSiswaMagangController@actionPengajuanMagang');
+
+			//MENU Approve Siswa Magang
+			Route::get('approve-siswa-magang', 'Humas\MagangSiswa\ApproveSiswaMagangController@viewApproveSiswaMagang');
+			Route::post('post-view-approve-siswa-magang', 'Humas\MagangSiswa\ApproveSiswaMagangController@actionViewDetailApproveSiswaMagang');
+			Route::get('approve-siswa-magang/view-detail/{id_periode_magang}/{id_rekanan_magang}/{nis_nama_siswa}', 'Humas\MagangSiswa\ApproveSiswaMagangController@viewDetailApproveSiswaMagang');
+			Route::get('approve-siswa-magang/datatables/{id_periode_magang}/{id_rekanan_magang}/{nis_nama_siswa}', 'Humas\MagangSiswa\ApproveSiswaMagangController@datatablesApproveSiswaMagang');
+
+			Route::post('action-approve-siswa-magang/{mode}/{id}/{id_siswa}/{id_periode_magang/{id_rekanan_magang}', 'Humas\MagangSiswa\ApproveSiswaMagangController@actionApproveSiswaMagang');
+
+
+			//MENU Komponen Nilai Magang
+			Route::get('komponen-nilai-magang', 'Humas\MagangSiswa\KomponenNilaiMagangController@viewKomponenNilaiMagang');
+			Route::post('post-view-komponen-nilai-magang', 'Humas\MagangSiswa\KomponenNilaiMagangController@actionViewKelasKomponenNilaiMagang');
+			Route::get('komponen-nilai-magang/view-periode/{id_periode_magang}', 'Humas\MagangSiswa\KomponenNilaiMagangController@viewKelasKomponenNilaiMagang');
+			Route::get('komponen-nilai-magang/datatables/{id_periode_magang}', 'Humas\MagangSiswa\KomponenNilaiMagangController@datatablesKomponenNilaiMagang');
+			Route::get('komponen-nilai-magang/add/{id_periode_magang}', 'Humas\MagangSiswa\KomponenNilaiMagangController@addKomponenNilai');
+			Route::get('komponen-nilai-magang/edit/{id_periode_magang}/{id}', 'Humas\MagangSiswa\KomponenNilaiMagangController@editKomponenNilai');
+
+			Route::post('action-komponen-nilai-magang/{mode}/{id}', 'Humas\MagangSiswa\KomponenNilaiMagangController@actionKomponenNilaiMagang');
+
+			//MENU Input Nilai
+			Route::get('input-nilai-magang', 'Humas\MagangSiswa\InputNilaiMagangController@viewPeriodeMagang');
+			Route::post('post-view-input-nilai-magang', 'Humas\MagangSiswa\InputNilaiMagangController@actionViewKomponenInputNilaiMagang');
+			Route::get('input-nilai-magang/view-komponen/{id_periode_magang}', 'Humas\MagangSiswa\InputNilaiMagangController@viewKomponenInputNilaiMagang');
+			Route::get('input-nilai-magang/datatables/{id_periode_magang}', 'Humas\MagangSiswa\InputNilaiMagangController@datatablesKomponenNilaiMagang');
+
+			Route::post('action-input-nilai-magang/{mode}/{id}', 'Humas\MagangSiswa\InputNilaiMagangController@actionInputNilaiMagang');
+		});
+
 		Route::namespace('Humas\Alumni')->prefix('alumni')->group(function() {
 			Route::get('/', 'AlumniController@index');
 			Route::get('/add', 'AlumniController@create');
