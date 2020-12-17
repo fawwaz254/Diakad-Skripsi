217051afa1ca375c0c774ba92d9a164977ef5a45
Rio Ramadhan <ramadhanar.rio@gmail.com>
2020-12-17 15:00:26 +0700

    feature-fix: laporan pembayaran siswa tahunan

--- app/Http/Controllers/Keuangan/LaporanKeuangan/PembayaranSiswaTahunanController.php
+++ app/Http/Controllers/Keuangan/LaporanKeuangan/PembayaranSiswaTahunanController.php
@@ -23,7 +23,7 @@ class PembayaranSiswaTahunanController extends BaseController
     {
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
-        $list_data = PembayaranBiaya::with('tagihan_biaya.siswa.kelas.jurusan');
+        $list_data = PembayaranBiaya::with('tagihan_biaya.kelas.jurusan');
 
         if ($year !== null) {
             $list_data = $list_data->whereYear('tgl_pembayaran', $year);
@@ -34,8 +34,8 @@ class PembayaranSiswaTahunanController extends BaseController
         foreach ($temp_list_data as $key => $value){
             $list_data[] = [
                 'jumlah_pembayaran' => $value->besar_pembayaran,
-                'kelas' => $value->tagihan_biaya->siswa->kelas->tingkat,
-                'jurusan' => $value->tagihan_biaya->siswa->kelas->jurusan->nm_jurusan,
+                'kelas' => $value->tagihan_biaya->kelas->tingkat,
+                'jurusan' => $value->tagihan_biaya->kelas->jurusan->nm_jurusan,
                 'tgl_bayar' => $value->tgl_pembayaran
             ];
         }
@@ -53,34 +53,4 @@ class PembayaranSiswaTahunanController extends BaseController
         ];
         return response()->json($data); 
     }
-
-    // public function datatablesPembayaranSiswa(Request $request)
-    // {
-    //     $input = (object) $request->input();
-    //     $auth_data = $input->auth_data;
-    //     $list_data = PembayaranBiaya::with('tagihan_biaya', 'tagihan_biaya.siswa', 'tagihan_biaya.siswa.pengguna')->with(['tagihan_biaya.detail_biaya' => function ($q) {
-    //         $q->with('biaya');
-    //     }]);
-
-    //     if (!empty($input->start_date) && !empty($input->end_date)) {
-    //         $list_data = $list_data->whereBetween('tgl_pembayaran', [$input->start_date.' 00:00:00', $input->end_date.' 23:59:59']);
-    //     }
-
-    //     $temp_list_data = $list_data->get();
-    //     $total = $temp_list_data->sum('besar_pembayaran');
-
-    //     return Datatables::of($list_data)
-    //             ->addColumn('tanggal_bayar', function ($item) {
-    //                 return date_format(date_create($item->tgl_pembayaran), "d M Y H:i").' WIB';
-    //             })
-    //             ->addColumn('keterangan_bayar', function ($item) {
-    //                 if(!empty($item->tagihan_biaya->detail_biaya->id_bulan)){
-    //                     return $item->tagihan_biaya->detail_biaya->biaya->nm_biaya.' bulan '.Carbon::createFromFormat('m', $item->tagihan_biaya->detail_biaya->id_bulan)->format('F');
-    //                 }else{
-    //                     return $item->tagihan_biaya->detail_biaya->biaya->nm_biaya;
-    //                 }
-    //             })
-    //             ->with('total', number_format($total))
-    //             ->make(true);
-    // }
 }
