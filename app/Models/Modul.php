91284a58625af5dadbfd2666cc8e5b8f3f6098f3
Achmad Ali B <achmadali.baidlowi@gmail.com>
2020-12-18 14:31:30 +0700

    add menus relation on modul Model

--- app/Models/Modul.php
+++ app/Models/Modul.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Menu;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -35,7 +36,10 @@ class Modul extends Model
     protected $guarded = [];
 
 
-
+    public function menus()
+    {
+        return $this->hasMany(Menu::class, 'id_modul')->where('akses', 1)->orderBy('urutan', 'asc');
+    }
 
 
 
