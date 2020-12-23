0d5621c36d34298f45e551f8efb65d8917041a6e
Achmad Ali B <achmadali.baidlowi@gmail.com>
2020-12-23 09:32:18 +0700

    create berkas kerjasama model and migration

---
+++ app/Models/BerkasKerjasama.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Kerjasama;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BerkasKerjasama extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'berkas_kerjasama';
+    protected $primaryKey = "id_berkas_kerjasama";
+    public $incrementing    = false;
+    public $timestamps      = true;
+
+    protected $fillable = [
+        'id_kerjasama',
+        'nama_file',
+        'type',
+        'path',
+        'versi',
+        'created_by',
+        'updated_by',
+        'deleted_by'
+    ];
+
+    public function kejasama()
+    {
+        return $this->hasMany(Kerjasama::class, 'id_instansi');
+    }
+
+}
