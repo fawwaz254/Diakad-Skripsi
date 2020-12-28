2a9c10b7c1a52dd251501dad1dc5a3a4ec0f1479
Achmad Ali B <achmadali.baidlowi@gmail.com>
2020-12-28 11:34:26 +0700

    implement baseModelTrait on BerkasKerjasama

--- app/Models/BerkasKerjasama.php
+++ app/Models/BerkasKerjasama.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use App\Models\Kerjasama;
+use App\Traits\BaseModelTraits;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BerkasKerjasama extends Model
 {
     use SoftDeletes;
+    use BaseModelTraits;
 
     protected $table = 'berkas_kerjasama';
     protected $primaryKey = "id_berkas_kerjasama";
