7048b5f89bff0c7c77f68d102af7474a8fb43df8
Achmad Ali B <achmadali.baidlowi@gmail.com>
2020-12-29 10:51:58 +0700

    create new function to handle operation response

--- app/helpers/Base.php
+++ app/helpers/Base.php
@@ -4,22 +4,23 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Storage;
 
-/**
- * create auth_data helper if doesnt exists
- * @return auth_data
- */
 if (!function_exists('auth_data')) {
+    /**
+     * get auth data from session
+     * @return auth_data
+     */
     function auth_data()
     {
       return Session::get('auth_data');
     }
 }
 
-/**
- * create genereate_id if doesnt exists
- * @return String
- */
+
 if (!function_exists('generate_id')) {
+  /**
+   * generate custom uuid by school prefix
+   * @return String
+   */
   function generate_id()
   {
     $now  = Carbon::now(env('APP_TIMEZONE', '')); 
@@ -29,26 +30,26 @@ if (!function_exists('generate_id')) {
   }
 }
 
-/**
- * create get_moduls if doesnt exists
- * @return array
- */
 if (!function_exists('get_moduls')) {
+  /**
+   * get modules that authorized to active user
+   * @return array
+   */
   function get_moduls()
   {    
     return auth_data()->moduls;
   }
 }
 
-/**
- * create storeFileToCloud if doesnt exists, 
- * @param string $path,
- * @param string $id
- * @param File $file
- * @param string $access
- * @return string
- */
 if (!function_exists('storeFileToCloud')) {
+  /**
+   * handle store file to cloud server 
+   * @param string $path,
+   * @param string $id
+   * @param File $file
+   * @param string $access
+   * @return string
+   */
   function storeFileToCloud($path, $id, $file, $access = 'public')
   {    
     $prefix = auth_data()->sekolah_data->nm_prefix;
@@ -56,14 +57,49 @@ if (!function_exists('storeFileToCloud')) {
   }
 }
 
-/**
- * create removeFileFromCloud if doesnt exists, 
- * @param string $filename,
- * @return any
- */
 if (!function_exists('removeFileFromCloud')) {
+  /**
+   * handle remove file resource from cloud server by filename 
+   * @param string $filename,
+   * @return any
+   */
   function removeFileFromCloud($filename)
   {
     return Storage::disk('spaces')->delete($filename);
+  }
+}
+
+
+if (!function_exists('web_response')) {
+  /**
+   * return web response of web operation
+   * @param string $path,
+   * @param string $message,
+   * @param int $status,
+   * @return array
+   */
+  function web_response($path = null, $message = "Ok", $status = 202)
+  {
+    return [
+      'status' => $status,
+      'path' => $path,
+      'message' => $message
+    ];
+  }
+}
+
+if (!function_exists('error_response')) {
+  /**
+   * return error response of web operation
+   * @param string $message,
+   * @param int $status,
+   * @return array
+   */
+  function error_response($message, $status = 300)
+  {
+    return [
+      'status' => $status,
+      'message' => $message
+    ];
   }
 }
