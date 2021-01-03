29f23638c41cde8d9ab6d74754dbc38e4cb75b34
Rio Ramadhan <ramadhanar.rio@gmail.com>
2021-01-03 17:14:18 +0700

    fix: welcome page kesiswaan, and profile

--- app/Http/Controllers/Kesiswaan/WelcomeController.php
+++ app/Http/Controllers/Kesiswaan/WelcomeController.php
@@ -7,6 +7,9 @@ use Illuminate\Routing\Controller as BaseController;
 
 use Yajra\Datatables\Datatables;
 
+use App\Models\Kelas;
+use App\Models\Siswa;
+
 use Auth;
 use DB;
 use Session;
@@ -15,7 +18,13 @@ class WelcomeController extends BaseController{
     public function indexWelcome(Request $request){
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
-        return view('kesiswaan/welcome', compact('auth_data'));
+
+        $data_tingkat = Kelas::select('tingkat')->distinct()->orderBy('tingkat', 'asc')->get();
+        $count_siswa = Siswa::whereNotNull('id_kelas')->count();
+
+        $last_siswa = Siswa::select('created_at')->orderBy('created_at', 'desc')->first();
+
+        return view('kesiswaan/welcome', compact('auth_data', 'data_tingkat', 'count_siswa', 'last_siswa'));
     }
 
 }
