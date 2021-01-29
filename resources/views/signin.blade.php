b0ffc0fca4d7e9c2726e8103f38df32d248fdddb
Rio Ramadhan <ramadhanar.rio@gmail.com>
2021-01-29 13:42:51 +0700

    fix: restore to original

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -18,8 +18,7 @@
                             {{csrf_field()}}
                             <div class="msg" style="font-size:1.5em;line-height:50px">
                                 <img class="hidden-md hidden-lg" src="https://diakad.sgp1.cdn.digitaloceanspaces.com/x-image-resources/{{env('APP_NAME', 'diakad')}}-logo.png" alt="Logo Diakad" style="height: 90px;" />
-                                <!-- <img src="https://diakad.sgp1.digitaloceanspaces.com/{{$sekolah->nm_singkat_sekolah}}/global/logo-sekolah" alt="Logo Sekolah" style="height:90px;" /> -->
-                                <img src="{{asset('media/muhammadiyah.jpg')}}" alt="Logo Sekolah" style="height:90px;" />
+                                <img src="https://diakad.sgp1.digitaloceanspaces.com/{{$sekolah->nm_singkat_sekolah}}/global/logo-sekolah" alt="Logo Sekolah" style="height:90px;" />
                                 <br>
                                 <strong>{{$sekolah->nm_sekolah}}</strong>
                             </div>
