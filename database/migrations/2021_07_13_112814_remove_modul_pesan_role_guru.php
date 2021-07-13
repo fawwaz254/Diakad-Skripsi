c187fcad5acac3dd38a507af05b1208f622d3a18
siffiyan1 <siffiyanassauri1@gmail.com>
2021-07-13 11:34:23 +0700

    add migration remove some menu role guru

---
+++ database/migrations/2021_07_13_112814_remove_modul_pesan_role_guru.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use App\Models\Role;
+use App\Models\Modul;
+use App\Models\Menu;
+use Carbon\Carbon;
+
+class RemoveModulPesanRoleGuru extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $now = Carbon::now(env('APP_TIMEZONE', ''));
+        $role_id = Role::where('nm_role', 'Guru')->first()->id_role;
+
+        // remove menu pesan masuk, pesan keluar, pesan dihapus modul pesan
+
+        $modul = Modul::where('id_role', $role_id)->where('nm_modul', 'Pesan')->first();
+
+        $menu = Menu::where('id_modul',$modul->id_modul)->where('nm_menu','Pesan Masuk')->first();
+        $menu->deleted_at = $now;
+        $menu->deleted_by = 'A8bT515358553655b8b4b05a6d86';
+        $menu->save();
+        $menu->delete();
+
+        $menu = Menu::where('id_modul',$modul->id_modul)->where('nm_menu','Pesan Keluar')->first();
+        $menu->deleted_at = $now;
+        $menu->deleted_by = 'A8bT515358553655b8b4b05a6d86';
+        $menu->save();
+        $menu->delete();
+
+        $menu = Menu::where('id_modul',$modul->id_modul)->where('nm_menu','Pesan Dihapus')->first();
+        $menu->deleted_at = $now;
+        $menu->deleted_by = 'A8bT515358553655b8b4b05a6d86';
+        $menu->save();
+        $menu->delete();
+
+        $modul->deleted_at = $now;
+        $modul->deleted_by = 'A8bT515358553655b8b4b05a6d86';
+        $modul->save();
+        $modul->delete();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
