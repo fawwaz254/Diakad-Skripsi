1a82197e3e574b165724069924af0611b8a0ae63
siffiyan1 <siffiyanassauri1@gmail.com>
2021-08-12 09:18:06 +0700

    rilis-note 9 agustus 2021

--- resources/views/rilis-note.blade.php
+++ resources/views/rilis-note.blade.php
@@ -1,11 +1,24 @@
 @php
 
-$data = json_decode(file_get_contents('rilis.json'),true);
-$data = $data['rilis-terbaru'];
+$data = [];
+
+$siswa = json_decode(file_get_contents('rilis/siswa.json'),true);
+$akademik = json_decode(file_get_contents('rilis/akademik.json'),true);
+$kesiswaan = json_decode(file_get_contents('rilis/kesiswaan.json'),true);
+$pendidikan = json_decode(file_get_contents('rilis/pendidikan.json'),true);
+$guru = json_decode(file_get_contents('rilis/guru.json'),true);
+
+array_push($data,$siswa);
+array_push($data,$akademik);
+array_push($data,$kesiswaan);
+array_push($data,$pendidikan);
+array_push($data,$guru);
 
 $mine = array_filter($data, function ($var) {
     return ($var['role'] == Request::segment(1));
 });
+
+$mine = array_values($mine);
 
 @endphp
 
