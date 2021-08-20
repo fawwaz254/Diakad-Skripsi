28af738f3d2e865e1a5388efe320b66eca191a8f
siffiyan1 <siffiyanassauri1@gmail.com>
2021-08-20 09:21:20 +0700

    change identity

--- resources/views/topbar.blade.php
+++ resources/views/topbar.blade.php
@@ -75,7 +75,7 @@
             <div class="navbar-header">
                 <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
                 <a href="javascript:void(0);" class="bars" style="display: none;"></a>
-                <a class="navbar-brand"><?=str_replace('-', ' ', strtoupper($theme_name))?> <?=strtoupper(auth_data()->sekolah_data->nm_sekolah)?> - {{strtoupper(env('APP_NAME', 'diakad'))}} </a>
+                <a class="navbar-brand"><?=str_replace('-', ' ', strtoupper($theme_name))?> <?=strtoupper(auth_data()->sekolah_data->nm_sekolah)?> - {{strtoupper(env('APP_NAME', 'dsmart edu'))}} </a>
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
