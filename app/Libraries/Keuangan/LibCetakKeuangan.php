815f9c7ddf5044bd37db40df53a7317f37b8e79e
Rio Ramadhan <riordhn@gmail.com>
2021-10-01 14:04:18 +0700

    temp admisi not now

--- app/Libraries/Keuangan/LibCetakKeuangan.php
+++ app/Libraries/Keuangan/LibCetakKeuangan.php
@@ -72,9 +72,9 @@ class LibCetakKeuangan{
                             FROM siswa
                             JOIN kelas ON kelas.id_kelas = siswa.id_kelas
                                 AND kelas.deleted_at IS NULL
-                            JOIN admisi ON admisi.id_siswa = siswa.id_siswa
-                                AND admisi.id_semester = ?
-                                AND admisi.deleted_at IS NULL
+                            -- JOIN admisi ON admisi.id_siswa = siswa.id_siswa
+                            --     AND admisi.id_semester = ?
+                            --     AND admisi.deleted_at IS NULL
                             JOIN status_pengguna ON status_pengguna.id_status_pengguna = admisi.id_status_pengguna
                                 AND status_pengguna.aktif_status_pengguna = 1
                                 AND status_pengguna.deleted_at IS NULL
