5fb3ed30fc44ceb7986ae9639b1842f3cc66555d
siffiyan1 <siffiyanassauri1@gmail.com>
2021-10-09 12:53:24 +0700

    remove frekunsi cetak pembayaran hari ini

--- resources/views/keuangan/utility/pembayaran-siswa/print-pembayaran-siswa.blade.php
+++ resources/views/keuangan/utility/pembayaran-siswa/print-pembayaran-siswa.blade.php
@@ -86,7 +86,7 @@
                             <th>No.</th>
                             <th>Nama Biaya</th>
                             <th>Besar Pembayaran</th>
-                            <th>Frekuensi</th>
+                            <!-- <th>Frekuensi</th> -->
                             <th>Subtotal</th>
                         </tr>
                         @php
@@ -104,12 +104,12 @@
                             <td>{{$pembayaran_siswa->nm_biaya." ".$pembayaran_siswa->keterangan}}</td>
                             @endif
                             <td>{{"Rp".number_format($pembayaran_siswa->besar_pembayaran)}}</td>
-                            <td>1x</td>
+                            <!-- <td>1x</td> -->
                             <td>{{"Rp".number_format($pembayaran_siswa->besar_pembayaran)}}</td>
                         </tr>
                         @endforeach
                         <tr>
-                            <td colspan="4" align="center"><b>TOTAL</b></td>
+                            <td colspan="3 align="center"><b>TOTAL</b></td>
                             <td align="center"><b>{{"Rp".number_format($data_pembayaran_siswa->sum('besar_pembayaran'))}}</b></td>
                         </tr>
                     </table>
