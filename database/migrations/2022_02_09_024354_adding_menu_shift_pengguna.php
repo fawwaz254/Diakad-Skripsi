55f4fcf74441d07b30d54b10800ae0297c12b77b
syahrule <syahrule01z@gmail.com>
2022-02-09 03:00:24 +0700

    add migration add menu

--- database/migrations/2022_02_09_024354_adding_menu_shift_pengguna.php
+++ database/migrations/2022_02_09_024354_adding_menu_shift_pengguna.php
@@ -4,10 +4,11 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
+
 use App\Models\Menu;
 use App\Models\Modul;
 
-class AddingMenuManajemenHariLiburOnModulAbsensi extends Migration
+class AddingMenuShiftPengguna extends Migration
 {
     /**
      * Run the migrations.
@@ -20,11 +21,12 @@ class AddingMenuManajemenHariLiburOnModulAbsensi extends Migration
 
         $menu = new Menu;
         $menu->id_modul = $modul->id_modul;
-        $menu->nm_menu = 'Manajemen Hari Libur';
-        $menu->page = 'manajemen-hari-libur';
-        $menu->urutan = 2;
+        $menu->nm_menu = 'Shift Pengguna';
+        $menu->page = 'shift_pengguna';
+        $menu->urutan = 3;
         $menu->akses = 1;
         $menu->save();
+
     }
 
     /**
