8ae9c6663f1de015aafe17f00ea173ac0756c541
Alfinadtiya <alfinaditia02@gmail.com>
2022-03-07 00:35:27 +0700

    update manajemen hari libur

--- resources/views/humas/absensi/manajemen-hari-libur/add-manajemen-hari-libur.blade.php
+++ resources/views/humas/absensi/manajemen-hari-libur/add-manajemen-hari-libur.blade.php
@@ -5,51 +5,35 @@
         </h2>
     </div>
 
-    <form method="POST" id="add-form" action="{{url()->current()}}">
-    {{csrf_field()}}
-
-    <div class="row clearfix">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="card">
-                <div class="header">
-                    <h2>ADD MANAJEMEN HARI LIBUR</h2>
-                </div>
-
-                <div class="body">
-
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <h2 class="card-inside-title">Date</h2>
-                            <input type="date" name="date" class="form-control" required>
-                        </div>
+    <form method="POST" id="form-validation" action="{{ url()->current() }}">
+        {{ csrf_field() }}
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>ADD MANAJEMEN HARI LIBUR</h2>
                     </div>
-
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                            <h2 class="card-inside-title">Explanation</h2>
-                            <input type="text" name="explanation" class="form-control" required>
+                    <div class="body">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <h2 class="card-inside-title">Date</h2>
+                                <input type="date" name="date" class="form-control" required>
+                            </div>
                         </div>
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <h2 class="card-inside-title">Explanation</h2>
+                                <input type="text" name="explanation" class="form-control" required>
+                            </div>
+                        </div>
+                        <button id="btn-submit" class="btn bg-red waves-effect">
+                            <i class="material-icons">save</i>
+                            <span>Save</span>
+                        </button>
                     </div>
-
-                    <button id="btn-submit" class="btn bg-red waves-effect">
-                        <i class="material-icons">save</i>
-                        <span>Save</span>
-                    </button>
-
                 </div>
-
             </div>
         </div>
-    </div>
-
     </form>
-
 </div>
 @include('scriptjs')
-<script>
-$( "#add-form" ).submit(function() {
-    $('#btn-submit').attr("disabled", true);
-    $('#btn-submit i').text('autorenew')
-    $('#btn-submit span').text('Loading')
-});
-</script>
