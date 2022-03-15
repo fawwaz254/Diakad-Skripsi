089192935f0cb37b715368f61c6e8f08a920c153
Alfinadtiya <alfinaditia02@gmail.com>
2022-03-15 10:18:21 +0700

    fix css manajemen file

--- resources/views/sekretariat/manajemen-file/data-file/view-data-file-sub-category.blade.php
+++ resources/views/sekretariat/manajemen-file/data-file/view-data-file-sub-category.blade.php
@@ -26,7 +26,7 @@ $total_file = 0;
 <div class="container-fluid">
     <div class="block-header">
         <div class="row clearfix">
-            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1" id="add-file">
+            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1" style="margin-right:50px" id="add-file">
                 <h2>
                     <a class="btn bg-blue waves-effect target-link"
                         href="{{ url(Request::segment(1) . '#manajemen-file/data-file/add') }}"><i
