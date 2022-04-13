436674758ae215e1a5feb6567ba98f6187977631
BACHTIAR8987854 <amminudinbahtiar101@gmail.com>
2022-04-13 12:36:51 +0700

    report-pimpinan

--- resources/views/administrator/welcome.blade.php
+++ resources/views/administrator/welcome.blade.php
@@ -1,12 +1,29 @@
 @php
-    $today = Carbon\Carbon::today('Asia/Jakarta');
+$today = Carbon\Carbon::today('Asia/Jakarta');
 @endphp
 <div class="container-fluid">
-    <div class="block-header">
-        <h2>DASHBOARD | {{$today->format('d M Y')}}</h2>
-    </div>
-    <div class="row clearfix">
-    </div>
-</div>
+    <div class="card">
+        <div class="header">
+            <h2>DASHBOARD | {{ $today->format('d M Y') }}</h2>
+        </div>
+        <div class="body">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <a href="{{ url(Request::segment(0) . '/report-pimpinan') }}">
+                        <div class="card">
+                            <div class="body bg-red" style="text-align: -webkit-center;">
+                                <img class="media-object" src="{{ url('media/flaticon/man.png') }}" width="64"
+                                    height="64">
+                                <h5>
+                                    Report Pimpinan
+                                </h5>
+                                <small>Data Penggunaan Diakad Untuk Setiap Role Semester
+                                    {{ $semester_aktif->tahun_ajaran }} {{ $semester_aktif->nm_semester }}</small>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
 
-@include('rilis-note')
+            @include('rilis-note')
+        </div>
