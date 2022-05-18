75e0155cadac669b0c7fc23b66b9e70f37627ce5
BACHTIAR8987854 <amminudinbahtiar101@gmail.com>
2022-05-18 13:10:43 +0700

    fix bug tracer alumni

--- resources/views/humas/alumni/tracer-alumni/add-edit-tracer-alumni.blade.php
+++ resources/views/humas/alumni/tracer-alumni/add-edit-tracer-alumni.blade.php
@@ -1,6 +1,6 @@
 <style>
     .status-header {
-        /* margin: 0 1.5rem; */
+        margin: 0 1.5rem;
         font-size: 18px;
         font-weight: normal;
         color: #111;
@@ -93,56 +93,33 @@
                             <textarea class="form-control" name="alamat_siswa" required="" aria-required="true"
                                 aria-invalid="true"> {{ !empty($alumni) ? $alumni->calon_siswa->alamat_jalan : '' }} </textarea>
                         </div>
-                        @if ($auth_data->sekolah_data->nm_singkat_sekolah == 'smpmuh6krian' || $auth_data->sekolah_data->nm_singkat_sekolah == 'smpypm1')
-                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <h2 class="card-inside-title"> Status </h2>
-                                <input class="with-gap radio-col-light-green form-control validate" type="radio"
-                                    name="status" value="sma" id="sma_status" required="required"
-                                    {{ isset($alumni) && $alumni->status == 'bekerja' ? 'checked' : '' }}>
-                                <label for="work_status"> SMA </label>
-                                <input class="with-gap radio-col-light-green form-control validate" type="radio"
-                                    name="status" value="smk" id="smk_status" required="required"
-                                    {{ isset($alumni) && $alumni->status == 'usaha' ? 'checked' : '' }}>
-                                <label for="enterpreneur_status"> SMK </label>
-                                <input class="with-gap radio-col-light-green form-control validate" type="radio"
-                                    name="status" value="ma" id="college_status" required="required"
-                                    {{ isset($alumni) && $alumni->status == 'kuliah' ? 'checked' : '' }}>
-                                <label for="college_status"> MA </label>
-                                <input class="with-gap radio-col-light-green form-control validate" type="radio"
-                                    name="status" value="berhenti" id="berhenti_status" required="required"
-                                    {{ isset($alumni) && $alumni->status == 'menunggu' ? 'checked' : '' }}>
-                                <label for="idle_status"> Tidak Lanjut Sekolah </label>
-                            </div>
-                        @else
-                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <h2 class="card-inside-title"> Status </h2>
-                                <input class="with-gap radio-col-light-green form-control validate" type="radio"
-                                    name="status" value="bekerja" id="work_status" required="required"
-                                    {{ isset($alumni) && $alumni->status == 'bekerja' ? 'checked' : '' }}>
-                                <label for="work_status"> Bekerja </label>
-                                <input class="with-gap radio-col-light-green form-control validate" type="radio"
-                                    name="status" value="usaha" id="enterpreneur_status" required="required"
-                                    {{ isset($alumni) && $alumni->status == 'usaha' ? 'checked' : '' }}>
-                                <label for="enterpreneur_status"> Wirausaha </label>
-                                <input class="with-gap radio-col-light-green form-control validate" type="radio"
-                                    name="status" value="kuliah" id="college_status" required="required"
-                                    {{ isset($alumni) && $alumni->status == 'kuliah' ? 'checked' : '' }}>
-                                <label for="college_status"> Kuliah </label>
-                                <input class="with-gap radio-col-light-green form-control validate" type="radio"
-                                    name="status" value="menunggu" id="idle_status" required="required"
-                                    {{ isset($alumni) && $alumni->status == 'menunggu' ? 'checked' : '' }}>
-                                <label for="idle_status"> Belum Bekerja </label>
-                            </div>
-                        @endif
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <h2 class="card-inside-title"> Status </h2>
+                            <input class="with-gap radio-col-light-green form-control validate" type="radio"
+                                name="status" value="bekerja" id="work_status" required="required"
+                                {{ isset($alumni) && $alumni->status == 'bekerja' ? 'checked' : '' }}>
+                            <label for="work_status"> Bekerja </label>
+                            <input class="with-gap radio-col-light-green form-control validate" type="radio"
+                                name="status" value="usaha" id="enterpreneur_status" required="required"
+                                {{ isset($alumni) && $alumni->status == 'usaha' ? 'checked' : '' }}>
+                            <label for="enterpreneur_status"> Wirausaha </label>
+                            <input class="with-gap radio-col-light-green form-control validate" type="radio"
+                                name="status" value="kuliah" id="college_status" required="required"
+                                {{ isset($alumni) && $alumni->status == 'kuliah' ? 'checked' : '' }}>
+                            <label for="college_status"> Kuliah </label>
+                            <input class="with-gap radio-col-light-green form-control validate" type="radio"
+                                name="status" value="menunggu" id="idle_status" required="required"
+                                {{ isset($alumni) && $alumni->status == 'menunggu' ? 'checked' : '' }}>
+                            <label for="idle_status"> Belum Bekerja </label>
+                        </div>
+
                         {{-- handle work data --}}
                         <div class="form_layout" id="work_state">
                             @include('./humas.alumni.forms.work_state')
                         </div>
                         {{-- handle Enterpreneur data --}}
                         <div class="form_layout" id="enterpreneur_state">
-                            @include(
-                                './humas.alumni.forms.enterpreneur_state'
-                            )
+                            @include('./humas.alumni.forms.enterpreneur_state')
                         </div>
                         {{-- handle College data --}}
                         <div class="form_layout" id="college_state">
@@ -152,32 +129,11 @@
                         <div class="form_layout" id="idle_state">
                             @include('./humas.alumni.forms.idle_state')
                         </div>
-                        {{-- handle senior high school data --}}
-                        <div class="form_layout" id="senior_high_school">
-                            @include(
-                                './humas.alumni.forms.senior_high_school'
-                            )
-                        </div>
-                        {{-- handle vocational high school data --}}
-                        <div class="form_layout" id="vocational_high_school">
-                            @include(
-                                './humas.alumni.forms.vocational_high_school'
-                            )
-                        </div>
-                        {{-- handle vocational high school data --}}
-                        <div class="form_layout" id="state_madrasah">
-                            @include('./humas.alumni.forms.state_madrasah')
-                        </div>
-                        {{-- handle drop out data --}}
-                        <div class="form_layout" id="drop_out">
-                            @include('./humas.alumni.forms.drop_out')
-                        </div>
 
 
                         <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                             <button id="submit" disabled class="btn btn-block bg-red waves-effect" type="submit">
-                                <i class="material-icons">save</i><span>
-                                    {{ !empty($alumni) ? 'Update' : 'Save' }}
+                                <i class="material-icons">save</i><span> {{ !empty($alumni) ? 'Update' : 'Save' }}
                                 </span>
                             </button>
                         </div>
@@ -218,10 +174,6 @@
                 break;
             case 'menunggu':
                 $('.form_layout#idle_state').show();
-                $('button#submit').attr('disabled', false);
-                break;
-            case 'sma':
-                $('.form_layout#senior_high_school').show();
                 $('button#submit').attr('disabled', false);
                 break;
         }
