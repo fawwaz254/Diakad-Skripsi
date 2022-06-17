c45beccb14e476fe539c48f2f1ac015be4ac1806
syahrule <syahrule01z@gmail.com>
2022-06-17 14:24:37 +0700

    fix tracer edit

--- resources/views/siswa/alumni/view-tracer-alumni-sma.blade.php
+++ resources/views/siswa/alumni/view-tracer-alumni-sma.blade.php
@@ -86,7 +86,9 @@
                 searchable: false,
                 orderable: false,
                 render: function(data) {
-                    return '<button class="btn btn-danger btn-circle waves-effect waves-circle waves-float" onclick="deleteAction(\'' +
+                    return '<a class="target-link btn btn-info btn-circle waves-effect waves-circle waves-float" href="'+ edit_url + '/' + data.id +'">'+
+                    '    <i class="material-icons">edit</i>'+
+                    '</a> '+'<button class="btn btn-danger btn-circle waves-effect waves-circle waves-float" onclick="deleteAction(\'' +
                         delete_url + '\', this)" data-id="' + data.id + '">' +
                         '    <i class="material-icons">delete_forever</i>' +
                         '</button>';
