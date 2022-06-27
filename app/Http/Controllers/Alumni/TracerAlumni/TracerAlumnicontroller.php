3d1d2d093091895c7918b2599e129c686e75d7b4
syahrule <syahrule01z@gmail.com>
2022-06-27 09:17:14 +0700

    fix tracer

--- app/Http/Controllers/Alumni/TracerAlumni/TracerAlumnicontroller.php
+++ app/Http/Controllers/Alumni/TracerAlumni/TracerAlumnicontroller.php
@@ -17,7 +17,7 @@ class TracerAlumnicontroller extends Controller
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
         $data_jurusan = Jurusan::all();
-        $data_kelas = Kelas::where('tingkat', 12)->orWhere('tingkat',9)->get();
+        $data_kelas = Kelas::where('tingkat', 12)->orWhere('tingkat',9)->orWhere('tingkat',3)->get();
         $alumni = null;
         $siswa = Siswa::where('id_pengguna',$auth_data->pengguna->id_pengguna)->with('calon_siswa','pengguna')->first();
         return view('siswa.alumni.add-edit-tracer-alumni', compact('auth_data', 'data_jurusan', 'alumni', 'data_kelas','siswa'));
