e74a50d06eb8bbc623f756e405e4576c4a4bac64
syahrule <syahrule01z@gmail.com>
2022-08-23 10:26:03 +0700

    fix

--- resources/views/keuangan/laporan-keuangan/cetak-laporan/pemasukan-pengeluaran/rekap-detail.blade.php
+++ resources/views/keuangan/laporan-keuangan/cetak-laporan/pemasukan-pengeluaran/rekap-detail.blade.php
@@ -142,7 +142,7 @@
                     {{ indonesiaDate(\Carbon\Carbon::parse($end_date)->format('Y-m-d')) }}
                     {{-- {{ indonesiaDate(\Carbon\Carbon::now()->format('Y-m-d'))  }} --}}
                     <br>Keuangan<br><br><br><br>
-                    <b><u>{{ $auth_data->pengguna->nm_pengguna }},S.E  </u></b>
+                    <b><u>{{ $auth_data->pengguna->nm_pengguna }}, SE  </u></b>
                 </p>
             </div>
         </div>
