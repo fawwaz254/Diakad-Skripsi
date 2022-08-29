623ec31dcc9499558f7e0c628e6f1692bdab31c4
syahrule <syahrule01z@gmail.com>
2022-08-29 21:33:49 +0700

    fix export shift history absensi

--- app/Http/Controllers/Humas/ShiftPengguna/ShiftPenggunaController.php
+++ app/Http/Controllers/Humas/ShiftPengguna/ShiftPenggunaController.php
@@ -14,6 +14,7 @@ use App\Models\Pengguna;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
 use App\Models\Sekolah;
+use App\Models\UnitKerja;
 use Maatwebsite\Excel\Facades\Excel;
 
 
@@ -59,7 +60,8 @@ class ShiftPenggunaController extends Controller
                 }
             }
         }
-        return view('humas/absensi/shift-pengguna/view-shift-pengguna', compact('date', 'hasil'));
+        $unit_kerja = UnitKerja::all();
+        return view('humas/absensi/shift-pengguna/view-shift-pengguna', compact('date', 'hasil','unit_kerja'));
     }
 
     public function addShiftPengguna(Request $request)
@@ -171,24 +173,31 @@ class ShiftPenggunaController extends Controller
         return redirect("/humas#absensi/shift_pengguna/" . $date);
     }
 
-    public function export_shift(Request $request,$date = null){
-        // dd($date);
+    public function export_shift(Request $request,$date = null,$val = null){
+
+        // dd($val);
         set_time_limit(9800);
-        $pengguna = pengguna::whereIn('status_join_table', [1, 2])->where('username', '!=', 'admin')
-        ->with('status_pengguna', 'guru.unit_kerja')
+
+        if($val == "0"){
+        $pengguna = pengguna::where('status_join_table', 1)->where('username', '!=', 'admin')
+        ->with('status_pengguna')
         ->whereHas('status_pengguna', function ($query) {
             $query->where('nm_status_pengguna', '=', 'AKTIF');
         })->get();
+        }else{
+            $pengguna = pengguna::where('status_join_table', 2)->where('username', '!=', 'admin')
+            ->with('status_pengguna', 'guru.unit_kerja')
+            ->whereHas('guru.unit_kerja', function ($query) use($val) {
+                $query->where('id_unit_kerja', '=', $val);
+            })
+            ->whereHas('status_pengguna', function ($query) {
+                $query->where('nm_status_pengguna', '=', 'AKTIF');
+            })->get();
+    
+        }
+    //   dd($pengguna);
 
-        //  $pengguna = pengguna::where('status_join_table', 2)->where('username', '!=', 'admin')
-        // ->with('status_pengguna', 'guru.unit_kerja')
-        // ->whereHas('guru.unit_kerja', function ($query) {
-        //     $query->where('id_unit_kerja', '=', 'Gz5B415566032945cc7e19ed69d3');
-        // })
-        // ->whereHas('status_pengguna', function ($query) {
-        //     $query->where('nm_status_pengguna', '=', 'AKTIF');
-        // })->get();
-
+  
         $year = Carbon::parse($date)->format('Y');
         $mount = Carbon::parse($date)->format('M');
 
