85f2e141c05becf3e46dab1311d8f7da917d9a45
syahrule <syahrule01z@gmail.com>
2022-09-26 10:49:38 +0700

    fix

--- resources/views/humas/absensi/rekap-absensi/view-rekap-absensi.blade.php
+++ resources/views/humas/absensi/rekap-absensi/view-rekap-absensi.blade.php
@@ -238,13 +238,13 @@
 </div>
 
 <script type="text/javascript">
-    $("input").on("change", function() {
-        this.setAttribute(
-            "data-date",
-            moment(this.value, "YYYY-MM-DD")
-            .format(this.getAttribute("data-date-format"))
-        )
-    }).trigger("change")
+    // $("input").on("change", function() {
+    //     this.setAttribute(
+    //         "data-date",
+    //         moment(this.value, "YYYY-MM-DD")
+    //         .format(this.getAttribute("data-date-format"))
+    //     )
+    // }).trigger("change")
 
     function viewSiswa() {
         window.location = '/humas#absensi/rekap-absensi/siswa'
