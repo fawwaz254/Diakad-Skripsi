65d2896559aa9c291202e7ee044c66f643313f81
syahrule <syahrule01z@gmail.com>
2022-10-12 13:13:35 +0700

    add menu set jadwal kelas in role guru

---
+++ database/migrations/2022_10_12_105039_add_menu_set_jadwal_kelas_in_role_guru.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Menu;
+use App\Models\Modul;
+
+class AddMenuSetJadwalKelasInRoleGuru extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $modul = Modul::where('nm_modul', 'Jadwal')->where('id_role', '2')->first();
+
+        $menu = new Menu;
+        $menu->id_modul = $modul->id_modul;
+        $menu->nm_menu = 'Set Jadwal Kelas';
+        $menu->page = 'set-jadwal-kelas';
+        $menu->urutan = 4;
+        $menu->akses = 1;
+        $menu->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('role_guru', function (Blueprint $table) {
+        //     //
+        // });
+    }
+}
