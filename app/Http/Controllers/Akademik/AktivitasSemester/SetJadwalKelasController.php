91c943f71ae35555f63df09af87410b13c4af193
Ega Alifya Firnando <egaalifyaf@gmail.com>
2022-10-12 10:28:29 +0700

    update: alert message validation

--- app/Http/Controllers/Akademik/AktivitasSemester/SetJadwalKelasController.php
+++ app/Http/Controllers/Akademik/AktivitasSemester/SetJadwalKelasController.php
@@ -49,7 +49,7 @@ class SetJadwalKelasController extends Controller
         if ($validator->fails()) {
             return [
                 'status' => 300, // FAILED
-                'message' => $validator->errors()->first()
+                'message' => 'Harap untuk memilih kelas terlebih dahulu'
             ];
         } else {
             return [
