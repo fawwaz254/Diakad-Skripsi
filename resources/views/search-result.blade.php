0a38442658f2d44c85a65363c412eb2e76ab3af8
Rio Ramadhan <ramadhanar.rio@gmail.com>
2022-10-19 14:04:10 +0700

    fixing search

--- resources/views/search-result.blade.php
+++ resources/views/search-result.blade.php
@@ -7,19 +7,21 @@
                 </div>
                 <div class="body">
                     <div class="list-group">
-                        @foreach($auth_data->menus->filter(function ($item) use ($search) {
-                            return false !== stristr($item->nm_menu, $search);
-                        }) as $menu)
-                        @if(!empty($menu->page))
-                        <a href="{{url(Request::segment(1).'#'.$menu->modul->route.'/'.$menu->page)}}" class="target-link list-group-item">
-                        @else
-                        <a href="javascript:void(0);" class="list-group-item">
-                        @endif
-                            <h4 class="list-group-item-heading">{{$menu->modul->nm_modul}}</h4>
-                            <p class="list-group-item-text">
-                                {{$menu->nm_menu}}
-                            </p>
-                        </a>
+                        @foreach(get_moduls() as $modul)
+                            @foreach($modul->menus->filter(function ($item) use ($search) {
+                                return false !== stristr($item->nm_menu, $search);
+                            }) as $menu)
+                            @if(!empty($menu->page))
+                            <a href="{{url(Request::segment(1).'#'.$modul->route.'/'.$menu->page)}}" class="target-link list-group-item">
+                            @else
+                            <a href="javascript:void(0);" class="list-group-item">
+                            @endif
+                                <h4 class="list-group-item-heading">{{$modul->nm_modul}}</h4>
+                                <p class="list-group-item-text">
+                                    {{$menu->nm_menu}}
+                                </p>
+                            </a>
+                            @endforeach
                         @endforeach
                     </div>
                 </div>
