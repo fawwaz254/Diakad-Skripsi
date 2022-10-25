0f29367df4b95d810f4e0979d1f8cd7e6d822bf1
Ega Alifya Firnando <egaalifyaf@gmail.com>
2022-10-25 10:16:26 +0700

    feature: add modul histori absensi siswa role bk

---
+++ database/migrations/2022_10_24_153445_add_modul_absensi_role_bimbingan_konseling.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Menu;
+use App\Models\Modul;
+use App\Models\Role;
+use Carbon\Carbon;
+
+class AddModulAbsensiRoleBimbinganKonseling extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $now = Carbon::now(env('APP_TIMEZONE', ''));
+
+        $role_id = Role::where('nm_role', 'Bimbingan Konseling')->first()->id_role;
+
+        $modul = Modul::create([
+            "id_role"       => $role_id,
+            "nm_modul"      => "Absensi",
+            "route"         => "absensi",
+            "urutan"        => 1,
+            "akses"         => 1,
+            "created_at"    => $now
+        ]);
+
+        $modul->menus()->createMany([
+            [
+                "nm_menu"      => "Histori Absensi Siswa",
+                "page"         => "histori-absensi-siswa",
+                "urutan"       => 1,
+                "akses"        => 1,
+                "created_at"   => $now
+            ],
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
