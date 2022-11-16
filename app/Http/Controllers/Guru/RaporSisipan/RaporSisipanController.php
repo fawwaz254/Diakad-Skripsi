42c9a4d7f18bb042e508b540f1a59a0c5d9da2b7
syahrule <syahrule01z@gmail.com>
2022-11-16 08:11:32 +0700

    fix rapor sisipan smk ypm2

--- app/Http/Controllers/Guru/RaporSisipan/RaporSisipanController.php
+++ app/Http/Controllers/Guru/RaporSisipan/RaporSisipanController.php
@@ -193,7 +193,7 @@ class RaporSisipanController extends Controller
                 $arrayJumlahBelumTerisi = array_count_values(array_column($belumTerisi, 'total'));
 
                 // //loop dan cari nilai kosong yang diatas 5
-                if($auth_data->sekolah_data->nm_singkat_sekolah == 'smkypm3taman'){
+                if($auth_data->sekolah_data->nm_singkat_sekolah == 'smkypm3taman' ||$auth_data->sekolah_data->nm_singkat_sekolah == 'smkypm2' ){
                     $nilaiSiswaYangKosong = $arrayJumlahBelumTerisi[2] ?? 0;
                 }else{
                     $nilaiSiswaYangKosong = 0;
@@ -353,7 +353,7 @@ class RaporSisipanController extends Controller
             ->whereHas('komponen_nilai', function ($query) {
                 $query->where('status',1)->where('type','!=','uas');
             })->get();
-            if($auth_data->sekolah_data->nm_singkat_sekolah == 'smkypm3taman'){
+            if($auth_data->sekolah_data->nm_singkat_sekolah == 'smkypm3taman' || $auth_data->sekolah_data->nm_singkat_sekolah == 'smkypm2'){
                 $nilai_siswa = [];
                 // $nilai_komponen = [];
                 if ($list_siswa) {
