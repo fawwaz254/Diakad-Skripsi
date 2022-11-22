914b53f7b43c67510db7c56589400c178de90e6d
Ega Alifya Firnando <egaalifyaf@gmail.com>
2022-11-22 07:27:01 +0700

    update: content [minor]

--- resources/views/pendidikan/siswa/update-foto/view-batch-upload-foto.blade.php
+++ resources/views/pendidikan/siswa/update-foto/view-batch-upload-foto.blade.php
@@ -1,7 +1,6 @@
 <div class="container-fluid">
     <div class="block-header">
-        <h2><a class="btn bg-blue waves-effect target-link"
-                href="{{url(Request::segment(1).'#siswa/update-foto/')}}"><i
+        <h2><a class="btn bg-blue waves-effect target-link" href="{{ url(Request::segment(1) . '#siswa/update-foto/') }}"><i
                     class="material-icons">backspace</i><span>Kembali</span></a></h2>
     </div>
     <div class="row clearfix">
@@ -9,7 +8,7 @@
             <div class="card">
                 <div class="header">
                     <h2>
-                        UPDATE FOTO SISWA BATCH
+                        Update Foto Siswa Batch
                     </h2>
                 </div>
                 <div class="body">
