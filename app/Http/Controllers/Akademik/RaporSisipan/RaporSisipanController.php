5e018a00a11e45d94bafebbf131d3c3471fdf841
syahrule <syahrule01z@gmail.com>
2022-12-09 11:07:09 +0700

    fix

--- app/Http/Controllers/Akademik/RaporSisipan/RaporSisipanController.php
+++ app/Http/Controllers/Akademik/RaporSisipan/RaporSisipanController.php
@@ -109,7 +109,7 @@ class RaporSisipanController extends Controller
 
         $rapor_sisipan = RaporSisipan::where('id_rapor_sisipan', $id_rapor_sisipan)->with('mata_pelajaran', 'kelas')->first();
 
-        $list_data = KomponenNilaiRaporSisipan::whereIn('urutan', [1, 2, 5, 6, 9])->get();
+        $list_data = KomponenNilaiRaporSisipan::where('status', 1)->where('type', '!=', 'uas')->get();
         $list_siswa = Siswa::where('id_kelas', $rapor_sisipan->id_kelas)->with('pengguna.status_pengguna')->whereHas('pengguna.status_pengguna', function ($query) {
             $query->where('aktif_status_pengguna', '=', '1');
         })->orderBy('nis_siswa')->get();
@@ -131,9 +131,9 @@ class RaporSisipanController extends Controller
                 foreach ($nilaiRapor as $a) {
                     $nilai_siswa[$nilaiRapor['id_komponen_nilai'] . $nilaiRapor['id_siswa'] . $nilaiRapor['id_rapor_sisipan']] = $nilaiRapor['nilai'];
 
-                    $nilai_sumatif1 = $list_data->firstWhere('nm_nilai', '=', 'NILAI SUMATIF 1');
-                    $nilai_sumatif2 = $list_data->firstWhere('nm_nilai', '=', 'NILAI SUMATIF 2');
-                    $sts = $list_data->firstWhere('nm_nilai', '=', 'STS');
+                    $nilai_sumatif1 = $list_data->firstWhere('urutaan', '=', '5');
+                    $nilai_sumatif2 = $list_data->firstWhere('urutaan', '=', '6');
+                    $sts = $list_data->firstWhere('urutaan', '=', '9');
                     if ($nilaiRapor['id_komponen_nilai']  == $nilai_sumatif1->id_komponen_nilai) {
                         $nilai_komponen[$nilaiRapor['id_siswa'] . 'nilai_sumasi1'] =  $nilaiRapor['nilai'];
                     }
--- resources/views/rapor-sisipan-sts.blade.php
+++ resources/views/rapor-sisipan-sts.blade.php
@@ -28,7 +28,7 @@
                 <td>{{ $siswa->pengguna->nm_pengguna }}</td>
                 @foreach ($data['list_data'] as $nilai)
                     <td style="text-align: center">
-                        {{ $data['nilai_siswa'][$nilai->id_komponen_nilai . $siswa->id_siswa . $data['id_rapor_sisipan']] }}
+                        {{ $data['nilai_siswa'][$nilai->id_komponen_nilai . $siswa->id_siswa . $data['id_rapor_sisipan']] ?? null }}
                     </td>
                 @endforeach
                 {{-- <td style="text-align: center;">
