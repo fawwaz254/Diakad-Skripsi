6146d0df081a3c80184808f2ae702cde173e2ef1
Ega Alifya Firnando <egaalifyaf@gmail.com>
2022-12-27 08:24:19 +0700

    update: keterangan kelompok biaya

--- resources/views/keuangan/utility/tagihan-siswa/view-detail-tagihan-siswa.blade.php
+++ resources/views/keuangan/utility/tagihan-siswa/view-detail-tagihan-siswa.blade.php
@@ -60,7 +60,7 @@
                                     <option value="0" selected >-- Semua --</option>
                                     @foreach($data_kelompok_biaya as $data)
                                         @if($data->status_kelompok_biaya == 1)
-                                            <option value="{{$data->id_kelompok_biaya}}">{{$data->nm_kelompok_biaya}} (Reguler)</option>
+                                            <option value="{{$data->id_kelompok_biaya}}">{{$data->nm_kelompok_biaya}}</option>
                                         @else
                                             <option value="{{$data->id_kelompok_biaya}}">{{$data->nm_kelompok_biaya}} (Khusus)</option>
                                         @endif
