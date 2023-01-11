47452abd16d168922589902708f8de70aedb8aea
syahrule <syahrule01z@gmail.com>
2023-01-11 14:53:16 +0700

    fix

--- resources/views/must-update-biodata.blade.php
+++ resources/views/must-update-biodata.blade.php
@@ -32,8 +32,22 @@
                                 </h2>
                             </div>
                             <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
-                                <input type="text" class="form-control" name="nomor_hp_ortu" aria-required="true"
+                                <input type="number" class="form-control" name="nomor_hp_ortu" aria-required="true"
                                     aria-invalid="true" value="{{ $siswa->wali_murid->nomor_hp_wali_murid ?? null }}"
+                                    required>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row clearfix">
+                            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                <h2 class="card-inside-title">
+                                    NAMA ORANG TUA<span class="is-required">*</span><br>
+                                    <small>Diisi Nama pemilik nomor hp</small>
+                                </h2>
+                            </div>
+                            <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                                <input type="text" class="form-control" name="nm_ortu" aria-required="true"
+                                    aria-invalid="true" value="{{ $siswa->wali_murid->nm_wali_murid ?? null }}"
                                     required>
                             </div>
                         </div>
