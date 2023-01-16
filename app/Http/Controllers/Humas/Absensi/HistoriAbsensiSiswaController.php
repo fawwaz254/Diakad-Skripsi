2fea8ba4ad9b0e22fa76b8ff9cb5931661bc52fc
syahrule <syahrule01z@gmail.com>
2023-01-16 14:27:43 +0700

    fix

--- app/Http/Controllers/Humas/Absensi/HistoriAbsensiSiswaController.php
+++ app/Http/Controllers/Humas/Absensi/HistoriAbsensiSiswaController.php
@@ -174,10 +174,7 @@ class HistoriAbsensiSiswaController extends Controller
             $shiftPengguna = $allShiftPengguna->firstWhere('id_pengguna', '=', $value->id_pengguna);
             $attendance =  $allPresensiPengguna->firstWhere('id_pengguna', '=', $value->id_pengguna);
             $shiftMaster = isset($shiftPengguna->shift_master) ? $shiftPengguna->shift_master : null;
-            $hasil[$key]['shift'] = false;
-            if ($shiftPengguna) {
-                $hasil[$key]['shift'] = true;
-            }
+            $hasil[$key]['shift'] = $shiftMaster;
 
             if ($attendance) {
                 if ($attendance->status) {
@@ -193,7 +190,7 @@ class HistoriAbsensiSiswaController extends Controller
                     $hasil[$key]['id_presensi_pengguna'] = $attendance->id_presensi_pengguna;
                 }
 
-                if ($attendance->check_in) {
+                if ($attendance->check_in && isset($shiftMaster)) {
                     $hasil[$key]['check_in'] = $attendance->check_in;
                     $hasil[$key]['status'] = "Masuk";
                     $jumlah_hadir++;
