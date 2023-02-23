da64ac94fa974c9b76275299ae380765802e5e4a
syahrule <syahrule01z@gmail.com>
2023-02-23 10:15:37 +0700

    add pts and pas in SIM, and give print

--- resources/views/keuangan/sim/spp/print-pembayaran-spp.blade.php
+++ resources/views/keuangan/sim/spp/print-pembayaran-spp.blade.php
@@ -10,13 +10,13 @@
         .page {
             width: 900px;
         }
-        
+
         .ttd {
             margin: 20px 10px;
             text-align: center;
             width: 33%;
         }
-        
+
         .clear {
             clear: both;
         }
@@ -24,7 +24,6 @@
         .text-left {
             text-align: left;
         }
-
     </style>
     <style type="text/css" media="print">
         @page {
@@ -35,24 +34,34 @@
 
 <body>
     <div class="page">
-        
+
         <table>
             <tr>
                 <td>
-                    <img src="https://diakad.sgp1.digitaloceanspaces.com/{{$auth_data->sekolah_data->nm_singkat_sekolah}}/global/logo-sekolah" alt="Logo Sekolah" style="height:50px;" />
+                    <img src="https://diakad.sgp1.digitaloceanspaces.com/{{ $auth_data->sekolah_data->nm_singkat_sekolah }}/global/logo-sekolah"
+                        alt="Logo Sekolah" style="height:50px;" />
                 </td>
                 <td>
-                    <h2 style="margin-left: 10px;">Tanda Bukti Pembayaran<br> {{strtoupper($auth_data->sekolah_data->nm_sekolah)}}</h2>
+                    <h2 style="margin-left: 10px;">Tanda Bukti Pembayaran<br>
+                        {{ strtoupper($auth_data->sekolah_data->nm_sekolah) }}</h2>
                 </td>
             </tr>
         </table>
         <table border="1" cellspacing="0" cellpadding="5" style="width: 100%;" class="text-left">
             <tr>
-                <td colspan="2" style="text-align: center;"><b>TANDA BUKTI PEMBAYARAN SPP</b> </td>
+                <td colspan="2" style="text-align: center;"><b>TANDA BUKTI PEMBAYARAN
+                        @if ($pembayaran->tagihan_biaya->keterangan == '-')
+                            SPP
+                        @else
+                            {{ $pembayaran->tagihan_biaya->keterangan }}
+                        @endif
+                    </b>
+                </td>
             </tr>
             <tr>
                 <td style="width: 100%; vertical-align: top;">
-                    <table border="0" cellspacing="0" cellpadding="5" style="width: 100%; vertical-align: top;" class="text-left">
+                    <table border="0" cellspacing="0" cellpadding="5" style="width: 100%; vertical-align: top;"
+                        class="text-left">
                         <tr>
                             <th style="width: 25%;">No.</th>
                             <td style="width: 5px;">:</td>
@@ -65,12 +74,18 @@
                         <tr style="vertical-align: top;">
                             <th>Untuk Pembayaran</th>
                             <td>:</td>
-                            <td>SPP</td>
+                            <td>
+                                @if ($pembayaran->tagihan_biaya->keterangan == '-')
+                                    SPP
+                                @else
+                                    {{ $pembayaran->tagihan_biaya->keterangan }}
+                                @endif
+                            </td>
                         </tr>
                         <tr>
                             <th>Jumlah</th>
                             <td>:</td>
-                            <td>{{ "Rp " . number_format($pembayaran->besar_pembayaran) }}</td>
+                            <td>{{ 'Rp ' . number_format($pembayaran->besar_pembayaran) }}</td>
                         </tr>
                         <tr style="vertical-align: top;">
                             <th>Terbilang</th>
@@ -83,23 +98,26 @@
                             <td class="ttd"></td>
                             <td class="ttd"></td>
                             <td class="ttd">
-                                {{ ($auth_data->sekolah_data->alamat_kecamatan !== null) ? $auth_data->sekolah_data->alamat_kecamatan . ',' : null}} {{ date_format(new DateTime($pembayaran->tgl_pembayaran), 'j M Y') }}
+                                {{ $auth_data->sekolah_data->alamat_kecamatan !== null ? $auth_data->sekolah_data->alamat_kecamatan . ',' : null }}
+                                {{ date_format(new DateTime($pembayaran->tgl_pembayaran), 'j M Y') }}
                             </td>
                         </tr>
                         <tr>
                             <td class="ttd">
-                                Kepala Sekolah 
-                                <br><br><br><br> 
-                                <span style="text-decoration: underline;"> {{ $auth_data->sekolah_data->nm_kepala_sekolah }} </span>
+                                Kepala Sekolah
+                                <br><br><br><br>
+                                <span style="text-decoration: underline;">
+                                    {{ $auth_data->sekolah_data->nm_kepala_sekolah }} </span>
                             </td>
                             <td class="ttd">
-                                Keuangan 
-                                <br><br><br><br> 
-                                <span style="text-decoration: underline;"> {{ $auth_data->pengguna->nm_pengguna }} </span>
+                                Keuangan
+                                <br><br><br><br>
+                                <span style="text-decoration: underline;"> {{ $auth_data->pengguna->nm_pengguna }}
+                                </span>
                             </td>
                             <td class="ttd">
                                 Penerima
-                                <br><br><br><br> 
+                                <br><br><br><br>
                                 .........................
                             </td>
                         </tr>
@@ -116,4 +134,5 @@
 <script>
     window.print();
 </script>
+
 </html>
