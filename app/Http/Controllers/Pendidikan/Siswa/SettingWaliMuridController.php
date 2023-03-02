70f8e691848cc9019d843d7417fc9a890bcdf333
syahrule <syahrule01z@gmail.com>
2023-03-02 09:40:16 +0700

    add message

--- app/Http/Controllers/Pendidikan/Siswa/SettingWaliMuridController.php
+++ app/Http/Controllers/Pendidikan/Siswa/SettingWaliMuridController.php
@@ -69,6 +69,7 @@ class SettingWaliMuridController extends BaseController
                 $wali->delete();
             }
         }
+        echo "Berhasil";
     }
 
     public function viewUploadSettingWaliMurid(Request $request, $id_kelas)
