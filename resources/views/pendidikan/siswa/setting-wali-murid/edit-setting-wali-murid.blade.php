9074833441da96a5b9edad7770f592af7e00ac46
syahrule <syahrule01z@gmail.com>
2023-03-06 10:28:09 +0700

    change setting wali murid in role kesiswaaan

--- resources/views/pendidikan/siswa/setting-wali-murid/edit-setting-wali-murid.blade.php
+++ resources/views/pendidikan/siswa/setting-wali-murid/edit-setting-wali-murid.blade.php
@@ -1,63 +1,61 @@
 <div class="container-fluid">
     <div class="block-header">
-        <h2><a class="btn bg-blue waves-effect target-link " href="{{url(Request::segment(1).'#siswa/setting-wali-murid/view-kelas/'.$siswa->id_kelas)}}"><i class="material-icons">backspace</i><span>Kembali</span></a></h2>
+        <h2><a class="btn bg-blue waves-effect target-link "
+                href="{{ url(Request::segment(1) . '#siswa/setting-wali-murid/view-kelas/' . $siswa->id_kelas) }}"><i
+                    class="material-icons">backspace</i><span>Kembali</span></a></h2>
     </div>
     <div class="row clearfix">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="card">
                 <div class="header">
                     <h2>
-                        EDIT WALI MURID {{$siswa->nm_pengguna}}
+                        EDIT WALI MURID {{ $siswa->nm_pengguna }}
                     </h2>
                 </div>
                 <div class="body">
-                    <form id="form-validation" method="POST" action="{{url(Request::segment(1).'/'.Request::segment(2).'/action-setting-wali-murid/edit/'.$siswa->id_siswa)}}">
-                        {{csrf_field()}}
+                    <form id="form-validation" method="POST"
+                        action="{{ url(Request::segment(1) . '/' . Request::segment(2) . '/action-setting-wali-murid/edit/' . $siswa->id_siswa) }}">
+                        {{ csrf_field() }}
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <h2 class="card-inside-title">
-                                    Nama dan HP Wali Murid
-                                </h2>
+
                                 <div class="row clearfix">
-                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                        <!-- <input type="text" id="realtxt" onkeyup="searchSel()" class="form-control"> -->
-                                        <select class="form-control show-tick" id="nomor_hp_wali_murid" name="nomor_hp_wali_murid">
-                                        </select>
+                                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                        <h2 class="card-inside-title">
+                                            NOMOR HP ORANG TUA<span class="is-required">*</span><br>
+                                            <small>Diisi nomor telepon selular (milik orangtua, atau wali) tanpa tanda
+                                                baca, contoh: 0815555555555</small>
+                                        </h2>
+                                    </div>
+                                    <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                                        <input type="number" class="form-control" name="nomor_hp_ortu"
+                                            aria-required="true" aria-invalid="true"
+                                            value="{{ $siswa->wali_murid->nomor_hp_wali_murid ?? null }}" required>
                                     </div>
                                 </div>
-                                <p>
-                                    Tidak menemukan nama dan nomor wali murid? <a target="_blank" href="{{url(Request::segment(1).'#'.Request::segment(2).'/'.Request::segment(3).'/add')}}">Tambah wali murid</a>
-                                </p>
-                                <h2 class="card-inside-title">
-                                    Sebagai Orangtua atau Wali
-                                </h2>
+                                <br>
                                 <div class="row clearfix">
-                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                        @if(!empty($wali_murid))
-                                            <select class="form-control show-tick" name="is_orang_tua">
-                                                @if($wali_murid->is_orang_tua == 1)
-                                                    <option value="1" selected="">Orangtua Kandung</option>
-                                                    <option value="0">Wali (Bukan Orangtua Kandung)</option>
-                                                @else
-                                                    <option value="1">Orangtua Kandung</option>
-                                                    <option value="0" selected="">Wali (Bukan Orangtua Kandung)</option>
-                                                @endif
-                                            </select>
-                                        @else
-                                            <select class="form-control show-tick" name="is_orang_tua">
-                                                <option value="1">Orangtua Kandung</option>
-                                                <option value="0">Wali (Bukan Orangtua Kandung)</option>
-                                            </select>
-                                        @endif
+                                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                                        <h2 class="card-inside-title">
+                                            NAMA ORANG TUA<span class="is-required">*</span><br>
+                                            <small>Diisi Nama pemilik nomor hp</small>
+                                        </h2>
+                                    </div>
+                                    <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                                        <input type="text" class="form-control" name="nm_ortu" aria-required="true"
+                                            aria-invalid="true" value="{{ $siswa->wali_murid->nm_wali_murid ?? null }}"
+                                            required>
                                     </div>
                                 </div>
+                                <br>
                                 <div class="row clearfix">
                                     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                     </div>
                                 </div>
                                 <div class="row clearfix">
                                     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                        <button class="btn btn-block bg-red waves-effect" type="submit"><i class="material-icons">save</i><span>Save</span></button>
+                                        <button class="btn btn-block bg-red waves-effect" type="submit"><i
+                                                class="material-icons">save</i><span>Save</span></button>
                                     </div>
                                 </div>
                             </div>
@@ -89,9 +87,9 @@
             url: base_url + '/kesiswaan/siswa/wali-murid/get-data',
             type: "GET",
             delay: 300,
-            processResults: function (data) {
+            processResults: function(data) {
                 return {
-                    results: $.map(data, function (item) {
+                    results: $.map(data, function(item) {
                         return {
                             text: item.nm_wali_murid + ' (' + item.nomor_hp_wali_murid + ')',
                             value: item.nomor_hp_wali_murid,
