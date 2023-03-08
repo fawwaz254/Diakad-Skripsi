08986d0dc40fc2858b665a37b4436d1d2afeefa1
syahrule <syahrule01z@gmail.com>
2023-03-08 14:07:12 +0700

    change input kapasitas to number in data ruangan

--- resources/views/sarana-prasarana/data-sarpras-ruangan/ruangan/edit-ruangan.blade.php
+++ resources/views/sarana-prasarana/data-sarpras-ruangan/ruangan/edit-ruangan.blade.php
@@ -1,6 +1,8 @@
 <div class="container-fluid">
     <div class="block-header">
-        <h2><a class="btn bg-blue waves-effect target-link " href="{{url(Request::segment(1).'#data-sarpras-ruangan/ruangan')}}"><i class="material-icons">backspace</i><span>Kembali</span></a></h2>
+        <h2><a class="btn bg-blue waves-effect target-link "
+                href="{{ url(Request::segment(1) . '#data-sarpras-ruangan/ruangan') }}"><i
+                    class="material-icons">backspace</i><span>Kembali</span></a></h2>
     </div>
     <div class="row clearfix">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -11,20 +13,23 @@
                     </h2>
                 </div>
                 <div class="body">
-                    <form id="form-validation" method="POST" action="{{url(Request::segment(1).'/'.Request::segment(2).'/action-ruangan/edit/'.$data_ruangan->id_ruangan)}}">
-                        {{csrf_field()}}
+                    <form id="form-validation" method="POST"
+                        action="{{ url(Request::segment(1) . '/' . Request::segment(2) . '/action-ruangan/edit/' . $data_ruangan->id_ruangan) }}">
+                        {{ csrf_field() }}
                         <h2 class="card-inside-title">
                             Jenis Ruangan
                         </h2>
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <select class="form-control show-tick" name="id_jenis_ruangan">
-                                    @foreach($data_jenis_ruangan as $data)
-                                    @if($data->id_jenis_ruangan == $data_ruangan->id_jenis_ruangan)
-                                    <option value="{{$data->id_jenis_ruangan}}" selected>{{$data->nm_jenis_ruangan}}</option>
-                                    @else
-                                    <option value="{{$data->id_jenis_ruangan}}">{{$data->nm_jenis_ruangan}}</option>
-                                    @endif
+                                    @foreach ($data_jenis_ruangan as $data)
+                                        @if ($data->id_jenis_ruangan == $data_ruangan->id_jenis_ruangan)
+                                            <option value="{{ $data->id_jenis_ruangan }}" selected>
+                                                {{ $data->nm_jenis_ruangan }}</option>
+                                        @else
+                                            <option value="{{ $data->id_jenis_ruangan }}">{{ $data->nm_jenis_ruangan }}
+                                            </option>
+                                        @endif
                                     @endforeach
                                 </select>
                             </div>
@@ -36,8 +41,10 @@
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <select class="form-control show-tick" name="id_kelas">
                                     <option value="">Bukan Kelas</option>
-                                    @foreach($kelas as $k)
-                                    <option value="{{$k->id_kelas}}" @if($data_ruangan->id_kelas == $k->id_kelas) selected @endif>{{$k->nm_kelas}}</option>
+                                    @foreach ($kelas as $k)
+                                        <option value="{{ $k->id_kelas }}"
+                                            @if ($data_ruangan->id_kelas == $k->id_kelas) selected @endif>{{ $k->nm_kelas }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
@@ -48,12 +55,13 @@
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <select class="form-control show-tick" name="id_gedung">
-                                    @foreach($data_gedung as $data)
-                                    @if($data->id_gedung == $data_ruangan->id_gedung)
-                                    <option value="{{$data->id_gedung}}" selected>{{$data->nm_gedung}}</option>
-                                    @else
-                                    <option value="{{$data->id_gedung}}">{{$data->nm_gedung}}</option>
-                                    @endif
+                                    @foreach ($data_gedung as $data)
+                                        @if ($data->id_gedung == $data_ruangan->id_gedung)
+                                            <option value="{{ $data->id_gedung }}" selected>{{ $data->nm_gedung }}
+                                            </option>
+                                        @else
+                                            <option value="{{ $data->id_gedung }}">{{ $data->nm_gedung }}</option>
+                                        @endif
                                     @endforeach
                                 </select>
                             </div>
@@ -64,12 +72,14 @@
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <select class="form-control show-tick" name="id_pemilik_sarpras">
-                                    @foreach($data_pemilik_sarpras as $data)
-                                    @if($data->id_pemilik_sarpras == $data_ruangan->id_pemilik_sarpras)
-                                    <option value="{{$data->id_pemilik_sarpras}}" selected>{{$data->nm_pemilik_sarpras}}</option>
-                                    @else
-                                    <option value="{{$data->id_pemilik_sarpras}}">{{$data->nm_pemilik_sarpras}}</option>
-                                    @endif
+                                    @foreach ($data_pemilik_sarpras as $data)
+                                        @if ($data->id_pemilik_sarpras == $data_ruangan->id_pemilik_sarpras)
+                                            <option value="{{ $data->id_pemilik_sarpras }}" selected>
+                                                {{ $data->nm_pemilik_sarpras }}</option>
+                                        @else
+                                            <option value="{{ $data->id_pemilik_sarpras }}">
+                                                {{ $data->nm_pemilik_sarpras }}</option>
+                                        @endif
                                     @endforeach
                                 </select>
                             </div>
@@ -79,8 +89,8 @@
                         </h2>
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <input type="text" class="form-control" name="nm_ruangan" required="" aria-required="true" aria-invalid="true"
-                                    value="{{$data_ruangan->nm_ruangan}}">
+                                <input type="text" class="form-control" name="nm_ruangan" required=""
+                                    aria-required="true" aria-invalid="true" value="{{ $data_ruangan->nm_ruangan }}">
                             </div>
                         </div>
                         <h2 class="card-inside-title">
@@ -88,8 +98,9 @@
                         </h2>
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <input type="text" class="form-control" name="kapasitas_ruangan" required="" aria-required="true"
-                                    aria-invalid="true" value="{{$data_ruangan->kapasitas_ruangan}}">
+                                <input type="number" class="form-control" name="kapasitas_ruangan" required=""
+                                    aria-required="true" aria-invalid="true"
+                                    value="{{ $data_ruangan->kapasitas_ruangan }}">
                             </div>
                         </div>
                         <h2 class="card-inside-title">
@@ -97,8 +108,9 @@
                         </h2>
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <input type="text" class="form-control" name="kapasitas_ujian" required="" aria-required="true"
-                                    aria-invalid="true" value="{{$data_ruangan->kapasitas_ujian}}">
+                                <input type="number" class="form-control" name="kapasitas_ujian" required=""
+                                    aria-required="true" aria-invalid="true"
+                                    value="{{ $data_ruangan->kapasitas_ujian }}">
                             </div>
                         </div>
                         <h2 class="card-inside-title">
@@ -106,8 +118,8 @@
                         </h2>
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                <textarea rows="4" cols="50" class="form-control" name="deskripsi_ruangan" required="" aria-required="true"
-                                    aria-invalid="true">{{$data_ruangan->deskripsi_ruangan}}</textarea>
+                                <textarea rows="4" cols="50" class="form-control" name="deskripsi_ruangan" required=""
+                                    aria-required="true" aria-invalid="true">{{ $data_ruangan->deskripsi_ruangan }}</textarea>
                             </div>
                         </div>
                         <h2 class="card-inside-title">
@@ -116,12 +128,12 @@
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                                 <select class="form-control show-tick" name="is_aktif">
-                                    @if($data_ruangan->is_aktif == 1)
-                                    <option value="1" selected>Aktif</option>
-                                    <option value="0">Non-Aktif</option>
+                                    @if ($data_ruangan->is_aktif == 1)
+                                        <option value="1" selected>Aktif</option>
+                                        <option value="0">Non-Aktif</option>
                                     @else
-                                    <option value="1">Aktif</option>
-                                    <option value="0" selected>Non-Aktif</option>
+                                        <option value="1">Aktif</option>
+                                        <option value="0" selected>Non-Aktif</option>
                                     @endif
                                 </select>
                             </div>
@@ -132,7 +144,8 @@
                         </div>
                         <div class="row clearfix">
                             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                <button class="btn btn-block bg-red waves-effect" type="submit"><i class="material-icons">save</i><span>Save</span></button>
+                                <button class="btn btn-block bg-red waves-effect" type="submit"><i
+                                        class="material-icons">save</i><span>Save</span></button>
                             </div>
                         </div>
                     </form>
