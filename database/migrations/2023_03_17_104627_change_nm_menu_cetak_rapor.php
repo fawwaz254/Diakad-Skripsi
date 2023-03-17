12f566a6b5024a6c30eb090f2b2ace83c2c7dca5
syahrule <syahrule01z@gmail.com>
2023-03-17 10:54:21 +0700

    change name cetak rapor

---
+++ database/migrations/2023_03_17_104627_change_nm_menu_cetak_rapor.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Modul;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeNmMenuCetakRapor extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $modul = Modul::where('id_role', '2')->where('nm_modul', 'Wali Kelas')->first();
+
+        if ($modul) {
+            $modul->menus()->where('nm_menu', 'Cetak Rapor Siswa')->update([
+                "nm_menu" => "Rapor Sisipan"
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
