7c83b9c0ce731e0737fe30b787219c8a6a430d4d
syahrule <syahrule01z@gmail.com>
2023-03-17 10:20:33 +0700

    fix

--- resources/views/guru/rapor-sisipan/daftar-nilai-sts/cetak-nilai-sts2.blade.php
+++ resources/views/guru/rapor-sisipan/daftar-nilai-sts/cetak-nilai-sts2.blade.php
@@ -207,7 +207,7 @@
                                     $nilaiFormatif = round($jumlahNilaFormatif / $nilaiFormatifLenght) * 2;
                                 }
                                 $nilaiSTS = $nilai_komponen[$siswa->id_siswa . 'sts'] * 2;
-                                $totalNilai = ($nilaiTugas + $nilaiFormatif + $nilaiSTS) / 5;
+                                $totalNilai = round(($nilaiTugas + $nilaiFormatif + $nilaiSTS) / 5);
                             @endphp
                             {{ $totalNilai != '0' ? $totalNilai : '' }}
                             {{-- {{ round(($nilai_komponen[$siswa->id_siswa . '1'] + $nilai_komponen[$siswa->id_siswa . '5'] + $nilai_komponen[$siswa->id_siswa . '6'] + $nilai_komponen[$siswa->id_siswa . '9'] * 2) / 5) }} --}}
