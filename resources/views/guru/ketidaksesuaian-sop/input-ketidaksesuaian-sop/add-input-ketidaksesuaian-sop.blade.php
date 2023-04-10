24b9d9d02c3a5dd5297aeef56b52fdd1918f031c
syahrule <syahrule01z@gmail.com>
2023-04-10 14:01:50 +0700

    fix input ketidaksesuaian SOP

--- resources/views/guru/ketidaksesuaian-sop/input-ketidaksesuaian-sop/add-input-ketidaksesuaian-sop.blade.php
+++ resources/views/guru/ketidaksesuaian-sop/input-ketidaksesuaian-sop/add-input-ketidaksesuaian-sop.blade.php
@@ -42,7 +42,7 @@
 
 
                         <h2 class="card-inside-title">
-                            Catatan Pelanggaran
+                            Catatan Ketidakseuaian SOP
                         </h2>
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -51,7 +51,7 @@
                             </div>
                         </div>
                         <h2 class="card-inside-title">
-                            Tanggal Pelanggaran
+                            Tanggal Ketidakseuaian SOP
                         </h2>
                         <div class="row clearfix">
                             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
