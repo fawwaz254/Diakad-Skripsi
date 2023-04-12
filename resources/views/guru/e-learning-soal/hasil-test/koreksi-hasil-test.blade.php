e21ff2a34fd6d27f36addbf122160fd8d35cd2c9
syahrule <syahrule01z@gmail.com>
2023-04-12 15:23:09 +0700

    add size

--- resources/views/guru/e-learning-soal/hasil-test/koreksi-hasil-test.blade.php
+++ resources/views/guru/e-learning-soal/hasil-test/koreksi-hasil-test.blade.php
@@ -17,7 +17,7 @@
                         <input type="hidden" name="id_paket_soal" value="{{ $id_paket_soal }}">
                         <input type="hidden" name="id_pengguna" value="{{ $id_pengguna }}">
                         <input type="hidden" name="total_nilai" value="{{ $total_nilai }}">
-                        Total Nilai Pilihan Ganda : {{  $total_nilai  }}
+                        Total Nilai Pilihan Ganda : {{ $total_nilai }}
                         @foreach ($questions as $question)
                             <hr style="height:1px;border:none;color:#333;background-color:#333;">
                             <p>Soal no. {{ $nomor++ }}</p>
@@ -26,11 +26,10 @@
                                 style="background:white; border: 1px solid #ccc; border-radius: 4px; display: block; padding: 9.5px; margin-bottom: 10px;">{!! $question->soal->content !!}</pre>
                             <p>Jawaban</p>
                             @if ($question->id_tipe_soal == 2)
-                                <textarea id="q1" class="form-control" name="soal" data-sample-short disabled>{!! $question->jawaban_essay !!}</textarea>
+                                <textarea id="q1" class="form-control" name="soal" data-sample-short disabled rows="4">{!! $question->jawaban_essay !!}</textarea>
                             @else
                                 @if ($question->type_file == 'pdf')
-                                    <iframe
-                                        src="https://diakad.sgp1.digitaloceanspaces.com/{{ $question->link_file }}"
+                                    <iframe src="https://diakad.sgp1.digitaloceanspaces.com/{{ $question->link_file }}"
                                         style="width:100%; height:535px;" frameborder="0"></iframe>
                                 @else
                                     <iframe
