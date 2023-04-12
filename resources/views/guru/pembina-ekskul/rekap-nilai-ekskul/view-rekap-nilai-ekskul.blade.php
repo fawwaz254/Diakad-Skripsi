06b0d74ad01add1ae499275804425d27721f72b6
Ega Alifya Firnando <egaalifyaf@gmail.com>
2023-04-12 15:46:28 +0700

    fix: error when data ekskul is deleted

--- resources/views/guru/pembina-ekskul/rekap-nilai-ekskul/view-rekap-nilai-ekskul.blade.php
+++ resources/views/guru/pembina-ekskul/rekap-nilai-ekskul/view-rekap-nilai-ekskul.blade.php
@@ -15,14 +15,15 @@
                                     <div class="form-line">
                                         <label>Semester</label>
                                         <select class="form-control show-tick" name="id_semester" required="">
-                                            @foreach($data_semester as $data)
-                                            <option value="{{$data->id_semester}}" {{(!empty($selected_semester) && ($selected_semester->id_semester == $data->id_semester) ? 'selected' : ($data->is_aktif_semester == 1)) ? 'selected' : ''}}>
-                                                {{$data->tahun_ajaran}}
-                                                {{$data->nm_semester}} 
-                                                @if($data->is_aktif_semester == 1)
-                                                    (Aktif)
-                                                @endif
-                                            </option>
+                                            @foreach ($data_semester as $data)
+                                                <option value="{{ $data->id_semester }}"
+                                                    {{ (!empty($selected_semester) && $selected_semester->id_semester == $data->id_semester ? 'selected' : $data->is_aktif_semester == 1) ? 'selected' : '' }}>
+                                                    {{ $data->tahun_ajaran }}
+                                                    {{ $data->nm_semester }}
+                                                    @if ($data->is_aktif_semester == 1)
+                                                        (Aktif)
+                                                    @endif
+                                                </option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -32,15 +33,19 @@
                                         <label>Ekskul</label>
                                         <select class="form-control show-tick" name="id_ekskul" required="">
                                             <option value="">-- Pilih Ekskul --</option>
-                                            @foreach($data_ekskul as $data)
-                                                <option {{(!empty($id_ekskul) && $id_ekskul == $data->ekskul->id_ekskul)? 'selected' : ''}} value="{{$data->ekskul->id_ekskul}}">{{$data->ekskul->nm_ekskul}}</option>
+                                            @foreach ($data_ekskul as $data)
+                                                @if (!empty($data->ekskul))
+                                                    <option value="{{ $data->ekskul->id_ekskul }}">
+                                                        {{ $data->ekskul->nm_ekskul }}</option>
+                                                @endif
                                             @endforeach
                                         </select>
                                     </div>
                                 </div>
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                                <button class="btn btn-block bg-btn-submit waves-effect" type="submit"><i class="material-icons">save</i><span>Save</span></button>
+                                <button class="btn btn-block bg-btn-submit waves-effect" type="submit"><i
+                                        class="material-icons">save</i><span>Save</span></button>
                             </div>
                         </div>
                     </form>
@@ -51,8 +56,8 @@
 </div>
 
 <script>
-    let modul_url       = '{{ $auth_data->modul_url }}';
-    let menu_url        = '{{ $auth_data->menu_url }}';
+    let modul_url = '{{ $auth_data->modul_url }}';
+    let menu_url = '{{ $auth_data->menu_url }}';
 
     $('select:not(.ms)').selectpicker();
     $('#form-validation').validate({
@@ -64,20 +69,20 @@
                 required: true
             }
         },
-        highlight: function (input) {
+        highlight: function(input) {
             $(input).parents('.form-group').addClass('error');
         },
-        unhighlight: function (input) {
+        unhighlight: function(input) {
             $(input).parents('.form-group').removeClass('error');
         },
-        errorPlacement: function (error, element) {
+        errorPlacement: function(error, element) {
             $(element).parents('.form-group').append(error);
         },
         submitHandler: function(form) {
             $('button').attr('disabled', 'disabled');
             let params = $(form).serializeArray();
             let load_url = modul_url + '/' + menu_url + '/detail';
-            $.each(params, function(i, field){
+            $.each(params, function(i, field) {
                 load_url += '/' + field.value;
             });
             loadURI(load_url);
