067fd25c274dbe7e2272e3a45326a75a4137911e
syahrule <syahrule01z@gmail.com>
2023-06-07 11:47:22 +0700

    Jurnal Harian Role BK

---
+++ resources/views/bk/jurnal-harian/tambah-jurnal-harian/add-tambah-jurnal-harian.blade.php
@@ -0,0 +1,150 @@
+<div class="container-fluid">
+    <div class="block-header">
+        <h2><a class="btn bg-blue waves-effect target-link"
+                href="{{ url(Request::segment(1) . '#' . Request::segment(2) . '/' . Request::segment(3)) }}"><i
+                    class="material-icons">backspace</i><span>Kembali</span></a></h2>
+    </div>
+    <div class="row clearfix">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="card">
+                <div class="header">
+                    <h2>
+                        TAMBAH JURNAL HARIAN
+                    </h2>
+                </div>
+                <div class="body">
+                    <form id="form-upload"
+                        method="POST"action="{{ url(Request::segment(1) . '/' . Request::segment(2) . '/' . Request::segment(3) . '/action-jurnal-harian/add/0') }}">
+                        {{ csrf_field() }}
+                        <h2 class="card-inside-title">
+                            Tanggal
+                        </h2>
+                        <div class="row clearfix">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <input type="text" class="datepicker form-control" name="tanggal" required=""
+                                    aria-required="true" aria-invalid="true" value="{{ $waktu }}">
+                            </div>
+                        </div>
+                        <h2 class="card-inside-title">
+                            Jenis Jurnal Harian
+                        </h2>
+                        <div class="row clearfix">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <textarea rows="1" cols="50" class="form-control" name="jenis" required="" aria-required="true"
+                                    aria-invalid="true"></textarea>
+                            </div>
+                        </div>
+
+                        <h2 class="card-inside-title">
+                            Status
+                        </h2>
+                        <div class="demo-radio-button">
+                            <input name="status" type="radio" value="1" id="target_1" />
+                            <label for="target_1">Selesai</label>
+                            <input name="status" type="radio" value="0" id="target_2" />
+                            <label for="target_2">Belum Selesai</label>
+                        </div>
+
+                        <h2 class="card-inside-title">
+                            File Pendukung ( pdf , pptx , docx , doc , xlsx , png , jpg , jpeg | max 5 mb )
+                        </h2>
+                        <div class="row clearfix">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <label>
+                                    <input type="file" class="form-control" name="file" />
+                                </label>
+                            </div>
+                        </div>
+                        <h2 class="card-inside-title">
+                            Uraian Kegiatan
+                        </h2>
+                        <div class="row clearfix">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                <textarea rows="4" cols="50" class="form-control" name="keterangan" required="" aria-required="true"
+                                    aria-invalid="true"></textarea>
+                            </div>
+                        </div>
+                        <div class="row clearfix">
+                            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            </div>
+                        </div>
+                        <div class="row clearfix">
+                            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                <button class="btn btn-block bg-red waves-effect" type="submit"><i
+                                        class="material-icons">save</i><span>Save</span></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@include('scriptjs')
+<script>
+    $(function() {
+        $('.datepicker').bootstrapMaterialDatePicker({
+            format: 'DD MMMM YYYY',
+            //lang : 'id',
+            clearButton: true,
+            weekStart: 1,
+            time: false
+        });
+    });
+</script>
+
+<script>
+    $('#form-upload').validate({
+        rules: {
+            'checkbox': {
+                required: true
+            },
+            'gender': {
+                required: true
+            }
+        },
+        highlight: function(input) {
+            $(input).parents('.form-group').addClass('error');
+        },
+        unhighlight: function(input) {
+            $(input).parents('.form-group').removeClass('error');
+        },
+        errorPlacement: function(error, element) {
+            $(element).parents('.form-group').append(error);
+        },
+        submitHandler: function(form) {
+            $('button').attr('disabled', 'disabled');
+            $.ajax({
+                url: form.action,
+                type: form.method,
+                enctype: 'multipart/form-data',
+                data: new FormData($('#form-upload')[0]),
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: function(response) {
+                    if (response.status == 200) {
+                        vex.dialog.alert(response.message);
+                    } else if (response.status == 201) {
+                        vex.dialog.alert(response.message);
+                        window.location.href = response.link;
+                    } else if (response.status == 202) {
+                        vex.dialog.alert(response.message);
+                        loadURI(response.path);
+                    } else if (response.status == 203) {
+                        vex.dialog.alert(response.message);
+                        primary_table.ajax.reload(null, false);
+                    } else if (response.status == 204) {
+                        loadURI(response.path);
+                    } else if (response.status == 300) {
+                        vex.dialog.alert(response.message);
+                    }
+                },
+                complete: function() {
+                    $('input').removeAttr('readonly', 'readonly');
+                    $('button').removeAttr('disabled');
+                }
+            });
+        }
+    });
+</script>
