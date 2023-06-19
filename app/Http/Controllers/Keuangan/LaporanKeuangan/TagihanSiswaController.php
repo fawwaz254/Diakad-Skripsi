0e197c6564b1bd12f52f3a1c38da04e337818fa6
@KhairilAmin <aminkhairil6@gmail.com>
2023-06-19 15:10:41 +0700

    Orderby Asc

--- app/Http/Controllers/Keuangan/LaporanKeuangan/TagihanSiswaController.php
+++ app/Http/Controllers/Keuangan/LaporanKeuangan/TagihanSiswaController.php
@@ -70,7 +70,7 @@ class TagihanSiswaController extends BaseController
             if ($status == 1) {
                 if (!empty($kelas)) {
                     if($kelas == 'all'){
-                        $list_data = $list_data->whereNotNull('id_kelas');
+                        $list_data = $list_data->whereNotNull('id_kelas')->orderBy("id_kelas", "asc");
                     }else{
                         $list_data = $list_data->where('id_kelas', $kelas);
                     }
