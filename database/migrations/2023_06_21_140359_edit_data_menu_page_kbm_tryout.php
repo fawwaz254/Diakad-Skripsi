c566b9b654f8d5fd5ad274dacd5e836da9982725
@KhairilAmin <aminkhairil6@gmail.com>
2023-06-21 14:57:04 +0700

    add page on table menu

---
+++ database/migrations/2023_06_21_140359_edit_data_menu_page_kbm_tryout.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Menu;
+
+
+class EditDataMenuPageKbmTryout extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $menu1 = Menu::where('nm_menu','Lihat Nilai KBM')->where('id_modul',13)->first();
+        $menu1->page = 'lihat-nilai-kbm';
+        $menu1->save();
+        $menu2 = Menu::where('nm_menu','Lihat Nilai KBM')->where('id_modul',38)->first();
+        $menu2->page = 'lihat-nilai-kbm';
+        $menu2->save();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
