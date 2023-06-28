d58119c0dfd626c63036b420464d12b76965bec8
syahrule <syahrule01z@gmail.com>
2023-06-28 10:44:11 +0700

    fix

--- resources/views/guru/welcome.blade.php
+++ resources/views/guru/welcome.blade.php
@@ -3,22 +3,22 @@
 @endphp
 <div class="container-fluid">
     <div class="block-header">
-        <h2>DASHBOARD | {{$today->format('d M Y')}}</h2>
+        <h2>DASHBOARD | {{ $today->format('d M Y') }}</h2>
     </div>
     <div class="row">
-        @if($role_dashboard)
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="card">
-                <div class="header">
-                    <h2>
-                        PENGUMUMAN
-                    </h2>
-                </div>
-                <div class="body">
-                {!!$role_dashboard->isi_dashboard!!}
+        @if ($role_dashboard)
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>
+                            PENGUMUMAN
+                        </h2>
+                    </div>
+                    <div class="body">
+                        {!! $role_dashboard->isi_dashboard !!}
+                    </div>
                 </div>
             </div>
-        </div>
         @endif
     </div>
     <br>
@@ -66,67 +66,70 @@
             </a>
         </div>
     </div> --}}
-    
+
 </div>
-    <div class="row">
-        @foreach (get_moduls() as $modul)
-                    @php
-                        $featuremenu = App\Models\Featuremenu::where('id_modul',$modul->id_modul)->first();
-                    @endphp
-                    @if ($featuremenu->is_aktif == 1)
-                        
-                    @if (auth_data()->sekolah_data->nm_singkat_sekolah !== 'smpypm2' && $modul->nm_modul=="Ketidaksesuaian SOP")
-                    
-                    @else
-                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 " data-toggle="modal" data-target=".bd-modal-lg-{{ $modul->route }}">
-                        <div class="card" style="margin: 1rem;">
-                            <div class="body bg-teal" style="text-align: -webkit-center;min-height: 26rem;">
-                                <img class="media-object" src="{{url('media/flaticon/'. $modul->route .'.png')}}" width="64" height="64">
-                                <h5>
-                                    {{ $modul->nm_modul }}
-                                </h5>
-                                <small>{{$featuremenu->deskripsi}}</small>
-                            </div>
+<div class="row">
+    @foreach (get_moduls() as $modul)
+        @php
+            $featuremenu = App\Models\Featuremenu::where('id_modul', $modul->id_modul)->first();
+        @endphp
+        @if (isset($featuremenu) && $featuremenu->is_aktif == 1)
+            @if (auth_data()->sekolah_data->nm_singkat_sekolah !== 'smpypm2' && $modul->nm_modul == 'Ketidaksesuaian SOP')
+            @else
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 " data-toggle="modal"
+                    data-target=".bd-modal-lg-{{ $modul->route }}">
+                    <div class="card" style="margin: 1rem;">
+                        <div class="body bg-teal" style="text-align: -webkit-center;min-height: 26rem;">
+                            <img class="media-object" src="{{ url('media/flaticon/' . $modul->route . '.png') }}"
+                                width="64" height="64">
+                            <h5>
+                                {{ $modul->nm_modul }}
+                            </h5>
+                            <small>{{ $featuremenu->deskripsi }}</small>
                         </div>
                     </div>
-                
-                    <div class="modal fade bd-modal-lg-{{ $modul->route }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                </div>
+
+                <div class="modal fade bd-modal-lg-{{ $modul->route }}" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                     <div class="modal-dialog modal-lg">
                         <div class="modal-content">
-                            <div class="row col-lg-12 d-flex justify-content-center" style="background-color:white;border-radus:1rem;">
+                            <div class="row col-lg-12 d-flex justify-content-center"
+                                style="background-color:white;border-radus:1rem;">
                                 <div class=" d-flex justify-content-center">
                                     @foreach ($modul->menus as $menu)
                                         {{-- @if ($menu->nm_menu == 'Tracer Alumni' && $detail_wali_kelas == null && $role_aktif !== 19 && $role_aktif !== 12) --}}
                                         {{-- @else --}}
-                                            <div id="menu-item-{{ $modul->route }}-{{ $menu->page }}" class="d-flex mx-auto submenu col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                                                <a class="target-link d-flex mx-auto" href="{{ url(Request::segment(1) . '#' . $modul->route . '/' . $menu->page) }}">
-                                                    <div class="card" style="margin: 1rem;">
-                                                        <div class="body bg-teal" style="text-align: -webkit-center;">
-                                                            <h5>
-                                                                {{ $menu->nm_menu }}
-                                                            </h5>
-                                                        </div>
+                                        <div id="menu-item-{{ $modul->route }}-{{ $menu->page }}"
+                                            class="d-flex mx-auto submenu col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                                            <a class="target-link d-flex mx-auto"
+                                                href="{{ url(Request::segment(1) . '#' . $modul->route . '/' . $menu->page) }}">
+                                                <div class="card" style="margin: 1rem;">
+                                                    <div class="body bg-teal" style="text-align: -webkit-center;">
+                                                        <h5>
+                                                            {{ $menu->nm_menu }}
+                                                        </h5>
                                                     </div>
-                                                </a>
-                                            </div>
+                                                </div>
+                                            </a>
+                                        </div>
                                         {{-- @endif --}}
                                     @endforeach
                                 </div>
                             </div>
                         </div>
                     </div>
-                    </div>
-                    @endif
-                    @else
-                    
-                    @endif
-                @endforeach
-    </div>
+                </div>
+            @endif
+        @else
+        @endif
+    @endforeach
+</div>
 </div>
 <script>
     $(document).ready(function() {
         $('div.submenu').click(function() {
-            $( "body" ).removeClass( "modal-open" );
+            $("body").removeClass("modal-open");
             $('.modal-backdrop').remove();
         });
     });
