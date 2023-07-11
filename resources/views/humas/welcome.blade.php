c12bd45c0a8c5c73261b0aefb49d0f38cee17258
@KhairilAmin <aminkhairil6@gmail.com>
2023-07-11 09:57:59 +0700

    Dashboard HUMAS

--- resources/views/humas/welcome.blade.php
+++ resources/views/humas/welcome.blade.php
@@ -5,6 +5,22 @@
     <div class="block-header">
         <h2>DASHBOARD | {{$today->format('d M Y')}}</h2>
     </div>
+    <div class="row">
+        @if ($role_dashboard)
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="header">
+                        <h2>
+                            PENGUMUMAN
+                        </h2>
+                    </div>
+                    <div class="body">
+                        {!! $role_dashboard->isi_dashboard !!}
+                    </div>
+                </div>
+            </div>
+        @endif
+    </div>
         <div class="row">
         {{-- <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
             <a class="target-link" href="{{url(Request::segment(1).'#kegiatan-harian/mengisi-form-kesehatan')}}">
