0462a1da4687333156fea53378d8ad170dff0a77
@KhairilAmin <aminkhairil6@gmail.com>
2023-08-03 15:52:49 +0700

    excel 1

--- app/Http/Controllers/Pendidikan/Wisuda/EntriWisudaController.php
+++ app/Http/Controllers/Pendidikan/Wisuda/EntriWisudaController.php
@@ -341,7 +341,7 @@ class EntriWisudaController extends BaseController
     }
     public function downloadExcel()
     {
-        $file = public_path() . "/excel/ContohFileExcelUploadNomorIjazahRevisi.xls";
+        $file = public_path() . "/excel/ContohFileExcelUploadNomorIjazahRevisi1.xls";
         ob_end_clean();
         ob_start(); 
         $headers = [
