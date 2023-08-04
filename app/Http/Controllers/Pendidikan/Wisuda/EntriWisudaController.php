cf1a7c5099a88eb0be2047920eefdea2882e0a27
@KhairilAmin <aminkhairil6@gmail.com>
2023-08-04 13:57:25 +0700

    Trigger excel2

--- app/Http/Controllers/Pendidikan/Wisuda/EntriWisudaController.php
+++ app/Http/Controllers/Pendidikan/Wisuda/EntriWisudaController.php
@@ -341,7 +341,7 @@ class EntriWisudaController extends BaseController
     }
     public function downloadExcel()
     {
-        $file = public_path() . "/excel/ContohFileExcelUploadNomorIjazahRevisi2.xls";
+        $file = public_path() . "/excel/ExcelTemplateIjazahSKKelulusan.xls";
         ob_end_clean();
         ob_start(); 
         $headers = [
