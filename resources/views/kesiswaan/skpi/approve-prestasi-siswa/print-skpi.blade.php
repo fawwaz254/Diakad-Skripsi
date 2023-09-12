472381f3e074c6111afda92bf8da777c327050af
Ega Alifya Firnando <egaalifyaf@gmail.com>
2023-09-12 21:46:28 +0700

    update: date signatures

--- resources/views/kesiswaan/skpi/approve-prestasi-siswa/print-skpi.blade.php
+++ resources/views/kesiswaan/skpi/approve-prestasi-siswa/print-skpi.blade.php
@@ -860,6 +860,8 @@
                     <td style="border: none;">Kab. Sidoarjo,
                         @if ($auth_data->sekolah_data->nm_singkat_sekolah == 'smawh2')
                         08 Mei 2023
+                        @elseif ($auth_data->sekolah_data->nm_singkat_sekolah == 'smkypm2')
+                        05 Mei 2023
                         @else
                         {{ indonesiaDate(\Carbon\Carbon::now()->format('Y-m-d')) }}
                         @endif
