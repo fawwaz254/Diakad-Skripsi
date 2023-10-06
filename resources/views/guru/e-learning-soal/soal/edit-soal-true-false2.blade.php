99402441bbd409856bcc67182e9672e116495e19
Rio Ramadhan <riordhn@gmail.com>
2023-10-06 08:46:17 +0700

    fixing bug edit soal true false dan match

--- resources/views/guru/e-learning-soal/soal/edit-soal-true-false2.blade.php
+++ resources/views/guru/e-learning-soal/soal/edit-soal-true-false2.blade.php
@@ -63,6 +63,7 @@
     </div>
 </div>
 
+@include('scriptjs')
 <script src="{{ asset('plugins/ckeditor/ckeditor.js') }}"></script>
 <script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
 <script>
@@ -73,75 +74,14 @@
         filebrowserUploadUrl: 'laravel-filemanager/upload?type=Files&_token='
     };
 
-    for (var i = 1; i < {{$no}}; i++) {
+    for (var i = 1; i < {{$x}}; i++) {
         id = 'q' + i;
         var editor = CKEDITOR.replace(id, options);
-        
+        timer = setInterval(updateDiv(id), 50);
     }
-</script>
 
-<script>
-    var primary_table = null;
-    $('#form-validation').validate({
-        rules: {
-            'checkbox': {
-                required: true
-            },
-            'gender': {
-                required: true
-            }
-        },
-        highlight: function(input) {
-            $(input).parents('.form-group').addClass('error');
-        },
-        unhighlight: function(input) {
-            $(input).parents('.form-group').removeClass('error');
-        },
-        errorPlacement: function(error, element) {
-            $(element).parents('.form-group').append(error);
-        },
-        submitHandler: function(form) {
-            for (var i = 1; i < {{$no}}; i++) {
-                id = 'q' + i;
-                CKEDITOR.instances[id].destroy();
-            }
-            
-            $('button').attr('disabled', 'disabled');
-            $.ajax({
-                processData: false,  // Important!
-                contentType: false,
-                cache: false,
-                url: form.action,
-                type: form.method,
-                data: new FormData($(form)[0]),
-                success: function(response) {
-                    if (response.status == 200) {
-                        vex.dialog.alert(response.message);
-                    } else if (response.status == 201) {
-                        vex.dialog.alert(response.message);
-                        window.location.href = response.link;
-                    } else if (response.status == 202) {
-                        vex.dialog.alert(response.message);
-                        setTimeout(() => {
-                            loadURI(response.path);
-                        }, 2000);
-                    } else if (response.status == 203) {
-                        vex.dialog.alert(response.message);
-                        primary_table.ajax.reload(null, false);
-                    } else if (response.status == 204) {
-                        loadURI(response.path);
-                    } else if (response.status == 205) {
-                        $('#modalMaster').modal('hide');
-                        vex.dialog.alert(response.message);
-                        primary_table.ajax.reload(null, false);
-                    } else if (response.status == 300) {
-                        vex.dialog.alert(response.message);
-                    }
-                },
-                complete: function() {
-                    $('button').removeAttr('disabled');
-                }
-            });
-        }
-    });
+    function updateDiv(id) {
+        let editorText = CKEDITOR.instances[id].getData();
+        $('#'+id).val(editorText);
+    }
 </script>
