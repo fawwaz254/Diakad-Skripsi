5e6f03a3ae79d0f1b9f94d77c22fb21e167d8c4b
syahrule <syahrule01z@gmail.com>
2023-10-27 10:41:47 +0700

    fix

--- app/Http/Controllers/Guru/RaporSisipan/RaporSisipanController.php
+++ app/Http/Controllers/Guru/RaporSisipan/RaporSisipanController.php
@@ -369,7 +369,7 @@ class RaporSisipanController extends Controller
             $query->where('aktif_status_pengguna', '=', '1');
         })
             ->whereHas('nilai_rapor_sisipan', function ($query) use ($id_rapor_sisipan) {
-                $query->where('id_rapor_sisipan', '=', $id_rapor_sisipan)->where('nilai', '!=', '0');
+                $query->where('id_rapor_sisipan', '=', $id_rapor_sisipan);
             })
             ->orderBy('nis_siswa')->get();
 
