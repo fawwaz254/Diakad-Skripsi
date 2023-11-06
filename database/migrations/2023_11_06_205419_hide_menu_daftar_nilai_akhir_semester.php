fd0633e130affd9b61b5caeae43eab8e2b5ce391
syahrule <syahrule01z@gmail.com>
2023-11-06 20:57:06 +0700

    hide menu daftar nilai akhir semester

---
+++ database/migrations/2023_11_06_205419_hide_menu_daftar_nilai_akhir_semester.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Menu;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class HideMenuDaftarNilaiAkhirSemester extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $menu = Menu::where('nm_menu', 'Daftar Nilai Akhir Semester')->get();
+        foreach ($menu as $m) {
+            $m->akses = '0';
+            $m->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
