f1728d081e91a5935c7d185f7d1773734262bd27
syahrule <syahrule01z@gmail.com>
2023-11-27 14:09:57 +0700

    fix sentry

--- resources/views/TracerAlumni2.blade.php
+++ resources/views/TracerAlumni2.blade.php
@@ -1,8 +1,8 @@
 <h2>Alumni yang bekerja</h2>
 <table class="table table-bordered">
 
-    <thead >
-        <tr >
+    <thead>
+        <tr>
             <td style="text-align: center; background-color: #d8d8d8;">No</td>
             <td style="background-color: #d8d8d8">Nama</td>
             <td style="background-color: #d8d8d8">Kelas</td>
@@ -16,38 +16,38 @@
             <td style="background-color: #d8d8d8">Alamat Instansi</td>
             <td style="background-color: #d8d8d8">Kontak Instansi</td>
             <td style="background-color: #d8d8d8">Bidang Usaha</td>
-           
 
-         
+
+
 
         </tr>
     </thead>
     <tbody>
-        @foreach ( $alumni['alumni_bekerja'] as $index =>  $detail_alumni )
-        <tr>
-            <td>{{ $index + 1 }}</td>
-            <td>{{ $detail_alumni->calon_siswa->nm_c_siswa }}</td>
-            <td>{{ $detail_alumni->kelas->nm_kelas }}</td>
-            <td>{{ $detail_alumni->calon_siswa->nomor_hp }}</td>
-            <td>{{ $detail_alumni->calon_siswa->alamat_jalan }}</td>
-            <td>{{ $detail_alumni->tahun_lulus }}</td>
-            <td>{{ $detail_alumni->status }}</td>
-            <td>{{ $detail_alumni->email }}</td>
-            <td>{{ $detail_alumni->url_medsos }}</td>
-            <td>{{ $detail_alumni->bekerja->nm_instansi }}</td>
-            <td>{{ $detail_alumni->bekerja->alamat_instansi }}</td>
-            <td>{{ $detail_alumni->bekerja->kontak_instansi }}</td>
-            <td>{{ $detail_alumni->bekerja->bidang_usaha_instansi }}</td>
-            
+        @foreach ($alumni['alumni_bekerja'] as $index => $detail_alumni)
+            <tr>
+                <td>{{ $index + 1 }}</td>
+                <td>{{ $detail_alumni->calon_siswa->nm_c_siswa }}</td>
+                <td>{{ isset($detail_alumni->kelas) ? $detail_alumni->kelas->nm_kelas : '' }}</td>
+                <td>{{ $detail_alumni->calon_siswa->nomor_hp }}</td>
+                <td>{{ $detail_alumni->calon_siswa->alamat_jalan }}</td>
+                <td>{{ $detail_alumni->tahun_lulus }}</td>
+                <td>{{ $detail_alumni->status }}</td>
+                <td>{{ $detail_alumni->email }}</td>
+                <td>{{ $detail_alumni->url_medsos }}</td>
+                <td>{{ $detail_alumni->bekerja->nm_instansi }}</td>
+                <td>{{ $detail_alumni->bekerja->alamat_instansi }}</td>
+                <td>{{ $detail_alumni->bekerja->kontak_instansi }}</td>
+                <td>{{ $detail_alumni->bekerja->bidang_usaha_instansi }}</td>
 
-        </tr>
+
+            </tr>
         @endforeach
     </tbody>
 </table>
 <br>
 <h2>Alumni yang kuliah</h2>
 <table class="table table-bordered">
-    <thead >
+    <thead>
         <tr>
             <td style="text-align: center; background-color: #d8d8d8">No</td>
             <td style="background-color: #d8d8d8">Nama</td>
@@ -64,30 +64,30 @@
             <td style="background-color: #d8d8d8">Prodi</td>
             <td style="background-color: #d8d8d8">Jenjang</td>
             <td style="background-color: #d8d8d8">Tahun masuk Perguruan</td>
-          
+
         </tr>
     </thead>
     <tbody>
-        @foreach ( $alumni['alumni_kuliah'] as $index =>  $detail_alumni )
-        <tr>
-            <td>{{ $index + 1 }}</td>
-            <td>{{ $detail_alumni->calon_siswa->nm_c_siswa }}</td>
-            <td>{{ $detail_alumni->kelas->nm_kelas }}</td>
-            <td>{{ $detail_alumni->calon_siswa->nomor_hp }}</td>
-            <td>{{ $detail_alumni->calon_siswa->alamat_jalan }}</td>
-            <td>{{ $detail_alumni->tahun_lulus }}</td>
-            <td>{{ $detail_alumni->status }}</td> 
-            <td>{{ $detail_alumni->email }}</td>
-            <td>{{ $detail_alumni->url_medsos }}</td>
-            <td>{{ $detail_alumni->kuliah->nm_perguruan }}</td>
-            <td>{{ $detail_alumni->kuliah->alamat_perguruan }}</td>
-            <td>{{ $detail_alumni->kuliah->fakultas }}</td>
-            <td>{{ $detail_alumni->kuliah->prodi }}</td>
-            <td>{{ $detail_alumni->kuliah->jenjang }}</td>
-            <td>{{ $detail_alumni->kuliah->tahun_masuk_perguruan}}</td>
-          
+        @foreach ($alumni['alumni_kuliah'] as $index => $detail_alumni)
+            <tr>
+                <td>{{ $index + 1 }}</td>
+                <td>{{ $detail_alumni->calon_siswa->nm_c_siswa }}</td>
+                <td>{{ $detail_alumni->kelas->nm_kelas }}</td>
+                <td>{{ $detail_alumni->calon_siswa->nomor_hp }}</td>
+                <td>{{ $detail_alumni->calon_siswa->alamat_jalan }}</td>
+                <td>{{ $detail_alumni->tahun_lulus }}</td>
+                <td>{{ $detail_alumni->status }}</td>
+                <td>{{ $detail_alumni->email }}</td>
+                <td>{{ $detail_alumni->url_medsos }}</td>
+                <td>{{ $detail_alumni->kuliah->nm_perguruan }}</td>
+                <td>{{ $detail_alumni->kuliah->alamat_perguruan }}</td>
+                <td>{{ $detail_alumni->kuliah->fakultas }}</td>
+                <td>{{ $detail_alumni->kuliah->prodi }}</td>
+                <td>{{ $detail_alumni->kuliah->jenjang }}</td>
+                <td>{{ $detail_alumni->kuliah->tahun_masuk_perguruan }}</td>
 
-        </tr>
+
+            </tr>
         @endforeach
     </tbody>
 </table>
@@ -95,7 +95,7 @@
 <h2>Alumni yang menunggu</h2>
 <table class="table table-bordered">
 
-    <thead >
+    <thead>
         <tr>
             <td style="text-align: center; background-color: #d8d8d8">No</td>
             <td style="background-color: #d8d8d8">Nama</td>
@@ -107,34 +107,34 @@
             <td style="background-color: #d8d8d8">Email Siswa</td>
             <td style="background-color: #d8d8d8">Url Medsos</td>
             <td style="background-color: #d8d8d8">Status Menunggu</td>
-            
+
 
         </tr>
     </thead>
     <tbody>
-        @foreach ( $alumni['alumni_menunggu'] as $index =>  $detail_alumni )
-        <tr>
-            <td>{{ $index + 1 }}</td>
-            <td>{{ $detail_alumni->calon_siswa->nm_c_siswa }}</td>
-            <td>{{ $detail_alumni->kelas->nm_kelas }}</td>
-            <td>{{ $detail_alumni->calon_siswa->nomor_hp }}</td>
-            <td>{{ $detail_alumni->calon_siswa->alamat_jalan }}</td>
-            <td>{{ $detail_alumni->tahun_lulus }}</td>
-            <td>{{ $detail_alumni->status }}</td> 
-            <td>{{ $detail_alumni->email }}</td>
-            <td>{{ $detail_alumni->url_medsos }}</td>
-            <td>{{ $detail_alumni->menunggu->status_menunggu }}</td>
-            
+        @foreach ($alumni['alumni_menunggu'] as $index => $detail_alumni)
+            <tr>
+                <td>{{ $index + 1 }}</td>
+                <td>{{ $detail_alumni->calon_siswa->nm_c_siswa }}</td>
+                <td>{{ $detail_alumni->kelas->nm_kelas }}</td>
+                <td>{{ $detail_alumni->calon_siswa->nomor_hp }}</td>
+                <td>{{ $detail_alumni->calon_siswa->alamat_jalan }}</td>
+                <td>{{ $detail_alumni->tahun_lulus }}</td>
+                <td>{{ $detail_alumni->status }}</td>
+                <td>{{ $detail_alumni->email }}</td>
+                <td>{{ $detail_alumni->url_medsos }}</td>
+                <td>{{ $detail_alumni->menunggu->status_menunggu }}</td>
 
-        </tr>
+
+            </tr>
         @endforeach
     </tbody>
 </table>
 <br>
 <h2>Alumni yang wirausaha</h2>
 <table class="table table-bordered">
- 
-    <thead >
+
+    <thead>
         <tr>
             <td style="text-align: center; background-color: #d8d8d8">No</td>
             <td style="background-color: #d8d8d8">Nama</td>
@@ -151,30 +151,30 @@
             <td style="background-color: #d8d8d8">Bidang usaha</td>
             <td style="background-color: #d8d8d8">Jumlah karyawan</td>
             <td style="background-color: #d8d8d8">Tahun rintis</td>
-           
+
         </tr>
     </thead>
     <tbody>
-        @foreach ( $alumni['alumni_wirausaha'] as $index =>  $detail_alumni )
-        <tr>
-            <td>{{ $index + 1 }}</td>
-            <td>{{ $detail_alumni->calon_siswa->nm_c_siswa }}</td>
-            <td>{{ $detail_alumni->kelas->nm_kelas }}</td>
-            <td>{{ $detail_alumni->calon_siswa->nomor_hp }}</td>
-            <td>{{ $detail_alumni->calon_siswa->alamat_jalan }}</td>
-            <td>{{ $detail_alumni->tahun_lulus }}</td>
-            <td>{{ $detail_alumni->status }}</td> 
-            <td>{{ $detail_alumni->email }}</td>
-            <td>{{ $detail_alumni->url_medsos }}</td>
-            <td>{{ $detail_alumni->usaha->nm_usaha }}</td>
-            <td>{{ $detail_alumni->usaha->alamat_usaha }}</td>
-            <td>{{ $detail_alumni->usaha->kontak_usaha }}</td>
-            <td>{{ $detail_alumni->usaha->bidang_usaha }}</td>
-            <td>{{ $detail_alumni->usaha->jumlah_karyawan }}</td>
-            <td>{{ $detail_alumni->usaha->tahun_rintis }}</td>
-            
+        @foreach ($alumni['alumni_wirausaha'] as $index => $detail_alumni)
+            <tr>
+                <td>{{ $index + 1 }}</td>
+                <td>{{ $detail_alumni->calon_siswa->nm_c_siswa }}</td>
+                <td>{{ $detail_alumni->kelas->nm_kelas }}</td>
+                <td>{{ $detail_alumni->calon_siswa->nomor_hp }}</td>
+                <td>{{ $detail_alumni->calon_siswa->alamat_jalan }}</td>
+                <td>{{ $detail_alumni->tahun_lulus }}</td>
+                <td>{{ $detail_alumni->status }}</td>
+                <td>{{ $detail_alumni->email }}</td>
+                <td>{{ $detail_alumni->url_medsos }}</td>
+                <td>{{ $detail_alumni->usaha->nm_usaha }}</td>
+                <td>{{ $detail_alumni->usaha->alamat_usaha }}</td>
+                <td>{{ $detail_alumni->usaha->kontak_usaha }}</td>
+                <td>{{ $detail_alumni->usaha->bidang_usaha }}</td>
+                <td>{{ $detail_alumni->usaha->jumlah_karyawan }}</td>
+                <td>{{ $detail_alumni->usaha->tahun_rintis }}</td>
 
-        </tr>
+
+            </tr>
         @endforeach
     </tbody>
 </table>
