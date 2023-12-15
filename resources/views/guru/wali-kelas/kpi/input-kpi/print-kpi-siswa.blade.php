0b8d38ee0d0283143beed6d0c0ac929020dd91bd
jagoanemakk <jagoanemak1234@gmail.com>
2023-12-15 13:17:29 +0700

    tanggal print input kpi role guru

--- resources/views/guru/wali-kelas/kpi/input-kpi/print-kpi-siswa.blade.php
+++ resources/views/guru/wali-kelas/kpi/input-kpi/print-kpi-siswa.blade.php
@@ -266,7 +266,7 @@ margin-left: auto;">
                 ____________________
             </td>
             <td width="30%" style="border-style : hidden; "></td>
-            <td width="30%" style="border-style : hidden;text-align:center ">Sidoarjo, 21 Desember 2023
+            <td width="30%" style="border-style : hidden;text-align:center ">Sidoarjo, 19 Desember 2023
                 {{-- {{ indonesiaDate(\Carbon\Carbon::now()->format('Y-m-d')) }} --}}
                 <br>
                 <br>
