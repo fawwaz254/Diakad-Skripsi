55094cae9ad2b7636e47d5063585e0c7fbf77180
syahrule <syahrule01z@gmail.com>
2023-12-15 14:09:24 +0700

    fix date rapor semester siti aminah

--- resources/views/akademik/rapor-semester/cetak-rapor/cetak-rapor-sitiaminah.blade.php
+++ resources/views/akademik/rapor-semester/cetak-rapor/cetak-rapor-sitiaminah.blade.php
@@ -274,7 +274,7 @@
                                 {{ $auth_data->sekolah_data->nm_kepala_sekolah }}</u></b>
                     </td>
                     <td style="width: 30%;">
-                        {{ 'Surabaya, ' . indonesiaDate(\Carbon\Carbon::now()->format('Y-m-d')) }}
+                        {{ 'Surabaya, 21 Desember 2023' }}
                         <br>
                         Wali Kelas
                         <br><br><br><br><br><br><br>
@@ -521,7 +521,7 @@
                         {{ $auth_data->sekolah_data->nm_kepala_sekolah }}</u></b>
             </td>
             <td style="width: 30%;">
-                {{ 'Surabaya, 21 DESEMBER 2023' }}
+                {{ 'Surabaya, 21 Desember 2023' }}
                 <br>
                 Wali Kelas
                 <br><br><br><br><br><br><br>
