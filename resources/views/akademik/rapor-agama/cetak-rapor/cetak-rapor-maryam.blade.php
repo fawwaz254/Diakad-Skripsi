91536ded10605e1a4d5e05e75d59bcf4e5a341b7
syahrule <syahrule01z@gmail.com>
2023-12-20 12:02:27 +0700

    Fix

--- resources/views/akademik/rapor-agama/cetak-rapor/cetak-rapor-maryam.blade.php
+++ resources/views/akademik/rapor-agama/cetak-rapor/cetak-rapor-maryam.blade.php
@@ -223,7 +223,7 @@
                                     <td style="text-align: center;">
                                         {{ isset($nilai_siswa[$siswa->id_siswa . $data2['id_mata_pelajaran'][0] . $list_komponen[1]->id_komponen_jenis_rapor . 'predikat']) ? $nilai_siswa[$siswa->id_siswa . $data2['id_mata_pelajaran'][0] . $list_komponen[1]->id_komponen_jenis_rapor . 'predikat'] : '' }}
                                     </td>
-                                    <td style="padding: 10px">
+                                    <td style="padding: 5px">
                                         {{ isset($nilai_siswa[$siswa->id_siswa . $data2['id_mata_pelajaran'][0] . $list_komponen[2]->id_komponen_jenis_rapor . 'nilai']) ? $nilai_siswa[$siswa->id_siswa . $data2['id_mata_pelajaran'][0] . $list_komponen[2]->id_komponen_jenis_rapor . 'nilai'] : '' }}
                                     </td>
                                     @php
