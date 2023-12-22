c302bf4f5e14c8eb5ee57577f7afa80e8272730c
Reza1290 <reza.muktasib@gmail.com>
2023-12-22 14:45:27 +0700

    update: +mutasi_aktif

--- app/Http/Controllers/Guru/WaliKelas/InputKPIController.php
+++ app/Http/Controllers/Guru/WaliKelas/InputKPIController.php
@@ -52,7 +52,7 @@ class InputKPIController extends Controller
         $semester_aktif = LibDataAkademik::fetchDataSemesterAktif($auth_data);
         $wali_kelas = LibGuru::fetchDataWaliKelasBySemester($auth_data, $guru->id_guru, $semester_aktif->id_semester);
         $list_data = Siswa::where('id_kelas', $wali_kelas->id_kelas)->with('pengguna')->whereHas('pengguna.status_pengguna', function ($q) {
-            $q->where('aktif_status_pengguna', 1)->where('nm_status_pengguna', 'AKTIF');
+            $q->where('aktif_status_pengguna', 1)->where('nm_status_pengguna', 'AKTIF')->ehere('nm_status_pengguna','MUTASI_MASUK');
         })->whereNotNull('id_kelas')->get()->unique('pengguna.nm_pengguna');
         
 
