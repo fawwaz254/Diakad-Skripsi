5d283ff24067f5218d254f44b57bc2b15f929ad3
Reza1290 <reza.muktasib@gmail.com>
2024-01-05 13:03:35 +0700

    add: menu wali kelas

---
+++ database/migrations/2024_01_05_122710_create_menu_wali_kelas.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Models\Modul;
+use App\Models\Role;
+use Carbon\Carbon;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuWaliKelas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $now = Carbon::now(env('APP_TIMEZONE', ''));
+
+        $role_id = Role::where('nm_role', 'Guru')->first()->id_role;
+
+        $modul = Modul::where('id_role', $role_id)->where('nm_modul', 'Wali Kelas')->first();
+
+        $modul->menus()->createMany([
+            [
+                "nm_menu"      => "Rekap Form",
+                "page"         => "rekap-form-wali",
+                "urutan"       => 15,
+                "akses"        => 1,
+                "created_at"   => $now
+            ],
+        ]);
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
