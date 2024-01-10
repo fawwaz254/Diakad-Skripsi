fe15b1b52b9cd2f719d530b80c4eb65bae818a6c
Reza1290 <reza.muktasib@gmail.com>
2024-01-10 14:19:27 +0700

    fix: formsisa guru

--- app/Http/Controllers/Siswa/FormSiswa/InputFormHarianController.php
+++ app/Http/Controllers/Siswa/FormSiswa/InputFormHarianController.php
@@ -185,9 +185,11 @@ class InputFormHarianController extends Controller
                     if ($input->jenis_pertanyaan[$key] == '1') {
                         $detail_jawaban_form->jawaban = $input->jawaban_pertanyaan[$key];
                     } elseif ($input->jenis_pertanyaan[$key] == '2') {
-                        $singkat_sekolah = $auth_data->sekolah_data->nm_singkat_sekolah;
-                        $file = Storage::disk('spaces')->putFile($singkat_sekolah . '/humas/' . $id, request()->jawaban_pertanyaan[$key], 'public');
-                        $detail_jawaban_form->jawaban = $file;
+                        if (!is_null(request()->jawaban_pertanyaan[$key]) && isset(request()->jawaban_pertanyaan[$key])) {
+                            $singkat_sekolah = $auth_data->sekolah_data->nm_singkat_sekolah;
+                            $file = Storage::disk('spaces')->putFile($singkat_sekolah . '/humas/' . $id, request()->jawaban_pertanyaan[$key], 'public');
+                            $detail_jawaban_form->jawaban = $file;
+                        }
                     } elseif ($input->jenis_pertanyaan[$key] == '3') {
                         $detail_jawaban_form->jawaban = $input->jawaban_pertanyaan[$key];
                     } elseif ($input->jenis_pertanyaan[$key] == '4') {
