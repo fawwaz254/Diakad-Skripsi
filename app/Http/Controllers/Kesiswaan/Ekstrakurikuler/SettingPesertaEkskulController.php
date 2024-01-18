b2565fc5f9f34165e246c48a75cd884778814d2a
aminudin bahtiar <aminudinbahtiar101@gmail.com>
2024-01-18 03:20:11 +0000

    Update SettingPesertaEkskulController.php

--- app/Http/Controllers/Kesiswaan/Ekstrakurikuler/SettingPesertaEkskulController.php
+++ app/Http/Controllers/Kesiswaan/Ekstrakurikuler/SettingPesertaEkskulController.php
@@ -291,7 +291,7 @@ class SettingPesertaEkskulController extends BaseController
                     return [
                         'status' => 202, // SUCCESS AND LOAD CONTENT
                         'message' => 'Input Peserta Ekskul Berhasil Dilakukan',
-                       //'path' => 'ekstrakurikuler/setting-peserta-ekskul/'//
+                        'path' => 'ekstrakurikuler/setting-peserta-ekskul/'
                     ];
                 } catch (\Exception $e) {
                     DB::rollback();
@@ -428,7 +428,7 @@ class SettingPesertaEkskulController extends BaseController
                     return [
                         'status' => 202, // SUCCESS AND LOAD CONTENT
                         'message' => 'Setting Pengambilan Ekskul Berhasil',
-                        'path' => 'ekstrakurikuler/setting-peserta-ekskul/setting/' . $id_ekskul
+                       // 'path' => 'ekstrakurikuler/setting-peserta-ekskul/setting/' . $id_ekskul //
                     ];
                 } catch (\Exception $e) {
                     DB::rollback();
