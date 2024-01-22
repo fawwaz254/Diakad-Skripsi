a5f4be02b28adb3b2a189b074033a7e40434696c
syahrule <syahrule01z@gmail.com>
2024-01-22 10:44:02 +0700

    remove status online offline fingerprint

--- resources/views/administrator/device/fingerprint/view-data-fingerprint.blade.php
+++ resources/views/administrator/device/fingerprint/view-data-fingerprint.blade.php
@@ -26,7 +26,7 @@
                                     <th>No</th>
                                     <th>Nama FP</th>
                                     <th>SN</th>
-                                    <th>Status</th>
+                                    {{-- <th>Status</th> --}}
                                     <th>WAN</th>
                                     <th>LAN</th>
                                     <th>PORT</th>
@@ -70,11 +70,13 @@
                 data: 'sn',
                 searchable: false,
                 orderable: false
-            }, {
-                data: 'status',
-                searchable: false,
-                orderable: false
-            }, {
+            },
+            // {
+            //     data: 'status',
+            //     searchable: false,
+            //     orderable: false
+            // },
+            {
                 data: 'ip_address_wan',
                 searchable: false,
                 orderable: false
