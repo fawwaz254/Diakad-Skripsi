293ee665a330a9537d40dafe393c11c1889b9bb5
Reza1290 <reza.muktasib@gmail.com>
2024-01-31 13:06:04 +0700

    add: absensi magang in wali murid

---
+++ database/migrations/2024_01_31_125654_create_menu_absensi_magang_in_wali_murid_modul_kesiswaan.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Menu;
+use App\Models\Modul;
+use App\Models\Sekolah;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMenuAbsensiMagangInWaliMuridModulKesiswaan extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $sekolah_now = Sekolah::first();
+        if(str_contains($sekolah_now->nm_sekolah,'SMK')){
+            $modul = Modul::where('nm_modul', 'Kesiswaan')->where('id_role',4)->first();
+            $menu1 = new Menu();
+            $menu1->id_modul = $modul->id_modul;
+            $menu1->nm_menu = 'Presensi Magang';
+            $menu1->page = 'absensi-magang';
+            $menu1->urutan = 6;
+            $menu1->akses = 1;
+            $menu1->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
