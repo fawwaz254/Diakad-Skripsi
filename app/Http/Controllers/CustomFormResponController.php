3f426c761b79b7d09bcedbd1bafa53f28cb761c7
Rio Ramadhan <riordhn@gmail.com>
2024-03-04 11:29:33 +0700

    fix: migration custom form

--- app/Http/Controllers/CustomFormResponController.php
+++ app/Http/Controllers/CustomFormResponController.php
@@ -149,7 +149,8 @@ class CustomFormResponController extends Controller
             }, 'role'])
                 ->whereIn('id_role', $auth_data->pengguna->role_pengguna->pluck('id_role'))
                 ->findOrFail($id);
-            if (isset($form->form_settings['limit']) && $form->form_settings['limit'] == 'true') {
+            $form_settings = json_decode($form->form_settings);
+            if (isset($form_settings['limit']) && $form_settings['limit'] == 'true') {
                 $sheets = CustomFormSheet::where('created_by', $auth_data->pengguna->id_pengguna)->where('id_custom_form', $id)->first();
                 if ($sheets) {
                     throw new Exception('Form ' . $form->nm_custom_form . ' Hanya Menerima 1 Respon');
@@ -221,7 +222,9 @@ class CustomFormResponController extends Controller
             $form = CustomForm::findOrFail($input->id_custom_form);
 
             // Biar aman ~
-            if (isset($form->form_settings['limit']) && $form->form_settings['limit'] == 'true') {
+            $form_settings = json_decode($form->form_settings);
+
+            if (isset($form_settings['limit']) && $form_settings['limit'] == 'true') {
                 $sheets = CustomFormSheet::where('created_by', $auth_data->pengguna->id_pengguna)->where('id_custom_form', $form->id_custom_form)->first();
                 if ($sheets) {
                     return [
@@ -345,7 +348,8 @@ class CustomFormResponController extends Controller
             $form = CustomFormSheet::with('form.form_komponen', 'form_respon.form_komponen')->where('created_by', $auth_data->pengguna->id_pengguna)->findOrFail($id);
             $startTime = Carbon::createFromFormat('Y-m-d H:i:s', $form->form->start_time);
             $endTime = Carbon::createFromFormat('Y-m-d H:i:s', $form->form->end_time);
-            if ($form->form->form_settings['editable'] !== 'true') {
+            $form_settings = json_decode($form->form->form_settings);
+            if ($form_settings['editable'] !== 'true') {
                 throw new Exception('Form Ini Tidak Dapat Diubah');
             }
             $time_now = $now->toTimeString();
--- app/Http/Controllers/Humas/FormBuilder/CustomFormController.php
+++ app/Http/Controllers/Humas/FormBuilder/CustomFormController.php
@@ -32,7 +32,7 @@ class CustomFormController extends Controller
             $code = $code . $character;
         }
 
-        if (CustomForm::whereJsonContains('form_settings->kode', $code)->exists()) {
+        if (CustomForm::where('form_settings', 'like', '%'.$code.'%')->exists()) {
             $this->customFormCodeGenerator();
         }
 
@@ -62,9 +62,10 @@ class CustomFormController extends Controller
                 return $data;
             })
             ->addColumn('kode',function($item){
+                $form_settings = json_decode($item->form_settings);
                 return [
-                    'ada' => isset($item->form_settings['kode']),
-                    'kode' => $item->form_settings['kode'] ?? ''
+                    'ada' => isset($form_settings['kode']),
+                    'kode' => $form_settings['kode'] ?? ''
                 ];
             })
             ->make(true);
@@ -131,7 +132,7 @@ class CustomFormController extends Controller
                 $custom_form->is_aktif          = $input->is_aktif;
                 $custom_form->start_time        = $input->start_time;
                 $custom_form->end_time          = $input->end_time;
-                $custom_form->form_settings     = $rule;
+                $custom_form->form_settings     = json_encode($rule);
                 $custom_form->created_by        = $auth_data->pengguna->id_pengguna;
                 $custom_form->save();
 
@@ -250,7 +251,7 @@ class CustomFormController extends Controller
                 $custom_form->jenis_custom_form = $input->jenis_custom_form;
                 $custom_form->start_time        = $input->start_time;
                 $custom_form->end_time          = $input->end_time;
-                $custom_form->form_settings     = $rule;
+                $custom_form->form_settings     = json_encode($rule);
                 $custom_form->created_by        = $auth_data->pengguna->id_pengguna;
                 $custom_form->save();
 
