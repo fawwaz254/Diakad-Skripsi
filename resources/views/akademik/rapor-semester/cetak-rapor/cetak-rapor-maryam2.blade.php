86ddda870b6aee5a1dbb5191965e1cdad3dae178
Ega Alifya Firnando <egaalifyaf@gmail.com>
2024-04-18 14:15:23 +0700

    update: date rapor sma maryam

--- resources/views/akademik/rapor-semester/cetak-rapor/cetak-rapor-maryam2.blade.php
+++ resources/views/akademik/rapor-semester/cetak-rapor/cetak-rapor-maryam2.blade.php
@@ -317,7 +317,7 @@
 
                     </td>
                     <td style="width: 30%;">
-                        {{ 'Surabaya, 26 April 2024' }}
+                        {{ 'Surabaya, 6 Mei 2024' }}
                         <br>
                         Wali Kelas
                         <br><br><br><br><br><br><br>
