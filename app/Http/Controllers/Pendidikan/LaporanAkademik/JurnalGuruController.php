35ff00cd9064270cf7d8ee265da1638062abfb86
Dwi Wahyu Prambodo <dwiwahyu1235@gmail.com>
2024-07-03 14:45:41 +0700

    add print pdf presensi mp on jurnal kelas and guru role pendidikan

--- app/Http/Controllers/Pendidikan/LaporanAkademik/JurnalGuruController.php
+++ app/Http/Controllers/Pendidikan/LaporanAkademik/JurnalGuruController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Pendidikan\LaporanAkademik;
 
+use App\Models\KelasMp;
+use App\Models\PengampuMp;
+use App\Models\PresensiMp;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
@@ -17,12 +20,14 @@ use DB;
 use Session;
 use Validator;
 
-class JurnalGuruController extends BaseController{
+class JurnalGuruController extends BaseController
+{
 
     protected $modul_url = 'laporan-akademik';
     protected $menu_url = 'jurnal-guru';
 
-    public function viewJurnalGuru(Request $request,$id_guru = null ,$id_semester = null){
+    public function viewJurnalGuru(Request $request, $id_guru = null, $id_semester = null)
+    {
         # code...
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
@@ -36,18 +41,19 @@ class JurnalGuruController extends BaseController{
         }
 
         $data_guru = Guru::join('pengguna', 'pengguna.id_pengguna', '=', 'guru.id_pengguna')
-                        ->where('pengguna.id_sekolah', '=', $auth_data->pengguna->id_sekolah)
-                        ->get();
+            ->where('pengguna.id_sekolah', '=', $auth_data->pengguna->id_sekolah)
+            ->get();
         $selected_guru = null;
-        if(!empty($id_guru)) {
-            $selected_guru = LibGuru::fetchDataAllGuru($auth_data,$id_guru);
+        if (!empty($id_guru)) {
+            $selected_guru = LibGuru::fetchDataAllGuru($auth_data, $id_guru);
         }
 
-    	return view('pendidikan/laporan-akademik/jurnal-guru/view-jurnal-guru',compact('auth_data','selected_guru','selected_semester','data_semester','data_guru'));
+        return view('pendidikan/laporan-akademik/jurnal-guru/view-jurnal-guru', compact('auth_data', 'selected_guru', 'selected_semester', 'data_semester', 'data_guru'));
 
     }
 
-    public function datatablesJurnalGuru(Request $request,$id_guru,$id_semester){
+    public function datatablesJurnalGuru(Request $request, $id_guru, $id_semester)
+    {
 
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
@@ -56,22 +62,53 @@ class JurnalGuruController extends BaseController{
         $list_data = LibGuru::fetchDataJadwalKBM($auth_data, $guru->id_pengguna, $id_semester);
 
         return Datatables::of($list_data)
-                ->addColumn('mata_pelajaran', function($item){
-                    return $item->kd_mata_pelajaran." - ".$item->nm_mata_pelajaran;
-                })
-                ->addColumn('jadwal_jam', function($item){
-                    return $item->jam_mulai.":".$item->menit_mulai." - ".$item->jam_selesai.":".$item->menit_selesai;
-                })
-                ->addColumn('status_pjmp', function($item){
-                    if($item->pjmp_pengampu_mp == 1) {
-                        return "PJMP";
-                    }
-                    else {
-                        return "Anggota";
-                    }
-                })
-                ->make(true);
+            ->addColumn('mata_pelajaran', function ($item) {
+                return $item->kd_mata_pelajaran . " - " . $item->nm_mata_pelajaran;
+            })
+            ->addColumn('jadwal_jam', function ($item) {
+                return $item->jam_mulai . ":" . $item->menit_mulai . " - " . $item->jam_selesai . ":" . $item->menit_selesai;
+            })
+            ->addColumn('status_pjmp', function ($item) {
+                if ($item->pjmp_pengampu_mp == 1) {
+                    return "PJMP";
+                } else {
+                    return "Anggota";
+                }
+            })
+            ->addColumn('action', function ($item) {
+                $data = array(
+                    'id_kelas_mp' => $item->id_kelas_mp,
+                );
+                return $data;
+            })
+            ->make(true);
 
+    }
+
+    public function printPdfJurnalGuru(Request $request, $id_kelas_mp)
+    {
+        $input = (object) $request->input();
+        $auth_data = $input->auth_data;
+
+        $kelas_mp = KelasMp::with('mata_pelajaran', 'kelas')->where('id_kelas_mp', $id_kelas_mp)->first();
+        $presensi_mp = PresensiMp::with(['kelas_mp', 'jadwal_kelas_mp.jadwal_hari'])->where('id_kelas_mp', $id_kelas_mp)->get();
+
+        $data_pertemuan = [];
+
+        for ($i = 1; $i <= 25; $i++) {
+            $presensiMp = $presensi_mp->firstWhere('pertemuan_ke', $i);
+            $data_pertemuan[] = [
+                'pertemuan_ke' => $i,
+                'presensi' => $presensiMp,
+                'hari' => $presensiMp ? $presensiMp->jadwal_kelas_mp->jadwal_hari->nm_jadwal_hari : '-',
+            ];
+        }
+
+        return view('pendidikan/laporan-akademik/jurnal-guru/print-jurnal-guru', [
+            'auth_data' => $auth_data,
+            'kelas_mp' => $kelas_mp,
+            'data_pertemuan' => $data_pertemuan
+        ]);
     }
 
 }
