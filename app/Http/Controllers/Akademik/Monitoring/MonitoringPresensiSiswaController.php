0d86a25e130c930345ebdb96ad1709371334ee6a
Dwi Wahyu Prambodo <dwiwahyu1235@gmail.com>
2024-09-12 10:55:17 +0700

    optimaze monitoring presesnsi siswa on role akademik

--- app/Http/Controllers/Akademik/Monitoring/MonitoringPresensiSiswaController.php
+++ app/Http/Controllers/Akademik/Monitoring/MonitoringPresensiSiswaController.php
@@ -30,6 +30,29 @@ class MonitoringPresensiSiswaController extends Controller
             $tahun = $now->year;
         }
 
+        $bulan = Bulan::find($id_bulan);
+        $data_bulan = Bulan::orderBy('id_bulan')->get();
+
+
+        $data_kelas = Kelas::where('is_aktif', '1')->orderBy('tingkat', 'asc')->orderBy('nm_kelas', 'asc')->get();
+
+        return view('akademik/monitoring/view-monitoring-presensi-siswa', compact('auth_data', 'data_bulan', 'bulan', 'tahun', 'data_kelas', 'id_kelas'));
+    }
+
+    public function viewDetailMonitoringPresensiSiswa(Request $request, $id_kelas = null, $id_bulan = null, $tahun = null)
+    {
+        $input = (object) $request->input();
+        $auth_data = $input->auth_data;
+
+        $now = Carbon::today();
+        if (empty($id_bulan)) {
+            $id_bulan = $now->month;
+        }
+
+        if (empty($tahun)) {
+            $tahun = $now->year;
+        }
+
         $start_month = Carbon::create($tahun, $id_bulan, 1, 0, 0, 0, 'Asia/Jakarta');
         $end_month = Carbon::create($tahun, $id_bulan, 1, 23, 59, 0, 'Asia/Jakarta')->endOfMonth();
         $dates = CarbonPeriod::create($start_month, $end_month);
@@ -38,7 +61,6 @@ class MonitoringPresensiSiswaController extends Controller
         $data_bulan = Bulan::orderBy('id_bulan')->get();
 
         $data_siswa = Siswa::where('id_kelas', $id_kelas)->with('pengguna')->get();
-
         $data_kelas = Kelas::where('is_aktif', '1')->orderBy('tingkat', 'asc')->orderBy('nm_kelas', 'asc')->get();
 
         $data_presensi = PresensiMp::with('presensi_mp_siswa')
@@ -60,7 +82,7 @@ class MonitoringPresensiSiswaController extends Controller
             }
         }
 
-
-        return view('akademik/monitoring/view-monitoring-presensi-siswa', compact('auth_data', 'dates', 'data_bulan', 'bulan', 'tahun', 'data_kelas', 'data_siswa', 'data_array_presensi', 'id_kelas'));
+        return view('akademik/monitoring/view-monitoring-presensi-siswa-detail', compact('auth_data', 'dates', 'data_bulan', 'bulan', 'tahun', 'data_siswa', 'data_kelas', 'data_array_presensi', 'id_kelas'));
     }
 }
+
