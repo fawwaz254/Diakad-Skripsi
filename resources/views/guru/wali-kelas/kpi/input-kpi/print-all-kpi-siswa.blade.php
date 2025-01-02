9e660c5b49d9c14aaa37d09f4188f896c5051c01
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2025-01-02 13:32:39 +0700

    change size F4

--- resources/views/guru/wali-kelas/kpi/input-kpi/print-all-kpi-siswa.blade.php
+++ resources/views/guru/wali-kelas/kpi/input-kpi/print-all-kpi-siswa.blade.php
@@ -36,9 +36,7 @@
         }
 
         .page {
-            /* width: 1200px; */
-            width: 210mm;
-            height: 329mm;
+            width: 1200px;
         }
 
         .body {
@@ -62,7 +60,7 @@
     <style type="text/css" media="print">
         @page {
             /* margin: 125mm 125mm 125mm 125mm;    */
-            size: portrait;
+            /* size: portrait; */
             size: 210mm 330mm;
             margin: 0mm;
         }
