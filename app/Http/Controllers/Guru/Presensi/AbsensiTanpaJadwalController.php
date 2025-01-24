0f238d0a096561cd6dca0ebd51d8fe54b7e3ddf7
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2025-01-24 17:08:24 +0700

    fix undefined id_guru pada SetJadwalKelas dan add where id_semester pada AbsensiTanpaJadwal

--- app/Http/Controllers/Guru/Presensi/AbsensiTanpaJadwalController.php
+++ app/Http/Controllers/Guru/Presensi/AbsensiTanpaJadwalController.php
@@ -52,8 +52,9 @@ class AbsensiTanpaJadwalController extends BaseController
         $jadwal_kelas_mp = JadwalKelasMp::with('kelas_mp')
             ->join('kelas_mp', 'jadwal_kelas_mp.id_kelas_mp', '=', 'kelas_mp.id_kelas_mp')
             ->join('pengampu_mp', 'pengampu_mp.id_kelas_mp', '=', 'kelas_mp.id_kelas_mp')
-            ->where('jadwal_kelas_mp.id_jadwal_hari', '=', '0')
-            ->where('pengampu_mp.id_guru', '=', $id_guru)
+            ->where('id_semester', $semester_aktif->id_semester)
+            ->where('jadwal_kelas_mp.id_jadwal_hari',  '0')
+            ->where('pengampu_mp.id_guru', $id_guru)
             ->get();
 
         $kelas = LibKelas::fetchDataKelas($auth_data, $id = null);
