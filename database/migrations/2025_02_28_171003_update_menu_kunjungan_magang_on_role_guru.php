c0769f17daf96c50cc51c65311da80c2783ff312
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2025-02-28 17:18:07 +0700

    change page kunjungan magang guru

---
+++ database/migrations/2025_02_28_171003_update_menu_kunjungan_magang_on_role_guru.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Menu;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateMenuKunjunganMagangOnRoleGuru extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $menu = Menu::where('nm_menu', 'Kunjungan magang')
+            ->where('page', 'add-kunjungan-magang')
+            ->get();
+
+        $menu->update([
+            'page' => 'list-kunjungan-magang',
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
