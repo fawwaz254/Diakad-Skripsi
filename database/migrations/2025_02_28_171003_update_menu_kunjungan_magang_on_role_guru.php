12ded28ff6eff777f6a614d9c623991376a7d611
Muhamad Matin Anugrah Pujayanto <m.matinanugrah.p2@gmail.com>
2025-03-03 15:50:27 +0700

    fix menu not found

--- database/migrations/2025_02_28_171003_update_menu_kunjungan_magang_on_role_guru.php
+++ database/migrations/2025_02_28_171003_update_menu_kunjungan_magang_on_role_guru.php
@@ -14,13 +14,16 @@ class UpdateMenuKunjunganMagangOnRoleGuru extends Migration
      */
     public function up()
     {
-        $menu = Menu::where('nm_menu', 'Kunjungan magang')
-            ->where('page', 'add-kunjungan-magang')
-            ->get();
+        $menu = Menu::where('page', 'add-kunjungan-magang')
+            ->first();
 
-        $menu->update([
-            'page' => 'list-kunjungan-magang',
-        ]);
+        if (!$menu) {
+            echo "Menu sudah di migrattion" . "\n";
+        } else {
+            $menu->update([
+                'page' => 'list-kunjungan-magang',
+            ]);
+        }
     }
 
     /**
