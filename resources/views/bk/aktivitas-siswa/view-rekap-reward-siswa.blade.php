8ac0c11f00e734e51c61ac7ae74906c1648d4c0c
misbaach <misbachanwar4@gmail.com>
2025-04-23 16:44:39 +0700

    feat: filter detail reward dan frekuensi aktivitas

--- resources/views/bk/aktivitas-siswa/view-rekap-reward-siswa.blade.php
+++ resources/views/bk/aktivitas-siswa/view-rekap-reward-siswa.blade.php
@@ -13,10 +13,10 @@
             <select class="form-control show-tick" name="kelas" required="">
               <option value="" selected disabled>-- Pilih Kelas --</option>
               @foreach ($data_kelas as $kelas)
-                <option value="{{ $kelas->id_kelas }}"
-                  {{ Request::input('kelas') == $kelas->id_kelas ? 'selected' : '' }}>
-                  {{ $kelas->nm_kelas }}</option>
-              @endforeach
+          <option value="{{ $kelas->id_kelas }}" {{ Request::input('kelas') == $kelas->id_kelas ? 'selected' : '' }}>
+          {{ $kelas->nm_kelas }}
+          </option>
+        @endforeach
             </select>
           </div>
           <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
@@ -26,10 +26,10 @@
             <select class="form-control show-tick" name="semester" required="" onchange="">
               <option value="" selected disabled>-- Pilih Semester --</option>
               @foreach ($data_semester as $semester)
-                <option value="{{ $semester->id_semester }}"
-                  {{ Request::input('semester') == $semester->id_semester ? 'selected' : '' }}>
-                  {{ $semester->nm_semester . ' ' . $semester->tahun_ajaran }}</option>
-              @endforeach
+          <option value="{{ $semester->id_semester }}" {{ Request::input('semester') == $semester->id_semester ? 'selected' : '' }}>
+          {{ $semester->nm_semester . ' ' . $semester->tahun_ajaran }}
+          </option>
+        @endforeach
             </select>
           </div>
         </div>
@@ -46,101 +46,143 @@
   </div>
   @if (!empty(Request::input('kelas')) && !empty(Request::input('semester')))
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-      <div class="card">
-        <div class="header">
-          <h2>Rekap Reward Siswa</h2>
-        </div>
-        <div class="body">
-          <div class="table-responsive">
-            <table class="table table-bordered table-striped table-hover dataTable display responsive nowrap"
-              id="table_rekap_reward">
-              <thead>
-                <tr>
-                  <th style="vertical-align : middle;text-align:center;">ID Siswa</th>
-                  <th style="vertical-align : middle;text-align:center;">No</th>
-                  <th style="vertical-align : middle;text-align:center;">Nama Siswa</th>
-                  @foreach ($data_aktivitas_reward_siswa as $aktivitas)
-                    <th style="vertical-align : middle;text-align:center;">{{ $aktivitas->nilai_karakter }}</th>
-                  @endforeach
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($data_siswa as $siswa)
-                  <tr>
-                    <td>{{ $siswa->id_siswa }}</td>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $siswa->nm_pengguna }}</td>
-                    @foreach ($data_aktivitas_reward_siswa as $aktivitas)
-                      @php
-                        $data = $data_reward
-                            ->where('id_siswa', $siswa->id_siswa)
-                            ->where('nilai_karakter', $aktivitas->nilai_karakter);
-                      @endphp
-                      @if ($data)
-                        <td style="text-align:center;">
-                          {{ $data->sum('total_point') }}
-                        </td>
-                      @else
-                        <td style="text-align:center;">0</td>
-                      @endif
-                    @endforeach
-                  </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </div>
-        </div>
+    <div class="card">
+      <div class="header">
+      <h2>Rekap Reward Siswa</h2>
       </div>
+      <div class="body">
+      <div class="table-responsive">
+        <table class="table table-bordered table-striped table-hover dataTable display responsive nowrap"
+        id="table_rekap_reward">
+        <thead>
+          <tr>
+          <th style="vertical-align : middle;text-align:center;">No</th>
+          <th style="vertical-align : middle;text-align:center;">NIS</th>
+          <th style="vertical-align : middle;text-align:center;">Nama Siswa</th>
+          @foreach ($data_aktivitas_reward_siswa as $aktivitas)
+        <th style="vertical-align : middle;text-align:center;">{{ $aktivitas->nilai_karakter }}</th>
+      @endforeach
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($data_siswa as $siswa)
+          <tr>
+          <td>{{ $loop->iteration }}</td>
+          <td>{{ $siswa->nis_siswa }}</td>
+          <td>{{ $siswa->nm_pengguna }}</td>
+          @foreach ($data_aktivitas_reward_siswa as $aktivitas)
+          @php
+      $data = $data_reward
+      ->where('id_siswa', $siswa->id_siswa)
+      ->where('nilai_karakter', $aktivitas->nilai_karakter);
+    @endphp
+          @if ($data)
+        <td style="text-align:center;">
+        <a href="#" style="color: black; text-decoration: underline" data-toggle="modal"
+        data-target="#modalDetailReward" data-id="{{ $siswa->id_siswa }}" data-nis="{{ $siswa->nis_siswa }}"
+        data-nama="{{ $siswa->nm_pengguna }}" data-karakter="{{ $aktivitas->nilai_karakter }}">
+        {{ $data->sum('total_point') }}
+        </a>
+        </td>
+      @else
+      <td style="text-align:center;">
+      <a href="#" class="btn btn-xs btn-info" data-toggle="modal" data-target="#modalDetailReward"
+      data-id="{{ $siswa->id_siswa }}" data-nama="{{ $siswa->nm_pengguna }}">
+      Lihat Detail
+      </a>
+      </td>
+    @endif
+      @endforeach
+          </tr>
+      @endforeach
+        </tbody>
+        </table>
+      </div>
+      </div>
+    </div>
     </div>
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-      <div class="card">
-        <div class="header">
-          <h2>Rekap Presensi Siswa</h2>
+    <div class="card">
+      <div class="header">
+      <h2>Rekap Presensi Siswa</h2>
+      </div>
+      <div class="body">
+      <div class="table-responsive">
+        <table class="table table-bordered table-striped table-hover dataTable display responsive nowrap"
+        id="table_rekap_reward">
+        <thead>
+          <tr>
+          <th style="vertical-align : middle;text-align:center;">No</th>
+          <th style="vertical-align : middle;text-align:center;">NIS</th>
+          <th style="vertical-align : middle;text-align:center;">Nama Siswa</th>
+          @foreach ($data_aktivitas_reward_siswa_presensi as $aktivitas)
+        <th style="vertical-align : middle;text-align:center;">{{ $aktivitas }}</th>
+      @endforeach
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($data_siswa as $siswa)
+          <tr>
+          <td>{{ $loop->iteration }}</td>
+          <td>{{ $siswa->nis_siswa }}</td>
+          <td>{{ $siswa->nm_pengguna }}</td>
+          @foreach ($data_aktivitas_reward_siswa_presensi as $aktivitas)
+          @php
+      $data = $data_presensi
+      ->where('id_kelas', $siswa->id_siswa)
+      ->where('nm_reward_siswa', $aktivitas)
+      ->count('id_kelas_mp');
+    @endphp
+          @if ($data)
+        <td style="text-align:center;">
+        {{ $data }}
+        </td>
+      @else
+      <td style="text-align:center;">0</td>
+    @endif
+      @endforeach
+          </tr>
+      @endforeach
+        </tbody>
+        </table>
+      </div>
+      </div>
+    </div>
+    </div>
+  @endif
+  <!-- Modal -->
+  <div class="modal fade" id="modalDetailReward" tabindex="-1" role="dialog" aria-labelledby="modalLabelReward"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title" id="modalLabelReward">Detail Reward Siswa</h4>
         </div>
-        <div class="body">
+        <div class="modal-body">
+          <p><strong>NIS:</strong> <span id="modalNISSiswa"></span></p>
+          <p><strong>Nama:</strong> <span id="modalNamaSiswa"></span></p>
+          <p><strong>Nilai Karakter:</strong> <span id="modalNilaiKarater"></span></p>
           <div class="table-responsive">
-            <table class="table table-bordered table-striped table-hover dataTable display responsive nowrap"
-              id="table_rekap_reward">
+            <table class="table table-bordered" id="tableDetailAktivitasReward">
               <thead>
                 <tr>
-                  <th style="vertical-align : middle;text-align:center;">ID Siswa</th>
-                  <th style="vertical-align : middle;text-align:center;">No</th>
-                  <th style="vertical-align : middle;text-align:center;">Nama Siswa</th>
-                  @foreach ($data_aktivitas_reward_siswa_presensi as $aktivitas)
-                    <th style="vertical-align : middle;text-align:center;">{{ $aktivitas }}</th>
-                  @endforeach
+                  <th>Nama Aktivitas</th>
+                  <th>Jenis Aktivitas</th>
+                  <th>Frekuensi</th>
                 </tr>
               </thead>
               <tbody>
-                @foreach ($data_siswa as $siswa)
-                  <tr>
-                    <td>{{ $siswa->id_siswa }}</td>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $siswa->nm_pengguna }}</td>
-                    @foreach ($data_aktivitas_reward_siswa_presensi as $aktivitas)
-                      @php
-                        $data = $data_presensi
-                            ->where('id_kelas', $siswa->id_siswa)
-                            ->where('nm_reward_siswa', $aktivitas)
-                            ->count('id_kelas_mp');
-                      @endphp
-                      @if ($data)
-                        <td style="text-align:center;">
-                          {{ $data }}
-                        </td>
-                      @else
-                        <td style="text-align:center;">0</td>
-                      @endif
-                    @endforeach
-                  </tr>
-                @endforeach
+                <!-- Data akan di-inject dengan JavaScript -->
               </tbody>
             </table>
           </div>
         </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Tutup</button>
+        </div>
       </div>
     </div>
-  @endif
+  </div>
 </div>
 
 <script>
@@ -160,4 +202,48 @@
       loadURI('{{ Request::segment(2) }}/{{ Request::segment(3) }}?kelas=' + kelas + '&semester=' + semester);
     }
   }
+</script>
+
+<script>
+  $('#modalDetailReward').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget);
+    var id = button.data('id');
+    var nis = button.data('nis');
+    var nama = button.data('nama');
+    var karakter = button.data('karakter');
+    var semester = $('select[name=semester]').val();
+
+    var modal = $(this);
+    modal.find('#modalNamaSiswa').text(nama);
+    modal.find('#modalNISSiswa').text(nis);
+    modal.find('#modalNilaiKarater').text(karakter);
+
+    // Kosongkan isi sebelumnya untuk menghindari duplikasi
+    modal.find('#tableDetailAktivitasReward tbody').empty();
+    modal.find('#modalTotalPoint').remove(); // kalau sebelumnya ada total point
+
+    var datatable_detail_url = base_url + '/' + role_url + '/reward-siswa/rekap-reward-siswa/detail/' + id + '?semester=' + semester + '&karakter=' + karakter;
+
+    $.ajax({
+      url: datatable_detail_url,
+      method: 'GET',
+      success: function (data) {
+        if (Array.isArray(data.reward_list)) {
+          data.reward_list.forEach(function (item) {
+
+            $('#tableDetailAktivitasReward tbody').append(`
+              <tr>
+                <td>${item.nm_aktivitas_reward_siswa}</td>
+                <td>${item.nm_jenis_aktivitas_reward}</td>
+                <td>${item.frekuensi}</td>
+              </tr>
+            `);
+          });
+        }
+      },
+      error: function (xhr) {
+        console.error("Gagal mengambil data reward:", xhr);
+      }
+    });
+  });
 </script>
