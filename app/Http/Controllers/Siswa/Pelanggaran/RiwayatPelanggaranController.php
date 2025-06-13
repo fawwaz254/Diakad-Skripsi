13e0f570e95045aa20bf50cc28270caa5a8c6843
jaguardeva <jaguardeva54@gmail.com>
2025-06-13 14:16:03 +0700

    feat(bk&siswa): display total poin pelanggaran

--- app/Http/Controllers/Siswa/Pelanggaran/RiwayatPelanggaranController.php
+++ app/Http/Controllers/Siswa/Pelanggaran/RiwayatPelanggaranController.php
@@ -10,157 +10,165 @@ use Yajra\Datatables\Datatables;
 use Illuminate\Support\Facades\App;
 
 use App\Libraries\Pendidikan\LibSiswa;
-
+use App\Models\PelanggaranSiswa;
+use App\Models\Siswa;
 use Auth;
 use DB;
 use Session;
 use Validator;
 
-class RiwayatPelanggaranController extends BaseController{
+class RiwayatPelanggaranController extends BaseController
+{
 
-    public function viewRiwayatPelanggaran(Request $request){
+    public function viewRiwayatPelanggaran(Request $request)
+    {
         # code...
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
 
-    	return view('siswa/pelanggaran/riwayat-pelanggaran/view-riwayat-pelanggaran',compact('auth_data'));
+        $siswa = Siswa::where("id_pengguna", $auth_data->pengguna->id_pengguna)
+            ->select("id_siswa")
+            ->first();
+        $pelanggaran = PelanggaranSiswa::where('id_siswa', $siswa->id_siswa)
+            ->with("subkategori_pelanggaran")
+            ->get();
 
+        $total_poin = 0;
+        foreach ($pelanggaran as $p) {
+            $total_poin += $p->subkategori_pelanggaran->poin_subkategori_pelanggaran;
+        }
+
+        return view('siswa/pelanggaran/riwayat-pelanggaran/view-riwayat-pelanggaran', compact('auth_data', "total_poin"));
     }
 
-    public function datatablesPelanggaranNonKBM(Request $request){
+    public function datatablesPelanggaranNonKBM(Request $request)
+    {
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
 
         $list_data = LibSiswa::fetchPelanggaranNonKBM($auth_data, $auth_data->pengguna->id_pengguna);
 
         return Datatables::of($list_data)
-                ->addColumn('tgl_pelanggaran', function($item){
-                    return strftime( "%A, %d %B %Y", strtotime($item->tgl_pelanggaran));
-                })
-                ->addColumn('aktor_input_pelanggaran', function($item){
-                    switch($item->aktor_input_pelanggaran){
-                        case 1: 
-                            return 'Role BK'; break;
-                        case 2: 
-                            return 'Kesiswaan'; break;
-                        case 3: 
-                            return 'Wali Kelas'; break;
-                        case 4:
-                            return 'Guru Reguler'; break;
-                        case 5:
-                            return 'Guru Piket'; break;
-                        default:
-                            return ''; break;
+            ->addColumn('tgl_pelanggaran', function ($item) {
+                return strftime("%A, %d %B %Y", strtotime($item->tgl_pelanggaran));
+            })
+            ->addColumn('aktor_input_pelanggaran', function ($item) {
+                switch ($item->aktor_input_pelanggaran) {
+                    case 1:
+                        return 'Role BK';
+                        break;
+                    case 2:
+                        return 'Kesiswaan';
+                        break;
+                    case 3:
+                        return 'Wali Kelas';
+                        break;
+                    case 4:
+                        return 'Guru Reguler';
+                        break;
+                    case 5:
+                        return 'Guru Piket';
+                        break;
+                    default:
+                        return '';
+                        break;
+                }
+            })
+            ->addColumn('nm_jenis_tindakan', function ($item) {
+                if ($item->is_sudah_tindakan == 1) {
+                    return $item->nm_jenis_tindakan;
+                } else {
+                    return "-";
+                }
+            })
+            ->addColumn('catatan_tindakan_pelanggaran', function ($item) {
+                if ($item->is_sudah_tindakan == 1) {
+                    return $item->catatan_tindakan_pelanggaran;
+                } else {
+                    return "-";
+                }
+            })
+            ->addColumn('tgl_tindakan_pelanggaran', function ($item) {
+                if ($item->is_sudah_tindakan == 1) {
+                    return strftime("%A, %d %B %Y", strtotime($item->tgl_tindakan_pelanggaran));
+                } else {
+                    return "-";
+                }
+            })
+            ->addColumn('aktor_input_tindakan_pelanggaran', function ($item) {
+                if ($item->is_sudah_tindakan == 1) {
+                    if ($item->aktor_input_tindakan_pelanggaran == 1) {
+                        return "Guru BK";
+                    } elseif ($item->aktor_input_tindakan_pelanggaran == 2) {
+                        return "Kesiswaan";
                     }
-                })
-                ->addColumn('nm_jenis_tindakan', function($item){
-                    if($item->is_sudah_tindakan == 1) {
-                        return $item->nm_jenis_tindakan;
-                    }
-                    else {
-                        return "-";
-                    }
-                })
-                ->addColumn('catatan_tindakan_pelanggaran', function($item){
-                    if($item->is_sudah_tindakan == 1) {
-                        return $item->catatan_tindakan_pelanggaran;
-                    }
-                    else {
-                        return "-";
-                    }
-                })
-                ->addColumn('tgl_tindakan_pelanggaran', function($item){
-                    if($item->is_sudah_tindakan == 1) {
-                        return strftime( "%A, %d %B %Y", strtotime($item->tgl_tindakan_pelanggaran));                    
-                    }
-                    else {
-                        return "-";
-                    }
-                })
-                ->addColumn('aktor_input_tindakan_pelanggaran', function($item){
-                    if($item->is_sudah_tindakan == 1) {
-                        if($item->aktor_input_tindakan_pelanggaran == 1) {
-                            return "Guru BK";
-                        }
-                        elseif($item->aktor_input_tindakan_pelanggaran == 2) {
-                            return "Kesiswaan";
-                        }
-                    }
-                    else {
-                        return "-";
-                    }
-                })
-                ->make(true);
+                } else {
+                    return "-";
+                }
+            })
+            ->make(true);
     }
 
-    public function datatablesPelanggaranKBM(Request $request){
+    public function datatablesPelanggaranKBM(Request $request)
+    {
         $input = (object) $request->input();
         $auth_data = $input->auth_data;
 
         $list_data = LibSiswa::fetchPelanggaranKBM($auth_data, $auth_data->pengguna->id_pengguna);
 
         return Datatables::of($list_data)
-                ->addColumn('semester', function($item){
-                            return $item->tahun_ajaran." ".$item->nm_semester;
-                        })
-                ->addColumn('mata_pelajaran', function($item){
-                    return $item->kd_mata_pelajaran." - ".$item->nm_mata_pelajaran;
-                })
-                ->addColumn('nm_pengguna', function($item){
-                    if( ! empty($item->gelar_depan) && ! empty($item->gelar_belakang)) {
-                        return $item->gelar_depan." ".$item->nm_pengguna.", ".$item->gelar_belakang;
+            ->addColumn('semester', function ($item) {
+                return $item->tahun_ajaran . " " . $item->nm_semester;
+            })
+            ->addColumn('mata_pelajaran', function ($item) {
+                return $item->kd_mata_pelajaran . " - " . $item->nm_mata_pelajaran;
+            })
+            ->addColumn('nm_pengguna', function ($item) {
+                if (! empty($item->gelar_depan) && ! empty($item->gelar_belakang)) {
+                    return $item->gelar_depan . " " . $item->nm_pengguna . ", " . $item->gelar_belakang;
+                } elseif (! empty($item->gelar_depan)) {
+                    return $item->gelar_depan . " " . $item->nm_pengguna;
+                } elseif (! empty($item->gelar_belakang)) {
+                    return $item->nm_pengguna . ", " . $item->gelar_belakang;
+                } else {
+                    return $item->nm_pengguna;
+                }
+            })
+            ->addColumn('tgl_entry', function ($item) {
+                return strftime("%A, %d %B %Y", strtotime($item->tgl_entry));
+            })
+            ->addColumn('nm_jenis_tindakan', function ($item) {
+                if ($item->is_sudah_tindakan == 1) {
+                    return $item->nm_jenis_tindakan;
+                } else {
+                    return "-";
+                }
+            })
+            ->addColumn('catatan_tindakan_pelanggaran', function ($item) {
+                if ($item->is_sudah_tindakan == 1) {
+                    return $item->catatan_tindakan_pelanggaran;
+                } else {
+                    return "-";
+                }
+            })
+            ->addColumn('tgl_tindakan_pelanggaran', function ($item) {
+                if ($item->is_sudah_tindakan == 1) {
+                    return strftime("%A, %d %B %Y", strtotime($item->tgl_tindakan_pelanggaran));
+                } else {
+                    return "-";
+                }
+            })
+            ->addColumn('aktor_input_tindakan_pelanggaran', function ($item) {
+                if ($item->is_sudah_tindakan == 1) {
+                    if ($item->aktor_input_tindakan_pelanggaran == 1) {
+                        return "Guru BK";
+                    } elseif ($item->aktor_input_tindakan_pelanggaran == 2) {
+                        return "Kesiswaan";
                     }
-                    elseif( ! empty($item->gelar_depan)) {
-                        return $item->gelar_depan." ".$item->nm_pengguna;   
-                    }
-                    elseif( ! empty($item->gelar_belakang)) {
-                        return $item->nm_pengguna.", ".$item->gelar_belakang;   
-                    }
-                    else {
-                        return $item->nm_pengguna; 
-                    }
-                })
-                ->addColumn('tgl_entry', function($item){
-                    return strftime( "%A, %d %B %Y", strtotime($item->tgl_entry));
-                })
-                ->addColumn('nm_jenis_tindakan', function($item){
-                    if($item->is_sudah_tindakan == 1) {
-                        return $item->nm_jenis_tindakan;
-                    }
-                    else {
-                        return "-";
-                    }
-                })
-                ->addColumn('catatan_tindakan_pelanggaran', function($item){
-                    if($item->is_sudah_tindakan == 1) {
-                        return $item->catatan_tindakan_pelanggaran;
-                    }
-                    else {
-                        return "-";
-                    }
-                })
-                ->addColumn('tgl_tindakan_pelanggaran', function($item){
-                    if($item->is_sudah_tindakan == 1) {
-                        return strftime( "%A, %d %B %Y", strtotime($item->tgl_tindakan_pelanggaran));                    
-                    }
-                    else {
-                        return "-";
-                    }
-                })
-                ->addColumn('aktor_input_tindakan_pelanggaran', function($item){
-                    if($item->is_sudah_tindakan == 1) {
-                        if($item->aktor_input_tindakan_pelanggaran == 1) {
-                            return "Guru BK";
-                        }
-                        elseif($item->aktor_input_tindakan_pelanggaran == 2) {
-                            return "Kesiswaan";
-                        }
-                    }
-                    else {
-                        return "-";
-                    }
-                })
-                ->make(true);
+                } else {
+                    return "-";
+                }
+            })
+            ->make(true);
     }
-
 }
