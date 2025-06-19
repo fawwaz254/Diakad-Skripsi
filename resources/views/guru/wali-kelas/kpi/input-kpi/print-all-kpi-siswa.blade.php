fc6561a1e319c51c339b0e8a6fd2b6eeb5e2a681
Rio Ramadhan <riordhn@gmail.com>
2025-06-19 11:30:00 +0700

    fix: show kop wh2

--- resources/views/guru/wali-kelas/kpi/input-kpi/print-all-kpi-siswa.blade.php
+++ resources/views/guru/wali-kelas/kpi/input-kpi/print-all-kpi-siswa.blade.php
@@ -87,6 +87,9 @@
 
     @foreach ($list_siswa as $siswa)
         <div class="page">
+            @if ($auth_data->sekolah_data->nm_singkat_sekolah == 'smawh2')
+                <img id="kop" style="width:99%;" src="{{ asset('media/kop-surat-logo-sma-wh-2.png') }}">
+            @endif
             <table cellspacing="0" cellpadding="10" style="width: 90%; margin: 20px  auto;" style="border-style : hidden">
                 <tr>
                     <td colspan="10" style="border-style : hidden">
